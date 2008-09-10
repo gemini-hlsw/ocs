79f248c36c7e4c0795986bf9bb529c401ceff498
anunez <anunez@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2008-09-10 15:16:33 +0000

    Just a few minor changes to the installation instructions before moving to the new svn machine
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-glue-cc/trunk@12968 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- config.mk
+++ config.mk
@@ -1,10 +1,10 @@
 #Directories used to build and install the GIAPI C++ language glue. 
 
 #Installation directory
-INSTALL_DIR := /Users/anunez/giapi/install
+INSTALL_DIR := /home/anunez/Projects/giapi/install
 
 #External libraries dependencies, used during build
-EXTERNAL_LIB := /Users/anunez/Projects/aspen/giapi/giapi-external
+EXTERNAL_LIB := /home/anunez/Projects/giapi/giapi-external
 
 #Default system include directory. 
 SYSTEM_INCLUDE_DIR := /usr/include
