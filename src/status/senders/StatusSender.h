6a664706371dfa8028df8366a5c543ca28a29506
anunez <anunez@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2008-12-16 21:20:10 +0000

    Typo fixed in comments
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-glue-cc/trunk@16300 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- src/status/senders/StatusSender.h
+++ src/status/senders/StatusSender.h
@@ -4,10 +4,10 @@
 namespace giapi {
 /**
  * This is the public status sender interface. These methods
- * are used by client programs to interact with the status sender. 
- * Instances of status senders are obtained through the  
- * {@link StatusFactory}. 
- * 
+ * are used by client programs to interact with the status sender.
+ * Instances of status senders are obtained through the
+ * {@link StatusFactory}.
+ *
  * @see StatusFactory
  */
 class StatusSender {
@@ -16,28 +16,28 @@ public:
 	/**
 	 * Post all pending status to Gemini. Pending statuses are those
 	 * whose value has changed since last time posted by GIAPI. A status
-	 * item is marked as "dirty" every time a new value is set. 
-	 * 
-	 * @return giapi::status::OK if the post suceeded. 
+	 * item is marked as "dirty" every time a new value is set.
+	 *
+	 * @return giapi::status::OK if the post succeeded.
 	 *         giapi::status::ERROR if there is some error in the attempt
-	 *         to send  
-	 * @throws PostException in case there is a problem with the underlying 
-	 *         mechanisms to execute the post. 
+	 *         to send
+	 * @throws PostException in case there is a problem with the underlying
+	 *         mechanisms to execute the post.
 	 */
 	virtual int postStatus() const throw (PostException) = 0;
 
 	/**
 	 * Post the specified status to Gemini. The status will be sent only
 	 * if it has changed its value since the last time it was posted. A status
-	 * item is marked as "dirty" every time a new value is set. 
-	 * 
+	 * item is marked as "dirty" every time a new value is set.
+	 *
 	 * @args   name The name of the status item to be posted
-	 * @return giapi::status::OK if the post suceeds. 
+	 * @return giapi::status::OK if the post suceeds.
 	 *         giapi::status::ERROR if there is no information associated
-	 *         to the specified status item or the <code>name</code> 
+	 *         to the specified status item or the <code>name</code>
 	 *         parameter is NULL
-	 * @throws PostException in case there is a problem with the underlying 
-	 *         mechanisms to execute the post. 
+	 * @throws PostException in case there is a problem with the underlying
+	 *         mechanisms to execute the post.
 	 */
 	virtual int postStatus(const char* name) const throw (PostException) = 0;
 
@@ -45,10 +45,10 @@ public:
 
 protected:
 	/**
-	 * Protected default constructor. StatusSender concrete objectes 
+	 * Protected default constructor. StatusSender concrete objects
 	 * are constructed using a {@link StatusFactory}.
-	 * 
-	 * @see StatusFactory 
+	 *
+	 * @see StatusFactory
 	 */
 	StatusSender();
 };
