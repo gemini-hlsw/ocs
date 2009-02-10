ec92f61a30beb21f14d9c1ca40ae7f7a191558b2
anunez <anunez@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2009-02-10 14:55:44 +0000

    Replaced char * usages with std::string in EpicsStatusItem
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-glue-cc/trunk@17797 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- giapi/EpicsStatusItem.h
+++ giapi/EpicsStatusItem.h
@@ -8,49 +8,49 @@ namespace giapi {
 
 /**
  * Interface for an Epics Status Item.
- * 
+ *
  * EpicsStatusItem objects are managed internally
- * by the GIAPI. Instrument code will get smart pointer references to 
- * these objects when the <code>update</code> method of the 
- * <code>EpicsStatusHandler</code> interface is invoked.  
- * 
+ * by the GIAPI. Instrument code will get smart pointer references to
+ * these objects when the <code>update</code> method of the
+ * <code>EpicsStatusHandler</code> interface is invoked.
+ *
  */
 class EpicsStatusItem {
 public:
 	/**
 	 * Return the name of the Epics channel
 	 * represented by this status item
-	 * 
-	 * @return Name of the Epics channel represented by 
+	 *
+	 * @return Name of the Epics channel represented by
 	 *         this epics status item
 	 */
-	virtual const char * getName() const = 0;
+	virtual const std::string getName() const = 0;
 
 	/**
 	 * Return the data type of the values contained
 	 * in this epics status item
-	 * 
-	 * @return the data type of the value or values 
-	 *         contained in this epics status item. 
+	 *
+	 * @return the data type of the value or values
+	 *         contained in this epics status item.
 	 */
 	virtual type::Type getType() const = 0;
 
 	/**
 	 * Provides access to the data associated to the
 	 * epics status item.
-	 * 
-	 * @return pointer to the data contained in this 
-	 *         epics status item. 
+	 *
+	 * @return pointer to the data contained in this
+	 *         epics status item.
 	 */
 	virtual const void * getData() const = 0;
 
 	/**
 	 * Return the number of elements stored in the data
 	 * part of this epics status item. All the elements
-	 * are of the same data type, as returned by the 
+	 * are of the same data type, as returned by the
 	 * <code>getType</code> method.
-	 * 
-	 * @return number of elements stored in the data 
+	 *
+	 * @return number of elements stored in the data
 	 *         part of this epics status element
 	 */
 	virtual int getCount() const = 0;
@@ -62,7 +62,7 @@ public:
 };
 
 /**
- * Definition of a smart pointer to an EpicsStatusItem instance 
+ * Definition of a smart pointer to an EpicsStatusItem instance
  */
 typedef std::tr1::shared_ptr<EpicsStatusItem> pEpicsStatusItem;
 }
