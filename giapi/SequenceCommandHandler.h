bc7632f90146efb68bb7ac4c9eecb9bda80de556
anunez <anunez@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2009-02-13 15:43:37 +0000

    Removed unnecesary definition file for SequenceCommandHandler interface
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-glue-cc/trunk@17957 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- giapi/SequenceCommandHandler.h
+++ giapi/SequenceCommandHandler.h
@@ -12,34 +12,34 @@ namespace giapi {
  * Interface definition for sequence command handlers.
  *
  * @see CommandUtil::subscribeApply
- * @see CommandUtil::subscribeCommand 
+ * @see CommandUtil::subscribeCommand
  */
 class SequenceCommandHandler {
 public:
 
 	/**
-	 * Handler method for sequence commands. Invoked when 
+	 * Handler method for sequence commands. Invoked when
 	 * commands are received by the GIAPI if the handler has been
-	 * registered using the <code>subscribeSequenceCommand</code> or the 
+	 * registered using the <code>subscribeSequenceCommand</code> or the
 	 * <code>subscribeApplyCommand</code> methods.
-	 * 
-	 * @param id the unique value that identifies the request or 
+	 *
+	 * @param id the unique value that identifies the request or
 	 *        equivalently identifies the set of actions started by the request.
 	 *        The handler should retain the value unless the actions
-	 *        started by the request are completed immediately. The same 
+	 *        started by the request are completed immediately. The same
 	 *        action id will not be used again in any future request unless
 	 *        the GMP is restarted
-	 * @param sequenceCommand the sequence command that should be handled, 
-	 *        such as TEST or REBOOT. 
+	 * @param sequenceCommand the sequence command that should be handled,
+	 *        such as TEST or REBOOT.
 	 * @param activity Activity requested by the sender, like PRESET_START or
 	 *        CANCEL
-	 * @param config  configuration associated to the command. 
-	 * 
-	 * @return a <code>pHandlerResponse</code> pointer, containing the result 
-	 *        of handling the command. 
-	 * 
+	 * @param config  configuration associated to the command.
+	 *
+	 * @return a <code>pHandlerResponse</code> pointer, containing the result
+	 *        of handling the command.
+	 *
 	 * @see CommandUtil::subscribeApply
-	 * @see CommandUtil::subscribeCommand 
+	 * @see CommandUtil::subscribeCommand
 	 */
 	virtual pHandlerResponse handle(command::ActionId id,
 			command::SequenceCommand sequenceCommand,
@@ -48,16 +48,16 @@ public:
 	/**
 	 * Virtual destructor
 	 */
-	virtual ~SequenceCommandHandler();
+	virtual ~SequenceCommandHandler() {};
 
 protected:
 	/**
-	 * Protected constructor. This is an abstract class that can not 
-	 * be instantiated. Instrument code needs to extend this class and 
-	 * provide a concrete implementation of the <code>handle</code> 
-	 * method. 
+	 * Protected constructor. This is an abstract class that can not
+	 * be instantiated. Instrument code needs to extend this class and
+	 * provide a concrete implementation of the <code>handle</code>
+	 * method.
 	 */
-	SequenceCommandHandler();
+	SequenceCommandHandler() {};
 
 };
 
