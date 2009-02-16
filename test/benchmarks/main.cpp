d22e2d57b419db9edba4f5378daa0c8c52eaf51f
anunez <anunez@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2009-02-16 18:08:32 +0000

    Reformatted and removed unnecessary header file
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-glue-cc/trunk@17972 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- test/benchmarks/main.cpp
+++ test/benchmarks/main.cpp
@@ -2,34 +2,31 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include <cppunit/TestResult.h>
 #include <iostream.h>
-#include <cppunit/BriefTestProgressListener.h>
-
 
 int main(int argc, char **argv) {
 	try {
 
 		CppUnit::TextUi::TestRunner runner;
 		CppUnit::TestFactoryRegistry &registry =
-			CppUnit::TestFactoryRegistry::getRegistry();
+				CppUnit::TestFactoryRegistry::getRegistry();
 
-		runner.addTest( registry.makeTest() );
+		runner.addTest(registry.makeTest());
 
-        std::cout << "=====================================================\n";
-        std::cout << "Starting the Benchmarks:" << std::endl;
-        std::cout << "-----------------------------------------------------\n";
+		std::cout << "=====================================================\n";
+		std::cout << "Starting the Benchmarks:" << std::endl;
+		std::cout << "-----------------------------------------------------\n";
 
-        bool wasSuccessful = runner.run( "", false );
+		bool wasSuccessful = runner.run("", false);
 
-        std::cout << "-----------------------------------------------------\n";
-        std::cout << "Finished with the Benchmarks." << std::endl;
-        std::cout << "=====================================================\n";
+		std::cout << "-----------------------------------------------------\n";
+		std::cout << "Finished with the Benchmarks." << std::endl;
+		std::cout << "=====================================================\n";
 
-        return !wasSuccessful;
-
+		return !wasSuccessful;
 
 	} catch (...) {
 		std::cout << "----------------------------------------" << std::endl;
-        std::cout << "- AN ERROR HAS OCCURED:                -" << std::endl;
-        std::cout << "----------------------------------------" << std::endl;
+		std::cout << "- AN ERROR HAS OCCURED:                -" << std::endl;
+		std::cout << "----------------------------------------" << std::endl;
 	}
 }
