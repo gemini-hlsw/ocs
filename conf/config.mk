bc8768e2a5a9162d702fde79a00394aa54f8e99e
anunez <anunez@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2010-03-14 06:57:12 +0000

    Updated tests makefiles to use updated APR library
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-glue-cc/trunk@24584 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- conf/config.mk
+++ conf/config.mk
@@ -1,10 +1,10 @@
 #Directories used to build and install the GIAPI C++ language glue. 
 
 #Installation directory
-INSTALL_DIR := /Users/anunez/Projects/giapi/install
+INSTALL_DIR := /home/anunez/Projects/giapi/install
 
 #External libraries dependencies, used during build
-EXTERNAL_LIB := /Users/anunez/Projects/giapi/giapi-external
+EXTERNAL_LIB := /home/anunez/Projects/giapi/giapi-external
 
 #Directory where distribution files are going to be stored
-DISTRIBUTION_DIR := /Users/anunez/Projects/giapi/dist
+DISTRIBUTION_DIR := /home/anunez/Projects/giapi/dist
