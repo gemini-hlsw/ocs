136fa5529f79e38b3a19972279d6ba3c0392d215
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2010-09-23 20:16:32 +0000

    Added some doxygen comments.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-glue-cc/trunk@27301 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- src/util/TimeUtil.h
+++ src/util/TimeUtil.h
@@ -7,6 +7,9 @@ namespace giapi {
 
 namespace util {
 
+/**
+* Class to provide a way to measure elapsed time.
+*/
 class TimeUtil {
 private:
     timeval startTime, endTime;
@@ -16,11 +19,51 @@ public:
     TimeUtil();
     ~TimeUtil();
 
+    /**
+    * Start the timer.
+    */
     void startTimer();
+
+    /**
+    * Stop the timer.
+    *
+    * @throws logic_error if this method is called before calling <code>startTimer()</code>
+    */
     void stopTimer() throw(std::logic_error);
+
+    /**
+    * Gets the elapsed time between subsequent calls to <code>startTimer()</code>
+    * and <code>stopTimer()</code>
+    *
+    * @param unit the time unit of the return value. Either Seconds, milliseconds or microseconds.
+    * @return the elapsed time
+    * @throws logic_error if this method is called before calling <code>stopTimer()</code>
+    */
     unsigned long long getElapsedTime(TimeUnit unit) throw(std::logic_error);
+
+    /**
+    * Gets the elapsed time in microseconds between subsequent calls to <code>startTimer()</code>
+    * and <code>stopTimer()</code>
+    *
+    * @return the elapsed time
+    * @throws logic_error if this method is called before calling <code>stopTimer()</code>
+    */
     unsigned long long getElapsedUSecs()throw(std::logic_error);
+    /**
+    * Gets the elapsed time in milliseconds between subsequent calls to <code>startTimer()</code>
+    * and <code>stopTimer()</code>
+    *
+    * @return the elapsed time
+    * @throws logic_error if this method is called before calling <code>stopTimer()</code>
+    */
     unsigned long long getElapsedMSecs()throw(std::logic_error);
+    /**
+    * Gets the elapsed time in seconds between subsequent calls to <code>startTimer()</code>
+    * and <code>stopTimer()</code>
+    *
+    * @return the elapsed time
+    * @throws logic_error if this method is called before calling <code>stopTimer()</code>
+    */
     unsigned long long getElapsedSecs()throw(std::logic_error);
 };
 }
