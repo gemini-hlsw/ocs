476b3e225ea66e538729c1154726e28bc5304a53
nbarriga <nbarriga@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-03-01 15:05:39 +0000

    Improved script.
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-glue-cc/trunk@32299 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- src/examples/epicsclients.sh
+++ src/examples/epicsclients.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-/gemsoft/opt/epics/base/bin/linux-x86/camonitor gpi:health0 gpi:health1 gpi:health2 gpi:health3 gpi:health4 gpi:health5 gpi:health6 gpi:health7 gpi:health8 gpi:health9 gpi:status0  gpi:status1  gpi:status2  gpi:status3  gpi:status4  gpi:status5  gpi:status6  gpi:status7  gpi:status8  gpi:status9 gpi:alarm0  gpi:alarm1  gpi:alarm2  gpi:alarm3  gpi:alarm4  gpi:alarm5  gpi:alarm6  gpi:alarm7  gpi:alarm8  gpi:alarm9  
+CAMONITOR=`which camonitor 2>/dev/null`
+if [ $CAMONITOR ]; then 
+    $CAMONITOR gpi:health0 gpi:health1 gpi:health2 gpi:health3 gpi:health4 gpi:health5 gpi:health6 gpi:health7 gpi:health8 gpi:health9 gpi:status0  gpi:status1  gpi:status2  gpi:status3  gpi:status4  gpi:status5  gpi:status6  gpi:status7  gpi:status8  gpi:status9 gpi:alarm0  gpi:alarm1  gpi:alarm2  gpi:alarm3  gpi:alarm4  gpi:alarm5  gpi:alarm6  gpi:alarm7  gpi:alarm8  gpi:alarm9  
+else
+    echo "Please install the epics-base RPM package";
+fi
