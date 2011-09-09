40f7b7879bfb63442fdf30db3f8afc8484ffb1b1
cquiroz <cquiroz@ee6ba543-07f8-4773-b02c-01d50e9d00ba>
2011-09-09 12:34:46 +0000

    Added getting all service properties to the example
    git-svn-id: http://source.gemini.edu/software/giapi/giapi-glue-cc/trunk@37241 ee6ba543-07f8-4773-b02c-01d50e9d00ba

--- src/examples/services.cpp
+++ src/examples/services.cpp
@@ -20,27 +20,43 @@ void
 terminate(int signal)
 {
     std::cout << "Exiting... " << std::endl;
-	exit(1);
+    exit(1);
 }
 
 int 
 main(int argc, char **argv)
 {
 
-	try {
+    try {
 
         std:: cout << "Starting Services Util Example" << std::endl;
 
-		signal(SIGABRT, terminate);
-		signal(SIGTERM, terminate);
-		signal(SIGINT, terminate);
+        signal(SIGABRT, terminate);
+        signal(SIGTERM, terminate);
+        signal(SIGINT, terminate);
 
         std:: string host = ServicesUtil::getProperty("GMP_HOST_NAME", 1000);
 
         std:: cout << "Hostname: " << host << std::endl;
 
-	} catch(GmpException & e) {
+        std:: string ancilliaryDataPath = ServicesUtil::getProperty("DHS_ANCILLARY_DATA_PATH", 1000);
+
+        std:: cout << "Ancilliary Data Path: " << ancilliaryDataPath << std::endl;
+
+        std:: string sciencePath = ServicesUtil::getProperty("DHS_SCIENCE_DATA_PATH", 1000);
+
+        std:: cout << "Science Path: " << sciencePath << std::endl;
+
+        std:: string intermediatePath = ServicesUtil::getProperty("DHS_INTERMEDIATE_DATA_PATH", 1000);
+
+        std:: cout << "Intermediate Data Path: " << intermediatePath << std::endl;
+
+        std:: string defaultValue = ServicesUtil::getProperty("DEFAULT", 1000);
+
+        std:: cout << "Default: " << defaultValue << std::endl;
+
+    } catch(GmpException & e) {
         std:: cerr << e.getMessage() << ". Is the GMP up?" << std::endl;
-	}
-	return 0;
+    }
+    return 0;
 }
