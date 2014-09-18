5d2fac8ce502f20ec3c3fbc1f520b0b7a7f49300
Florian Nussberger <florian.nussberger@gmail.com>
2014-09-18 09:47:49 -1000

    REL-1368: Replaced asInstanceOf with match statement.

--- bundle/edu.gemini.qpt.client/src/main/scala/edu/gemini/qpt/core/listeners/PropagationWindowsListener.scala
+++ bundle/edu.gemini.qpt.client/src/main/scala/edu/gemini/qpt/core/listeners/PropagationWindowsListener.scala
@@ -15,7 +15,6 @@ import scala.collection.JavaConverters._
  */
 class PropagationWindowsListener extends MarkerModelListener[Variant] {
 
-<<<<<<< HEAD
   def propertyChange(evt : PropertyChangeEvent) = evt.getSource match {
     case variant: Variant => addMarkers(variant)
     case _ => throw new RuntimeException("Listener can only be used with variants.")
@@ -26,14 +25,6 @@ class PropagationWindowsListener extends MarkerModelListener[Variant] {
     val markerManager : MarkerManager = variant.getSchedule.getMarkerManager
     markerManager.clearMarkers(this, variant)
 
-=======
-  def propertyChange(evt : PropertyChangeEvent): Unit = {
-    val variant : Variant = evt.getSource.asInstanceOf[Variant]
-    val markerManager : MarkerManager = variant.getSchedule.getMarkerManager
-    markerManager.clearMarkers(this, variant)
-
-    // Check if there are any laser observations for which we don't have clearance windows.
->>>>>>> 2fb8c91297394db849f1cdb4feb49496861dee53
     val lgsAllocs = variant.getAllocs.asScala.filter(_.getObs.getLGS)
     lgsAllocs.filter(a => hasNoClearanceWindows(a.getObs)).foreach { a =>
       markerManager.addMarker(false, this, Marker.Severity.Error, "LGS observation has no clearance windows.", variant, a)
@@ -41,21 +32,13 @@ class PropagationWindowsListener extends MarkerModelListener[Variant] {
   }
 
   // check if qpt obs object is missing clearance windows
-<<<<<<< HEAD
   private def hasNoClearanceWindows(obs: Obs): Boolean = {
-=======
-  def hasNoClearanceWindows(obs: Obs): Boolean = {
->>>>>>> 2fb8c91297394db849f1cdb4feb49496861dee53
     val lttsObs = Option(LttsServicesClient.getInstance.getObservation(obs))
     lttsObs.isEmpty || hasNoClearanceWindows(lttsObs.get)
   }
 
   // check if the LTTS obs object is missing clearance windows
-<<<<<<< HEAD
   private def hasNoClearanceWindows(lttsObs: Observation): Boolean =
-=======
-  def hasNoClearanceWindows(lttsObs: Observation): Boolean =
->>>>>>> 2fb8c91297394db849f1cdb4feb49496861dee53
     lttsObs.getLaserTargets.asScala.exists(_.getClearanceWindows.isEmpty)
 
 
