2e2359619f0905df01cdd9077ee1b0cd7fba26e2
Sebastian Raaphorst <sraaphor@gemini.edu>
2014-09-22 10:54:57 -0300

    OCSADV-119: Changed PosAngleConstraint to support UNBOUNDED.

--- bundle/edu.gemini.pot/src/main/java/edu/gemini/spModel/telescope/PosAngleConstraint.java
+++ bundle/edu.gemini.pot/src/main/java/edu/gemini/spModel/telescope/PosAngleConstraint.java
@@ -13,24 +13,11 @@ import java.util.List;
  */
 public enum PosAngleConstraint {
     /** No constraint on adjusting pos angle. */
-    UNKNOWN() {
-        public ImList<Angle> steps(Angle start, Angle stepSize) {
-            int deg = (int) Math.round(stepSize.toPositive().toDegrees().getMagnitude());
-            int stepCount = (deg == 0) ? 1 : 360 / deg;
-
-            Angle cur = start;
-            List<Angle> res = new ArrayList<Angle>(stepCount);
-            for (int i=0; i<stepCount; ++i) {
-                res.add(cur);
-                cur = cur.add(stepSize);
-            }
-
-            return DefaultImList.create(res);
-        }
-    },
+    UNKNOWN,
 
     /** The provided pos angle only. */
     FIXED() {
+        @Override
         public ImList<Angle> steps(Angle start, Angle stepSize) {
             return ImCollections.singletonList(start);
         }
@@ -38,23 +25,37 @@ public enum PosAngleConstraint {
 
     /** The provided pos angle, or the pos angle plus 180 deg. */
     FIXED_180() {
+        @Override
         public ImList<Angle> steps(Angle start, Angle stepSize) {
             return DefaultImList.create(start, start.add(Angle.ANGLE_PI));
         }
     },
 
-    /** A pos angle aligned with the parallactic angle. */
-//    PARALLACTIC,
+    /** The unbounded case, where we want to allow for any angle accessible
+     * by the guide probe.
+     */
+    UNBOUNDED,
 
-    /** A pos angle aligned with the prallactic angle. */
-//    PARALLACTIC_180,
     ;
 
     /**
      * Steps through position angles from the start angle through 360 degrees
      * by step size, but obeying the position angle constraint.  For example,
      * the FIXED constraint would provide just a single angle, the start
-     * angle.  A NONE constraint will step through the entire range.
+     * angle.  An UNKNOWN or UNBOUNDED constraint will step through the entire range.
+     * As the entire range is used by multiple enum values, it is the default.
      */
-    public abstract ImList<Angle> steps(Angle start, Angle stepSize);
+    public ImList<Angle> steps(Angle start, Angle stepSize) {
+        int deg = (int) Math.round(stepSize.toPositive().toDegrees().getMagnitude());
+        int stepCount = (deg == 0) ? 1 : 360 / deg;
+
+        Angle cur = start;
+        List<Angle> res = new ArrayList<Angle>(stepCount);
+        for (int i=0; i<stepCount; ++i) {
+            res.add(cur);
+            cur = cur.add(stepSize);
+        }
+
+        return DefaultImList.create(res);
+    }
 }
