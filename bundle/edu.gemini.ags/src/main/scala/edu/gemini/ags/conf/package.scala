7d52597cfc5d72e6c906f017a378c60930a3c96f
swalker2m <swalker2m@mac.com>
2014-11-18 12:55:06 -0300

    OCSADV-191: cleanup

--- bundle/edu.gemini.ags/src/main/scala/edu/gemini/ags/conf/package.scala
+++ bundle/edu.gemini.ags/src/main/scala/edu/gemini/ags/conf/package.scala
@@ -3,9 +3,6 @@ package edu.gemini.ags
 import edu.gemini.spModel.gemini.obscomp.SPSiteQuality.{SkyBackground, ImageQuality}
 import edu.gemini.spModel.guide.GuideSpeed
 
-import scalaz._
-import Scalaz._
-
 package object conf {
 
   sealed case class MagLimitsId(name: String)
@@ -43,41 +40,15 @@ package object conf {
   case class FaintnessKey(iq: ImageQuality, sb: SkyBackground, gs: GuideSpeed) {
     override def toString: String = s"($iq, $sb, $gs)"
   }
+
   type FaintnessMap = Map[FaintnessKey, Double]
 
+  val AllFaintnessKeys =
+    (for {
+      iq <- ImageQuality.values()
+      sb <- SkyBackground.values()
+      gs <- GuideSpeed.values()
+    } yield FaintnessKey(iq, sb, gs)).toSet
+
   type CalcMap = Map[MagLimitsId, ProbeLimitsCalc]
-
-  // The parser validates most aspects of the CalcMap, but doesn't catch missing
-  // tables or duplicate faintness keys.
-  def validate(cm: CalcMap): String \/ Unit = {
-    def missingIds: String \/ Unit =
-      AllLimitsIds.filterNot(cm.contains).map(_.name) match {
-        case Nil => ().right
-        case ids => ids.mkString("Missing table for: ", ", ", "").left
-      }
-
-    // Each ProbeLimitsCalc entry should have all these keys.
-    val allKeys =
-      (for {
-        iq <- ImageQuality.values()
-        sb <- SkyBackground.values()
-        gs <- GuideSpeed.values()
-      } yield FaintnessKey(iq, sb, gs)).toSet
-
-    def incompleteTables: String \/ Unit = {
-      val errors = cm.map { case (MagLimitsId(name), ProbeLimitsCalc(_, _, fm)) =>
-        name -> (allKeys &~ fm.keySet)
-      }.filterNot(_._2.isEmpty).map { case (name, keys) =>
-        s"$name is missing entries: ${keys.mkString(", ")}"
-      }
-      if (errors.isEmpty) ().right
-      else errors.mkString(", ").left
-    }
-
-    for {
-      _ <- missingIds
-      _ <- incompleteTables
-    } yield cm
-  }
-
 }
