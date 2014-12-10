c94b492ce5f769df29a83f58e202d38de870c759
Rob Norris <rob_norris@mac.com>
2014-12-09 17:14:53 -0800

    initial tests

---
+++ bundle/edu.gemini.oodb.too.url/src/test/scala/edu/gemini/spdb/rapidtoo/www/Arbitraries.scala
@@ -0,0 +1,42 @@
+package edu.gemini.spdb.rapidtoo.www
+
+import edu.gemini.shared.skyobject.Magnitude
+import edu.gemini.shared.skyobject.Magnitude.{ Band, System }
+import edu.gemini.spModel.core.{ Angle, RightAscension, Declination }
+import edu.gemini.spdb.rapidtoo.TooGuideTarget.GuideProbe
+import org.scalacheck._
+import org.scalacheck.Gen._
+import org.scalacheck.Arbitrary._
+
+trait Arbitraries {
+
+  implicit val arbAngle: Arbitrary[Angle] =
+    Arbitrary(arbitrary[Short].map(n => n / 10.0).map(Angle.fromDegrees))
+
+  implicit val arbRA: Arbitrary[RightAscension] =
+    Arbitrary(arbitrary[Angle].map(RightAscension.fromAngle))
+
+  implicit val arbDec: Arbitrary[Declination] =
+    Arbitrary(arbitrary[Angle].map(Declination.zero.offset(_)._1))
+
+  implicit val arbMagnitudeBand: Arbitrary[Band] =
+    Arbitrary(oneOf(Band.values))
+
+  implicit val arbMagnitudeSystem: Arbitrary[System] =
+    Arbitrary(oneOf(System.values))
+
+  implicit val arbMagnitude: Arbitrary[Magnitude] =
+    Arbitrary {
+      for {
+        value  <- arbitrary[Double]
+        band   <- arbitrary[Band]
+        system <- arbitrary[System]
+      } yield new Magnitude(band, value, system)
+    }
+
+  implicit val arbGuideProbe: Arbitrary[GuideProbe] =
+    Arbitrary(oneOf(GuideProbe.values))
+
+}
+
+
