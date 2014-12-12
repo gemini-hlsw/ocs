b9fa35092e63e5adca91f04ddf1064e184d5e487
Sebastian Raaphorst <sraaphor@gemini.edu>
2014-12-12 17:12:33 -0300

    OCSADV-196: Fixed several bugs in ImPolygon and made changes as recommended by others.

--- bundle/edu.gemini.shared.util/src/main/scala/edu/gemini/shared/util/immutable/ImPolygon.scala
+++ bundle/edu.gemini.shared.util/src/main/scala/edu/gemini/shared/util/immutable/ImPolygon.scala
@@ -3,86 +3,89 @@ package edu.gemini.shared.util.immutable
 import java.awt.{Rectangle, Polygon, Shape}
 import java.awt.geom._
 
-
 /**
  * An immutable class representing a polygon as a collection of points.
- * Note that building the polygon point-by-point is possible through the addPoint method, but is not
- * efficient, and instead, the collection of points should be passed as a constructor argument.
- * @param points the points, in order, representing the vertices of the polygon, which is assumed to be closed
- *               and thus does not require the first point to also be the last.
+ * @param points the points, in reverse order, representing the vertices of the polygon, which is assumed to be closed
+ *               and thus does not require the first point to also be the last. The order is reversed to make calls to
+ *               addPoint more efficient, so that points can be added to the list via a cons operation instead of
+ *               an append.
  */
-case class ImPolygon(points: List[(Double,Double)] = Nil) extends Shape {
-  // Calculate the path.
-  private val closedPath = {
-    val pathOption = points.headOption.map {
-      case(x,y) =>
-        val path = new GeneralPath()
-        path.moveTo(x,y)
-        path
+sealed class ImPolygon private (points: List[(Double,Double)] = Nil) extends Shape {
+  private lazy val closedPath = {
+    // We need to move to the rightmost point, which happens when the Path is empty (i.e. the current point of the
+    // Path is null).
+    val path = points.foldRight {
+      val initialPath = new Path2D.Double
+      if (points.isEmpty)
+        initialPath.moveTo(0,0)
+      initialPath
+    }{ case ((x,y), p) =>
+        if (p.getCurrentPoint == null) p.moveTo(x,y)
+        else p.lineTo(x,y)
+        p
     }
 
-    pathOption.foreach { path => {
-      points.drop(1).foreach { case (x,y) => path.lineTo(x,y) } }
-      path.closePath()
-    }
-    pathOption
+    path.closePath()
+    path
   }
 
   // Calculate the bounds.
-  private val bounds = closedPath.getOrElse(new GeneralPath()).getBounds
+  private lazy val bounds = closedPath.getBounds2D
 
+  // Return a copy (for immutability) of the bounds, which is created by getBounds without requiring typecasting.
+  // Note that a Rectangle has integer values, so this may not tightly bound the polygon.
   override def getBounds: Rectangle =
-    bounds
+    bounds.getBounds
 
+  // Return a new object representing the bounds as a Rectangle2D: getBounds2D instantiates a new Rectangle2D.
   override def getBounds2D: Rectangle2D =
     bounds.getBounds2D
 
   override def getPathIterator(at: AffineTransform): PathIterator =
-    closedPath.map(_.getPathIterator(at)).orNull
+    closedPath.getPathIterator(at)
 
   // Flatness is ignored because a polygon is already flat.
   override def getPathIterator(at: AffineTransform, flatness: Double): PathIterator =
     getPathIterator(at)
 
   override def contains(x: Double, y: Double): Boolean =
-    !((points.length <= 2 || !bounds.contains(x, y)) || !closedPath.forall(_.contains(x, y)))
+    !((points.length <= 2 || !bounds.contains(x, y)) || !closedPath.contains(x, y))
 
   override def contains(p: Point2D): Boolean =
     contains(p.getX, p.getY)
 
   override def contains(x: Double, y: Double, w: Double, h: Double): Boolean =
-    !((points.isEmpty || !bounds.contains(x, y)) || !closedPath.forall(_.contains(x, y, w, h)))
+    !((points.isEmpty || !bounds.contains(x, y)) || !closedPath.contains(x, y, w, h))
 
   override def contains(r: Rectangle2D): Boolean =
     contains(r.getX, r.getY, r.getWidth, r.getHeight)
 
   override def intersects(x: Double, y: Double, w: Double, h: Double): Boolean =
-    !((points.isEmpty || !bounds.contains(x, y)) || !closedPath.forall(_.intersects(x, y, w, h)))
+    !((points.isEmpty || !bounds.intersects(x, y, w, h)) || !closedPath.intersects(x, y, w, h))
 
   override def intersects(r: Rectangle2D): Boolean =
     intersects(r.getX, r.getY, r.getWidth, r.getHeight)
 
   def addPoint(x: Double, y: Double): ImPolygon =
-    new ImPolygon(points ++ List((x,y)))
+    new ImPolygon((x,y) :: points)
 
   def addPoint(p: Point2D): ImPolygon =
     addPoint(p.getX, p.getY)
 }
 
 object ImPolygon {
+  def apply() =
+    new ImPolygon(Nil)
+
+  def apply(points: List[(Double,Double)]) =
+    new ImPolygon(points.reverse)
+
   def apply(rect: Rectangle2D): ImPolygon = {
-    val points = for {
-      x <- List(rect.getMinX, rect.getMaxX)
-      y <- List(rect.getMinY, rect.getMaxY)
-    } yield (x, y)
-    new ImPolygon(points)
+    val (minx, maxx) = (rect.getMinX, rect.getMaxX)
+    val (miny, maxy) = (rect.getMinY, rect.getMaxY)
+    ImPolygon(List((minx,miny),(minx,maxy),(maxx,maxy),(maxx,miny)))
   }
 
-  def apply(pol: Polygon): ImPolygon = {
-    val points = for {
-      x <- pol.xpoints.map(_.toDouble)
-      y <- pol.ypoints.map(_.toDouble)
-    } yield (x, y)
-    new ImPolygon(points.toList)
-  }
+  def apply(pol: Polygon): ImPolygon =
+    ImPolygon((pol.xpoints.map(_.toDouble), pol.ypoints.map(_.toDouble)).zipped.toList)
 }
