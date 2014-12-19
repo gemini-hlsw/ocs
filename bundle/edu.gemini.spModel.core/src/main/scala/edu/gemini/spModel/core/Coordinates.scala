429519726a628818264ff1f4478e71905ac0cdc2
Carlos Quiroz <cquiroz@gemini.edu>
2014-12-19 09:10:52 -0300

    OCSADV-27 Rewrite of CoordinatesDiff in Scala, adding a new class and method to Coordinates and port of existing tests #time 2h

--- bundle/edu.gemini.spModel.core/src/main/scala/edu/gemini/spModel/core/Coordinates.scala
+++ bundle/edu.gemini.spModel.core/src/main/scala/edu/gemini/spModel/core/Coordinates.scala
@@ -1,5 +1,6 @@
 package edu.gemini.spModel.core
 
+import scala.math._
 import scalaz._, Scalaz._
 
 /** A point in the sky, given right ascension and declination. */
@@ -41,6 +42,62 @@ object Coordinates extends ((RightAscension, Declination) => Coordinates) {
   implicit val CoordinatesEqual: Equal[Coordinates] =
     Equal.equalA
 
+  case class Difference(posAngle: Angle, distance: Angle) {
+    /**
+     * Gets the offset coordinates relative to the base position.
+     */
+    def offset: Offset = {
+      val phi = posAngle.toRadians
+      val h = distance.toDegrees
+      // Position angle is east of north, or relative to 90 degrees.
+      // Swapping sin and cos here to compensate.
+      val p = Angle.fromDegrees(h * sin(phi))
+      val q = Angle.fromDegrees(h * cos(phi))
+      Offset(p, q)
+    }
+
+    override def toString =
+      f"distance ${distance.toDegrees * 3600} arcsec, ${posAngle.toDegrees} deg E of N"
+  }
+
+  def difference(base: Coordinates, point: Coordinates): Difference = {
+    val radian: Double = 180.0 / Math.PI
+    val limitDistance = 0.0000004
+
+    // coordinates transformed to radians
+    val alf = point.ra.toAngle.toRadians
+    val alf0 = base.ra.toAngle.toRadians
+    val del = point.dec.toAngle.toRadians
+    val del0 = base.dec.toAngle.toRadians
+
+    val sd0 = Math.sin(del0)
+    val sd = Math.sin(del)
+    val cd0 = Math.cos(del0)
+    val cd = Math.cos(del)
+    val cosda = Math.cos(alf - alf0)
+    val cosd = sd0 * sd + cd0 * cd * cosda
+    val dist = {
+      val acos = Math.acos(cosd)
+      if (acos.isNaN) 0.0 else acos
+    }
+
+    val phi = if (dist > limitDistance) {
+        val sind = Math.sin(dist)
+        val pcospa = (sd * cd0 - cd * sd0 * cosda) / sind
+        val cospa = {
+          val abs = Math.abs(pcospa)
+          if (abs > 1.0) pcospa / abs else pcospa
+        }
+        val sinpa = cd * Math.sin(alf - alf0) / sind
+        val pphi = Math.acos(cospa)
+
+        if (sinpa < 0.0) (Math.PI * 2) - pphi else pphi
+      } else {
+        0
+      }
+
+    Difference(Angle.fromDegrees(phi * radian), Angle.fromDegrees(dist * radian))
+  }
 }
 
 
