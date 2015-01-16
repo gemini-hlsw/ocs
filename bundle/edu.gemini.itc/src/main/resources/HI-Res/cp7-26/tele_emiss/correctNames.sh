92edb9ed17f79fe75e5a699d4c1f3ba8d1d48e3e
Florian Nussberger <florian.nussberger@gmail.com>
2015-01-16 10:46:09 -1000

    Remove seemingly unused dat files (70!).

--- bundle/edu.gemini.itc/src/main/resources/HI-Res/cp7-26/tele_emiss/correctNames.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mv -v 2ag_nq280_nm.dat telEmiss_2ag.dat
-mv -v 2al+1ag_nq280_nm.dat telEmiss_2al+1ag.dat
-mv -v 2al_nq280_nm.dat telEmiss_2al.dat
-mv -v 3ag_nq280_nm.dat telEmiss_3ag.dat
-
--- bundle/edu.gemini.itc/src/main/resources/HI-Res/mk7-26/tele_emiss/correctNames.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mv -v 2ag_nq273_nm.dat telEmiss_2ag.dat
-mv -v 2al+1ag_nq273_nm.dat telEmiss_2al+1ag.dat
-mv -v 2al_nq273_nm.dat telEmiss_2al.dat
-mv -v 3ag_nq273_nm.dat telEmiss_3ag.dat
-
