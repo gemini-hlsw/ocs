ec6f3454478e8b8b51bc81c2594ec2e38435a421
Sebastian Raaphorst <sraaphor@gemini.edu>
2015-01-16 15:22:06 -0300

    REL-2162: Modified P2 and QPT code to not report errors for missing ODGW and Canopus stars.

--- bundle/edu.gemini.qpt.client/src/main/scala/edu/gemini/qpt/core/listeners/AgsAnalysisListener.scala
+++ bundle/edu.gemini.qpt.client/src/main/scala/edu/gemini/qpt/core/listeners/AgsAnalysisListener.scala
@@ -8,6 +8,9 @@ import edu.gemini.ags.api.AgsAnalysis
 import edu.gemini.qpt.core.Marker.Severity
 import edu.gemini.qpt.core.Variant
 import edu.gemini.qpt.core.util.MarkerManager
+import edu.gemini.spModel.gemini.gems.Canopus
+import edu.gemini.spModel.gemini.gsaoi.GsaoiOdgw
+import edu.gemini.spModel.guide.GuideProbeGroup
 
 import scala.collection.JavaConverters._
 
@@ -23,9 +26,13 @@ class AgsAnalysisListener extends MarkerModelListener[Variant] {
     // Iterate over the observations in the variant and determine if they should generate
     // markers based on the AgsAnalysis.
     for (alloc <- variant.getAllocs.asScala if !alloc.getObs.getAgsAnalysis.isEmpty) {
-      // Only analyses with a severity level should generate a marker.
+      // Only analyses with a severity level that are not ODGW and Canopus should generate a marker.
       for {
         a <- alloc.getObs.getAgsAnalysis.asScala
+        if (a match {
+          case AgsAnalysis.NoGuideStarForGroup(group) => !ignoredProbeGroups.contains(group)
+          case _ => true
+        })
         s <- severity(a)
       } yield {
         markerManager.addMarker(false, this, s, a.message(withProbe = true), variant, alloc)
@@ -39,6 +46,9 @@ class AgsAnalysisListener extends MarkerModelListener[Variant] {
 }
 
 object AgsAnalysisListener {
+  // We want to ignore AgsAnalysis problems for ODGW and Canopus.
+  val ignoredProbeGroups: Set[GuideProbeGroup] = Set(GsaoiOdgw.Group.instance, Canopus.Wfs.Group.instance)
+
   def severity(a: AgsAnalysis): Option[Severity] =
     a.quality match {
       case DeliversRequestedIq   => None
