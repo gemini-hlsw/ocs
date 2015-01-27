7e6d60f2e65f7c9b7a08aef873737dda2312fb57
Florian Nussberger <florian.nussberger@gmail.com>
2015-01-27 11:51:11 -1000

    Turn GnirsOrderSelector into singleton, load dat file once only.

--- bundle/edu.gemini.itc/src/main/java/edu/gemini/itc/gnirs/GnirsOrderSelector.java
+++ bundle/edu.gemini.itc/src/main/java/edu/gemini/itc/gnirs/GnirsOrderSelector.java
@@ -7,23 +7,19 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Scanner;
 
-public class GnirsOrderSelector {
+public final class GnirsOrderSelector {
 
-    private final String orderFilename = "orders";
-    private final List<Integer> _order_list;      //list of the order numbers
-    private final List<Integer> _lambda1_list;    //start wavelenght for that order
-    private final List<Integer> _lambda2_list;    //end wavelength fot that order
+    private static final String orderFilename = "orders";
+    private static final List<Integer> _order_list;      //list of the order numbers
+    private static final List<Integer> _lambda1_list;    //start wavelenght for that order
+    private static final List<Integer> _lambda2_list;    //end wavelength fot that order
 
-    /**
-     * Creates a new instance of GnirsOrderSelector
-     */
-    public GnirsOrderSelector(String directory, String prefix, String suffix) throws Exception {
-        final String file = directory + "/" + prefix + orderFilename + suffix;
+    static {
+        _order_list = new ArrayList<>();
+        _lambda1_list = new ArrayList<>();
+        _lambda2_list = new ArrayList<>();
+        final String file = "/" + Gnirs.INSTR_DIR + "/" + Gnirs.INSTR_PREFIX + orderFilename + Gnirs.DATA_SUFFIX;
         try (final Scanner scan = DatFile.scan(file)) {
-            _order_list = new ArrayList<>();
-            _lambda1_list = new ArrayList<>();
-            _lambda2_list = new ArrayList<>();
-
             while (scan.hasNext()) {
                 _order_list.add(scan.nextInt());
                 _lambda1_list.add(scan.nextInt());
@@ -32,34 +28,31 @@ public class GnirsOrderSelector {
         }
     }
 
-    private int findOrder(double centralWavelength) throws Exception {
+    private GnirsOrderSelector() {}
 
-        int centralWavelengthInt = (new Double(centralWavelength)).intValue();
-        ListIterator lambdaStart = _lambda1_list.listIterator();
-        ListIterator lambdaEnd = _lambda2_list.listIterator();
-        ListIterator order = _order_list.listIterator();
+    public static int getOrder(final double centralWavelength) {
+
+        final ListIterator<Integer> lambdaStart = _lambda1_list.listIterator();
+        final ListIterator<Integer> lambdaEnd = _lambda2_list.listIterator();
+        final ListIterator<Integer> order = _order_list.listIterator();
 
         int selectedOrder = -1;
 
         while (lambdaStart.hasNext() && lambdaEnd.hasNext()) {
-            int wavelenStart = ((Integer) lambdaStart.next()).intValue();
-            int wavelenEnd = ((Integer) lambdaEnd.next()).intValue();
+            final int wavelenStart = lambdaStart.next();
+            final int wavelenEnd = lambdaEnd.next();
             if (centralWavelength >= wavelenStart && centralWavelength <= wavelenEnd) {
-                selectedOrder = ((Integer) order.next()).intValue();
+                selectedOrder = order.next();
             } else {
                 order.next();
             }
         }
 
         if (selectedOrder == -1) {
-            throw new Exception("Order could not be found for centralWavelength" + centralWavelength);
+            throw new IllegalArgumentException("Order could not be found for centralWavelength" + centralWavelength);
         }
 
         return selectedOrder;
-    }
-
-    int getOrder(double centralWavelength) throws Exception {
-        return findOrder(centralWavelength);
     }
 
 }
