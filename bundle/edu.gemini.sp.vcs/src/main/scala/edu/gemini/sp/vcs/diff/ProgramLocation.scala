4f22526698c78328cc46ea519ea1878925f01b60
swalker2m <swalker2m@mac.com>
2015-03-04 13:15:05 -0300

    OCSADV-200: add ProgramLocation.fold; use it

--- bundle/edu.gemini.sp.vcs/src/main/scala/edu/gemini/sp/vcs/diff/ProgramLocation.scala
+++ bundle/edu.gemini.sp.vcs/src/main/scala/edu/gemini/sp/vcs/diff/ProgramLocation.scala
@@ -2,38 +2,37 @@ package edu.gemini.sp.vcs.diff
 
 import scalaz._
 
-sealed trait ProgramLocation
+sealed trait ProgramLocation {
+  def fold[A](loc: => A, rem: => A): A
+}
 
 sealed trait ProgramLocationSet {
   def +(loc: ProgramLocation): ProgramLocationSet
 }
 
 object ProgramLocation {
-  case object Local  extends ProgramLocation
-  case object Remote extends ProgramLocation
+  case object Local  extends ProgramLocation {
+    def fold[A](loc: => A, rem: => A): A = loc
+  }
+
+  case object Remote extends ProgramLocation {
+    def fold[A](loc: => A, rem: => A): A = rem
+  }
 
   implicit def ProgramLocationEqual: Equal[ProgramLocation] = Equal.equalA
 
   case object Neither    extends ProgramLocationSet {
-    def +(loc: ProgramLocation): ProgramLocationSet = loc match {
-      case Local  => LocalOnly
-      case Remote => RemoteOnly
-    }
-
+    def +(loc: ProgramLocation): ProgramLocationSet = loc.fold(LocalOnly, RemoteOnly)
   }
+
   case object LocalOnly  extends ProgramLocationSet {
-    def +(loc: ProgramLocation): ProgramLocationSet = loc match {
-      case Remote => Both
-      case _      => this
-    }
+    def +(loc: ProgramLocation): ProgramLocationSet = loc.fold(this, Both)
   }
 
   case object RemoteOnly extends ProgramLocationSet {
-    def +(loc: ProgramLocation): ProgramLocationSet = loc match {
-      case Local => Both
-      case _     => this
-    }
+    def +(loc: ProgramLocation): ProgramLocationSet = loc.fold(Both, this)
   }
+
   case object Both       extends ProgramLocationSet {
     def +(loc: ProgramLocation): ProgramLocationSet = this
   }
