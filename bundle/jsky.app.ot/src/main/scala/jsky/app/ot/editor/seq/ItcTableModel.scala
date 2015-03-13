35e60ca82dbfba0b5d7ded06a36dc2009fbeb327
Florian Nussberger <florian.nussberger@gmail.com>
2015-03-13 10:54:11 -1000

    OCSADV-248,OCSADV-250: Additional ITC tabs showing the "unique" configurations.

---
+++ bundle/jsky.app.ot/src/main/scala/jsky/app/ot/editor/seq/ItcTableModel.scala
@@ -0,0 +1,101 @@
+package jsky.app.ot.editor.seq
+
+import javax.swing.table.AbstractTableModel
+
+import edu.gemini.shared.util.StringUtil
+import edu.gemini.spModel.config2.ItemKey
+
+/** Columns in the table are defined by their header label and a function on the unique config of the row. */
+case class Column(label: String, value: UniqueConfig => Object)
+
+object ItcTableModel {
+  /** Defines a set of header columns for all tables. */
+  val headers = Seq(
+    Column("Data Labels",     c => c.labels),
+    Column("Images",          c => new java.lang.Integer(c.count)),             // must be an object for JTable
+    Column("Exposure Time",   c => new java.lang.Double(c.singleExposureTime)), // must be an object for JTable
+    Column("Total Exp. Time", c => new java.lang.Double(c.totalExposureTime))   // must be an object for JTable
+  )
+}
+
+/**
+ * ITC tables have three types of columns: a series of header columns, then all the values that change and are
+ * relevant for the different unique configs (denoted by their ItemKey values) and finally the ITC calculation
+ * results.
+ */
+sealed trait ItcTableModel extends AbstractTableModel {
+
+  val headers: Seq[Column]
+  val keys:    Seq[ItemKey]
+  val results: Seq[Column]
+  val uniqueSteps: Seq[UniqueConfig]
+
+  def getRowCount: Int = uniqueSteps.size
+
+  def getColumnCount: Int = headers.size + keys.size + results.size
+
+  def getKeyAt(col: Int): Option[ItemKey] = col match {
+    case c if c >= headers.size && c < headers.size + keys.size => Some(key(col))
+    case _                                                      => None
+  }
+
+  def getValueAt(row: Int, col: Int): Object = col match {
+    case c if c <  headers.size               => header(col).value(uniqueSteps(row))
+    case c if c >= headers.size + keys.size   => result(col).value(uniqueSteps(row))
+    case c                                    => uniqueSteps(row).config.getItemValue(key(col))
+  }
+
+  override def getColumnName(col: Int): String = col match {
+    case c if c <  headers.size               => header(col).label
+    case c if c >= headers.size + keys.size   => result(col).label
+    case c                                    => StringUtil.toDisplayName(key(col).getName)
+  }
+
+  // Translate overall column index into the corresponding header, column or key value.
+  private def header(col: Int) = headers(col)
+  private def key   (col: Int) = keys   (col - headers.size)
+  private def result(col: Int) = results(col - headers.size - keys.size)
+
+ }
+
+/** Generic ITC imaging tables model. */
+sealed trait ItcImagingTableModel extends ItcTableModel
+
+class ItcGenericImagingTableModel(val keys: Seq[ItemKey], val uniqueSteps: Seq[UniqueConfig]) extends ItcImagingTableModel {
+  val headers = ItcTableModel.headers
+  val results = Seq(
+    Column("PPF",             c => ""),
+    Column("S/N Single",      c => ""),
+    Column("S/N Total",       c => ""),
+    Column("Messages",        c => "OK")
+  )
+}
+
+/** GMOS specific ITC imaging table model. */
+class ItcGmosImagingTableModel(val keys: Seq[ItemKey], val uniqueSteps: Seq[UniqueConfig]) extends ItcImagingTableModel {
+  val headers = ItcTableModel.headers
+  val results = Seq(
+    Column("CCD1 PPF",        c => ""),
+    Column("CCD1 S/N Single", c => ""),
+    Column("CCD1 S/N Total",  c => ""),
+    Column("CCD2 PPF",        c => ""),
+    Column("CCD2 S/N Single", c => ""),
+    Column("CCD2 S/N Total",  c => ""),
+    Column("CCD3 PPF",        c => ""),
+    Column("CCD3 S/N Single", c => ""),
+    Column("CCD3 S/N Total",  c => ""),
+    Column("Messages",        c => "OK")
+  )
+}
+
+
+/** Generic ITC spectroscopy table model. */
+sealed trait ItcSpectroscopyTableModel extends ItcTableModel
+
+class ItcGenericSpectroscopyTableModel(val keys: Seq[ItemKey], val uniqueSteps: Seq[UniqueConfig]) extends ItcSpectroscopyTableModel {
+  val headers = ItcTableModel.headers
+  val results = Seq(
+    Column("Messages",        c => "OK")
+  )
+}
+
---
+++ bundle/jsky.app.ot/src/main/scala/jsky/app/ot/editor/seq/UniqueConfig.scala
@@ -0,0 +1,112 @@
+package jsky.app.ot.editor.seq
+
+import edu.gemini.spModel.config2.{Config, ConfigSequence}
+import edu.gemini.spModel.gemini.acqcam.InstAcqCam
+import edu.gemini.spModel.gemini.flamingos2.Flamingos2
+import edu.gemini.spModel.gemini.gmos.{GmosNorthType, GmosSouthType, InstGmosNorth, InstGmosSouth}
+import edu.gemini.spModel.gemini.gnirs.GNIRSConstants
+import edu.gemini.spModel.gemini.gsaoi.Gsaoi
+import edu.gemini.spModel.gemini.michelle.{InstMichelle, MichelleParams}
+import edu.gemini.spModel.gemini.nifs.InstNIFS
+import edu.gemini.spModel.gemini.niri.{InstNIRI, Niri}
+import edu.gemini.spModel.gemini.trecs.{InstTReCS, TReCSParams}
+import jsky.app.ot.editor.seq.Keys._
+
+
+/**
+ * Unique configurations represent sets of observes in a sequence which are done with the same
+ * instrument configuration and exposure times.
+ */
+case class UniqueConfig(count: Int, labels: String, config: Config) {
+
+  /** Single exposure time is either the given exposure time or the time on source divided by the number of images. */
+  def singleExposureTime: Double = singleTime.getOrElse(totalTime.getOrElse(0.0) / count)
+
+  /** Total exposure time is either total time on source (TReCS/Michelle) or the single exposure time times the number of images. */
+  def totalExposureTime: Double = totalTime.getOrElse(singleTime.getOrElse(0.0) * count)
+
+  /** TReCS and Michelle have an optional total time on source. */
+  private val totalTime = Option(config.getItemValue(INST_TIME_ON_SRC_KEY)).map(_.toString.toDouble)
+
+  /** Instruments other than TReCS and Michelle have a defined exposure time per image. */
+  private val singleTime = Option(config.getItemValue(INST_EXP_TIME_KEY)).map(_.toString.toDouble)
+
+}
+
+/**
+ * Utility functions to get unique configurations from ConfigurationSequence objects.
+ */
+object UniqueConfig {
+
+  /** Gets all unique science imaging configurations from the given sequence. */
+  def imagingConfigs(seq: ConfigSequence): Seq[UniqueConfig] =
+    uniqueConfigs(seq, c => isScience(c) && isImaging(c))
+
+  /** Gets all unique spectroscopy configurations from the given sequence. */
+  def spectroscopyConfigs(seq: ConfigSequence): Seq[UniqueConfig] =
+    uniqueConfigs(seq, c => isScience(c) && isSpectroscopy(c))
+
+  // Checks if a config is science or not; only science observations are relevant for ITC
+  private def isScience(c: Config): Boolean =
+    Option(c.getItemValue(OBS_TYPE_KEY)).fold(false)(!_.equals("CAL"))
+
+  // Decides if a configuration is for spectroscopy or not.
+  private def isSpectroscopy(c: Config): Boolean = !isImaging(c)
+
+  // Decides if a configuration is for imaging or spectroscopy based on the presence of a disperser element.
+  private def isImaging(c: Config): Boolean = c.getItemValue(INST_INSTRUMENT_KEY) match {
+    case InstAcqCam.INSTRUMENT_NAME_PROP    => true  // Acq cam is imaging only
+    case Flamingos2.INSTRUMENT_NAME_PROP    => c.getItemValue(INST_DISPERSER_KEY).equals(Flamingos2.Disperser.NONE)
+    case InstGmosNorth.INSTRUMENT_NAME_PROP => c.getItemValue(INST_DISPERSER_KEY).equals(GmosNorthType.DisperserNorth.MIRROR)
+    case InstGmosSouth.INSTRUMENT_NAME_PROP => c.getItemValue(INST_DISPERSER_KEY).equals(GmosSouthType.DisperserSouth.MIRROR)
+    case GNIRSConstants.INSTRUMENT_NAME_PROP=> false // GNIRS is spectroscopy only
+    case Gsaoi.INSTRUMENT_NAME_PROP         => true  // Gsaoi is imaging only
+    case InstMichelle.INSTRUMENT_NAME_PROP  => c.getItemValue(INST_DISPERSER_KEY).equals(MichelleParams.Disperser.MIRROR)
+    case InstNIFS.INSTRUMENT_NAME_PROP      => false // NIFS is spectroscopy only
+    case InstNIRI.INSTRUMENT_NAME_PROP      => c.getItemValue(INST_DISPERSER_KEY).equals(Niri.Disperser.NONE)
+    case InstTReCS.INSTRUMENT_NAME_PROP     => c.getItemValue(INST_DISPERSER_KEY).equals(TReCSParams.Disperser.MIRROR)
+  }
+
+  // Gets all "unique configs" (i.e. configs that are relevant for ITC) from the given sequence. The predicate
+  // defines which steps have to be taken into account, i.e. spectroscopy vs imaging and no calibrations.
+  private def uniqueConfigs(seq: ConfigSequence, predicate: Config => Boolean): Seq[UniqueConfig] = {
+    val steps        = seq.getAllSteps.toSeq.filter(predicate)
+    val groupedSteps = steps.groupBy(hash).toSeq
+    val mappedSteps  = groupedSteps.map{case (h, cs) => UniqueConfig(cs.size, labels(cs), cs.head)}.toSeq
+    mappedSteps.sortBy(_.config.getItemValue(DATALABEL_KEY).toString)
+  }
+
+  // Creates a hash for the given config step taking into account only the values that are relevant for ITC
+  // calculations, e.g. ignoring offsets and data labels.
+  private def hash(step: Config): Int = step.getKeys.
+      filterNot(_.getParent.equals(CALIBRATION_KEY)).
+      filterNot(_.equals(DATALABEL_KEY)).           // don't take data label into account (step number)
+      filterNot(_.equals(TEL_P_KEY)).               // configs with different P offsets are considered as one config
+      filterNot(_.equals(TEL_Q_KEY)).               // dito for Q
+      filterNot(step.getItemValue(_) == null).      // occasionally we get null values..
+      map(step.getItemValue(_).hashCode()).
+      foldLeft(17)((acc, h) => 37*acc + h)
+
+  // Creates a text label based on the spans of steps covered by the steps.
+  // E.g. ((1,2,3),(10,11),(15)) is turned into "001-003, 010-011, 015"
+  private def labels(cs: Seq[Config]): String = {
+    val ls = cs.map(_.getItemValue(DATALABEL_KEY).toString.toInt).sorted
+    findSpans(ls).map {
+      case i :: Nil => i.toString
+      case i :: is => f"$i%03d-${is.last}%03d"
+    }.mkString(", ")
+  }
+
+  // Finds spans of int values that immediately follow each other.
+  // E.g. (1,2,3,10,11,15) is turned into ((1,2,3),(10,11),(15)).
+  // Note, this is done backwards, so we can always prepend the newest element.
+  private def findSpans(ns: Seq[Int]): Seq[Seq[Int]] = {
+    val e: Seq[Seq[Int]] = Seq()
+    ns.reverse.foldLeft(e) {
+      case (a, i) if a.isEmpty           => Seq(Seq(i))
+      case (a, i) if a.head.head == i+1  => (i +: a.head) +: a.tail
+      case (a, i)                        => Seq(i) +: a
+    }
+  }
+
+}
