9528d1cceb987b58a68e25867ceda77fcddb5e1e
Carlos Quiroz <cquiroz@gemini.edu>
2015-04-07 18:56:34 -0300

    OCSADV-272 Remove duplicated class #time 10m

--- bundle/edu.gemini.ags/src/main/scala/edu/gemini/ags/conf/MagLimitsId.scala
+++ bundle/edu.gemini.ags/src/main/scala/edu/gemini/ags/conf/MagLimitsId.scala
@@ -5,6 +5,6 @@ import edu.gemini.spModel.guide.GuideSpeed
 
 final case class MagLimitsId(name: String)
 
-case class FaintnessKey(iq: ImageQuality, sb: SkyBackground, gs: GuideSpeed) {
+final case class FaintnessKey(iq: ImageQuality, sb: SkyBackground, gs: GuideSpeed) {
   override def toString: String = s"($iq, $sb, $gs)"
 }
