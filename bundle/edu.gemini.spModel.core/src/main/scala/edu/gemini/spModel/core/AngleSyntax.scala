0b11d621549f2c09da1e18a5ec48cd9a833253e1
swalker2m <swalker2m@mac.com>
2015-04-09 14:42:40 -0300

    OCSADV-273: incorporate cleanup based on feedback

--- bundle/edu.gemini.spModel.core/src/main/scala/edu/gemini/spModel/core/AngleSyntax.scala
+++ bundle/edu.gemini.spModel.core/src/main/scala/edu/gemini/spModel/core/AngleSyntax.scala
@@ -4,16 +4,16 @@ package edu.gemini.spModel.core
  * Syntax for working with angle-like types.
  */
 object AngleSyntax {
-  implicit class ToAngleOps[A](value: A) {
-    def angle(implicit ta: ToDegrees[A]): Angle    = ta.toAngle(value)
-    def arcsecs(implicit ta: ToDegrees[A]): Double = ta.toArcsecs(value)
-    def arcmins(implicit ta: ToDegrees[A]): Double = ta.toArcmins(value)
-    def degrees(implicit ta: ToDegrees[A]): Double = ta.toDegrees(value)
+  implicit class ToDegreesOps[A](value: A)(implicit td: ToDegrees[A]) {
+    def angle: Angle    = td.toAngle(value)
+    def arcsecs: Double = td.toArcsecs(value)
+    def arcmins: Double = td.toArcmins(value)
+    def degrees: Double = td.toDegrees(value)
 
     // ....
   }
 
-  implicit class FromAngleOps(value: Double) {
+  implicit class FromDegreesOps(value: Double) {
     def arcsecs[A](implicit fa: FromDegrees[A]): A = fa.fromArcsecs(value)
     def arcmins[A](implicit fa: FromDegrees[A]): A = fa.fromArcmins(value)
     def degrees[A](implicit fa: FromDegrees[A]): A = fa.fromDegrees(value)
@@ -21,18 +21,11 @@ object AngleSyntax {
     // ....
   }
 
-  implicit class IsoAngleOps[A](value: A) {
-    def +(that: A)(implicit ia: IsoAngle[A]): A =
-      (value.degrees + that.degrees).degrees
-
-    def -(that: A)(implicit ia: IsoAngle[A]): A =
-      (value.degrees - that.degrees).degrees
-
-    def *(factor: Double)(implicit ia: IsoAngle[A]): A =
-      (value.degrees * factor).degrees
-
-    def flip(implicit ia: IsoAngle[A]): A =
-      value + 180.degrees[A]
+  implicit class IsoAngleOps[A](value: A)(implicit ia: IsoAngle[A]) {
+    def +(that: A): A        = ia.add(value, that)
+    def -(that: A): A        = ia.subtract(value, that)
+    def *(factor: Double): A = ia.multiply(value, factor)
+    def flip: A              = ia.flip(value)
 
     // ....
   }
---
+++ bundle/edu.gemini.spModel.core/src/main/scala/edu/gemini/spModel/core/OffsetP.scala
@@ -0,0 +1,31 @@
+package edu.gemini.spModel.core
+
+import scalaz.{Order, Monoid, Show}
+
+/**
+ * Offset in P.
+ */
+case class OffsetP(toAngle: Angle) extends AnyVal
+
+object OffsetP {
+  val Zero = OffsetP(Angle.zero)
+
+  implicit val IsoAngleP = new IsoAngle[OffsetP] {
+    override def toDegrees(p: OffsetP): Double   = Angle.signedDegrees(p.toAngle.toDegrees)
+    override def fromDegrees(d: Double): OffsetP = OffsetP(Angle.fromDegrees(d))
+  }
+
+  import AngleSyntax._
+
+  implicit val ShowP: Show[OffsetP] =
+    Show.shows(p => f"${p.arcsecs}%4.03f arcsecs")
+
+  implicit val MonoidP: Monoid[OffsetP] =
+    new Monoid[OffsetP] {
+      val zero = Zero
+      def append(a: OffsetP, b: => OffsetP): OffsetP = IsoAngleP.add(a, b)
+    }
+
+  implicit val OrderP: Order[OffsetP] =
+    Order.orderBy(_.degrees)
+}
