0278bdb48f567e6451e7ce82e63ac71f54db7754
Carlos Quiroz <cquiroz@gemini.edu>
2015-04-16 11:54:18 -0300

    OCSADV-293 First version of the votable proxy server that can export name/ra/dec #time 3h

---
+++ bundle/edu.gemini.catalog/src/main/scala/edu/gemini/catalog/skycat/binding/skyobj/Votable2SkyCatalogServlet.scala
@@ -0,0 +1,65 @@
+package edu.gemini.catalog.skycat.binding.skyobj
+
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse, HttpServlet}
+import edu.gemini.catalog.api.{CatalogQuery, RadiusConstraint}
+import edu.gemini.catalog.votable.VoTableClient
+import edu.gemini.spModel.core.Target.SiderealTarget
+import edu.gemini.spModel.core.{Declination, Angle, RightAscension, Coordinates}
+
+import scala.collection.JavaConverters._
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import scalaz._
+import Scalaz._
+
+@Deprecated
+class Votable2SkyCatalogServlet extends HttpServlet {
+  private def toRow(t: SiderealTarget):String = f"${t.name}%-10s\t${t.coordinates.ra.toAngle.toDegrees}%+03.07f\t${t.coordinates.dec.toDegrees}%+03.07f"
+  private def headers:String = s"4UC\tRA\tDEC\n----------\t-----------\t-----------"
+
+  override protected def doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+
+    val params:Map[String, String] = req.getParameterMap.asScala.map(t => t._1.toString -> t._2.asInstanceOf[Array[String]](0)).toMap
+    val ra = params.get("ra")
+    val dec = params.get("dec")
+    val r1 = params.get("r1")
+    val r2 = params.get("r2")
+    val out = (ra |@| dec |@| r1 |@| r2){(r, d, ra1, ra2) =>
+      for {
+        raAngle      <- Angle.parseHMS(r)
+        decAngle     <- Angle.parseDMS(d)
+        innerRadius  <- ra1.parseDouble.disjunction
+        outterRadius <- ra2.parseDouble.disjunction
+      } yield {
+        val coordinates = Coordinates(RightAscension.fromAngle(raAngle), Declination.fromAngle(decAngle).getOrElse(Declination.zero))
+
+        val rc = RadiusConstraint.between(Angle.fromArcmin(innerRadius), Angle.fromArcmin(outterRadius))
+
+        val query = CatalogQuery.catalogQuery(coordinates, rc, None)
+
+        val result = VoTableClient.catalog(query).map { q =>
+          if (q.result.containsError) {
+            q.result.problems.mkString(", ")
+          } else {
+            s"$headers\n${q.result.targets.rows.map(toRow).mkString("\n")}"
+          }
+        }
+
+        // Servlet is synchronous, we need to Await
+        Await.result(result, 30.seconds)
+      }
+    }
+    out.fold(resp.setStatus(400)){
+      _.fold(
+        _ => resp.setStatus(404),
+        v => {
+          resp.setContentType("text/plain")
+          resp.setStatus(200)
+          resp.getWriter.print(v)
+        }
+      )
+    }
+  }
+}
