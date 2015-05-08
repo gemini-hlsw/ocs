4dbfc7daf42aabee4df441f2123d22399b9ddba3
Carlos Quiroz <cquiroz@gemini.edu>
2015-05-08 10:57:45 -0300

    OCSADV-272 Remove filters for magnitudes R and UC #time 5m

--- bundle/edu.gemini.ags/src/main/scala/edu/gemini/ags/gems/mascot/MagLimits.scala
+++ bundle/edu.gemini.ags/src/main/scala/edu/gemini/ags/gems/mascot/MagLimits.scala
@@ -17,7 +17,7 @@ case class MagLimits private (bmag: Double,
                      kmag: Double) {
 
   // preallocate to use in filter
-  private lazy val filters = List(new Magnitude(bmag, MagnitudeBand.B), new Magnitude(vmag, MagnitudeBand.V), new Magnitude(rmag, MagnitudeBand.R), new Magnitude(rmag, MagnitudeBand.UC), new Magnitude(jmag, MagnitudeBand.J), new Magnitude(hmag, MagnitudeBand.H), new Magnitude(kmag, MagnitudeBand.K))
+  private lazy val filters = List(new Magnitude(bmag, MagnitudeBand.B), new Magnitude(vmag, MagnitudeBand.V), new Magnitude(jmag, MagnitudeBand.J), new Magnitude(hmag, MagnitudeBand.H), new Magnitude(kmag, MagnitudeBand.K))
   private lazy val RLikeFilters = RLikeBands.map(new Magnitude(rmag, _))
 
   /**
