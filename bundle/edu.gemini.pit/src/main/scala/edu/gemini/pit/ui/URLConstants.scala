326543e210c93128e9a5a012b85bcf84d902f819
Carlos Quiroz <cquiroz@gemini.edu>
2015-06-08 15:14:49 -0300

    REL-2271 Update message for download button #time 5m

--- bundle/edu.gemini.pit/src/main/scala/edu/gemini/pit/ui/URLConstants.scala
+++ bundle/edu.gemini.pit/src/main/scala/edu/gemini/pit/ui/URLConstants.scala
@@ -6,5 +6,5 @@ import edu.gemini.model.p1.immutable.Semester
  * Contains constant for URLs used in help contexts
  */
 object URLConstants {
-  val GET_TEMPLATES = (s"http://software.gemini.edu/phase1/templates/${Semester.current.display}/", "Download LaTeX/Word templates for the text sections.")
+  val GET_TEMPLATES = (s"http://software.gemini.edu/phase1/templates/${Semester.current.display}/", "Open directory for LaTeX/Word text section templates in browser.")
 }
