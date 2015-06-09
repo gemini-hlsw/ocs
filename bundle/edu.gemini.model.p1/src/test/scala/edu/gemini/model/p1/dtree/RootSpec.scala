fae177405246343a1892eabc1ddb7f746eb017ba
Carlos Quiroz <cquiroz@gemini.edu>
2015-06-09 18:39:53 -0300

    REL-2355 Update RootSpec test to use the same semester #time 5m

--- bundle/edu.gemini.model.p1/src/test/scala/edu/gemini/model/p1/dtree/RootSpec.scala
+++ bundle/edu.gemini.model.p1/src/test/scala/edu/gemini/model/p1/dtree/RootSpec.scala
@@ -8,7 +8,7 @@ class RootSpec extends SpecificationWithJUnit {
 
   "The Root Spec" should {
     "include Gsaoi" in {
-      val root = new Root(Semester(2015, A))
+      val root = new Root(Semester(2016, A))
       root.choices must contain(Instrument.Gsaoi)
     }
     "include Texes" in {
@@ -16,27 +16,27 @@ class RootSpec extends SpecificationWithJUnit {
       root.choices must contain (Instrument.Texes)
     }
     "include Speckles" in {
-      val root = new Root(Semester(2015, A))
+      val root = new Root(Semester(2016, A))
       root.choices must contain (Instrument.Dssi)
     }
     "include Visitor" in {
-      val root = new Root(Semester(2015, B))
+      val root = new Root(Semester(2016, B))
       root.choices must contain(Instrument.Visitor)
     }
     "include Gpi" in {
-      val root = new Root(Semester(2015, A))
+      val root = new Root(Semester(2016, A))
       root.choices must contain(Instrument.Gpi)
     }
     "includes Graces" in {
-      val root = new Root(Semester(2015, A))
+      val root = new Root(Semester(2016, A))
       root.choices must contain(Instrument.Graces)
     }
-    "Michelle has been removed in 2015A" in {
-      val root = new Root(Semester(2015, A))
+    "Michelle has been removed in 2016A" in {
+      val root = new Root(Semester(2016, A))
       root.choices must not contain Instrument.Michelle
     }
-    "T-ReCS has been removed in 2015A" in {
-      val root = new Root(Semester(2015, A))
+    "T-ReCS has been removed in 2016A" in {
+      val root = new Root(Semester(2016, A))
       root.choices must not contain Instrument.Trecs
     }
   }
