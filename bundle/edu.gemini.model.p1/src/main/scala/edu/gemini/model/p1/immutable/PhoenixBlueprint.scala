697384b55f1c4628d161cfdfa80055bd60a8afe5
Carlos Quiroz <cquiroz@gemini.edu>
2015-06-10 12:12:19 -0300

    REL-2356 Add Phoenix blueprint to the phase I model #time 3h

---
+++ bundle/edu.gemini.model.p1/src/main/scala/edu/gemini/model/p1/immutable/PhoenixBlueprint.scala
@@ -0,0 +1,35 @@
+package edu.gemini.model.p1.immutable
+
+import edu.gemini.model.p1.{mutable => M}
+
+object PhoenixBlueprint {
+  def apply(m: M.PhoenixBlueprint): PhoenixBlueprint = new PhoenixBlueprint(m)
+}
+
+case class PhoenixBlueprint(fpu: PhoenixFocalPlaneUnit, filter: PhoenixFilter) extends GeminiBlueprintBase {
+  def name: String = s"Phoenix ${fpu.value} ${filter.value}"
+
+  def this(m: M.PhoenixBlueprint) = this(
+    m.getFpu,
+    m.getFilter
+  )
+
+  override def instrument: Instrument = Instrument.Gpi
+  override def ao: AoPerspective = AoNone
+
+  def mutable(n: Namer) = {
+    val m = Factory.createPhoenixBlueprint()
+    m.setId(n.nameOf(this))
+    m.setName(name)
+    m.setFpu(fpu)
+    m.setFilter(filter)
+    m
+  }
+
+  def toChoice(n: Namer) = {
+    val m = Factory.createPhoenixBlueprintChoice()
+    m.setPhoenix(mutable(n))
+    m
+  }
+
+}
