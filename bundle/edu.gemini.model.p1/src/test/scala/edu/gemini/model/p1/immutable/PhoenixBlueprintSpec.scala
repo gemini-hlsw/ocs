ba66339dfe41b167a05be579d3063823e658a636
Carlos Quiroz <cquiroz@gemini.edu>
2015-06-10 13:20:33 -0300

    REL-2356 Unit test for the Phoenix blueprint #time 30m

---
+++ bundle/edu.gemini.model.p1/src/test/scala/edu/gemini/model/p1/immutable/PhoenixBlueprintSpec.scala
@@ -0,0 +1,57 @@
+package edu.gemini.model.p1.immutable
+
+import java.io.InputStreamReader
+
+import edu.gemini.model.p1.{mutable => M}
+import org.specs2.mutable._
+
+import scala.xml.XML
+
+class PhoenixBlueprintSpec extends SpecificationWithJUnit with SemesterProperties {
+
+  "The Phoenix Blueprint" should {
+    "has an FPU and a filter" in {
+      // trivial sanity tests
+      val blueprint = PhoenixBlueprint(M.PhoenixFocalPlaneUnit.MASK_1, M.PhoenixFilter.H6073)
+      blueprint.fpu must beEqualTo(M.PhoenixFocalPlaneUnit.MASK_1)
+      blueprint.filter must beEqualTo(M.PhoenixFilter.H6073)
+    }
+    "never uses Lgs" in {
+      val blueprint = PhoenixBlueprint(M.PhoenixFocalPlaneUnit.MASK_1, M.PhoenixFilter.H6073)
+      blueprint.ao must beEqualTo(AoNone)
+    }
+    "has an appropriate public name" in {
+      val blueprint = PhoenixBlueprint(M.PhoenixFocalPlaneUnit.MASK_1, M.PhoenixFilter.H6073)
+      blueprint.name must beEqualTo("Phoenix 0.17 arcsec slit H6073")
+    }
+    "is not a visitor" in {
+      val blueprint = PhoenixBlueprint(M.PhoenixFocalPlaneUnit.MASK_1, M.PhoenixFilter.H6073)
+      blueprint.visitor must beFalse
+      val observation = Observation(Some(blueprint), None, None, Band.BAND_1_2, None)
+
+      val proposal = Proposal.empty.copy(observations = observation :: Nil)
+      val xml = XML.loadString(ProposalIo.writeToString(proposal))
+
+      // verify the blueprint has a false attribute
+      xml must \\("Phoenix") \ "visitor" \> "false"
+    }
+    "export fpu and filter to XML" in {
+      val blueprint = PhoenixBlueprint(M.PhoenixFocalPlaneUnit.MASK_1, M.PhoenixFilter.H6073)
+      val observation = Observation(Some(blueprint), None, None, Band.BAND_1_2, None)
+
+      val proposal = Proposal.empty.copy(observations = observation :: Nil)
+      val xml = XML.loadString(ProposalIo.writeToString(proposal))
+
+      // verify the exported value
+      xml must \\("fpu") \> "0.17 arcsec slit"
+      xml must \\("filter") \> "H6073"
+    }
+    "be possible to deserialize" in {
+      val proposal = ProposalIo.read(new InputStreamReader(getClass.getResourceAsStream("proposal_with_phoenix.xml")))
+
+      proposal.blueprints.head.visitor must beFalse
+      proposal.blueprints must beEqualTo(PhoenixBlueprint(M.PhoenixFocalPlaneUnit.MASK_1, M.PhoenixFilter.H6073) :: Nil)
+    }
+  }
+
+}
