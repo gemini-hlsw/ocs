1fffac2277035b75a34e293c8e32d30c971d222d
Carlos Quiroz <cquiroz@gemini.edu>
2015-07-02 11:06:31 -0300

    OCSADV-395 Reduce visibility of internal structures of the QueryResults Table #time 10m

--- bundle/jsky.app.ot/src/main/scala/edu/gemini/catalog/ui/QueryResultsFrame.scala
+++ bundle/jsky.app.ot/src/main/scala/edu/gemini/catalog/ui/QueryResultsFrame.scala
@@ -26,101 +26,139 @@ trait PreferredSizeFrame { this: Window =>
 
 object QueryResultsWindow {
 
+  private object table {
+
+    sealed trait TableColumn[T] {
+      val title: String
+
+      def lens: PLens[Target, T]
+
+      def displayValue(t: T): String = t.toString
+
+      def render(target: Target): String = ~lens.get(target).map(displayValue)
+    }
+
+    case class IdColumn(title: String) extends TableColumn[String] {
+      override val lens = Target.name
+    }
+
+    case class RAColumn(title: String) extends TableColumn[RightAscension] {
+      override val lens = Target.coords >=> Coordinates.ra.partial
+
+      override def displayValue(t: RightAscension) = t.toAngle.formatHMS
+    }
+
+    case class DECColumn(title: String) extends TableColumn[Declination] {
+      override val lens = Target.coords >=> Coordinates.dec.partial
+
+      override def displayValue(t: Declination) = t.formatDMS
+    }
+
+    case class PMRAColumn(title: String) extends TableColumn[RightAscensionAngularVelocity] {
+      override val lens = Target.pm >=> ProperMotion.deltaRA.partial
+
+      override def displayValue(t: RightAscensionAngularVelocity): String = f"${t.velocity.masPerYear}%.2f"
+    }
+
+    case class PMDecColumn(title: String) extends TableColumn[DeclinationAngularVelocity] {
+      override val lens = Target.pm >=> ProperMotion.deltaDec.partial
+
+      override def displayValue(t: DeclinationAngularVelocity): String = f"${t.velocity.masPerYear}%.2f"
+    }
+
+    case class MagnitudeColumn(band: MagnitudeBand) extends TableColumn[Magnitude] {
+      override val title = band.name
+      // Lens from list of magnitudes to the band's magnitude if present
+      val bLens: List[Magnitude] @?> Magnitude = PLens(ml => ml.find(_.band === band).map(m => Store(b => sys.error("read-only lens"), m)))
+      override val lens = Target.magnitudes >=> bLens
+
+      override def displayValue(t: Magnitude): String = f"${t.value}%.2f"
+    }
+
+    val baseColumnNames: List[TableColumn[_]] = List(IdColumn("Id"), RAColumn("RA"), DECColumn("DE"))
+    val pmColumns: List[TableColumn[_]] = List(PMRAColumn("pmRA"), PMDecColumn("pmDEC"))
+    val magColumns = MagnitudeBand.all.map(MagnitudeColumn)
+
+    case class TargetsModel(targets: List[SiderealTarget]) extends AbstractTableModel {
+
+      // Available columns from the list of targets
+      val columns = {
+        val bandsInTargets = targets.flatMap(_.magnitudes).map(_.band).distinct
+        val hasPM = targets.exists(_.properMotion.isDefined)
+        val pmCols = if (hasPM) pmColumns else Nil
+        val magCols = magColumns.filter(m => bandsInTargets.contains(m.band))
+
+        baseColumnNames ::: pmCols ::: magCols
+      }
+
+      override def getRowCount = targets.length
+
+      override def getColumnCount = columns.size
+
+      override def getColumnName(column: Int): String =
+        columns(column).title
+
+      override def getValueAt(rowIndex: Int, columnIndex: Int) = (targets.isDefinedAt(rowIndex) option {
+        val target = targets(rowIndex)
+        columns.zipWithIndex.find(_._2 == columnIndex).map { c =>
+          c._1.render(target)
+        }
+      }).flatten.orNull
+    }
+
+    case class QueryResultsFrame(table: Table) extends Frame with PreferredSizeFrame {
+      title = "Query Results"
+      contents = new BorderPanel() {
+        add(new ScrollPane() {
+          contents = table
+        }, BorderPanel.Position.Center)
+      }
+      adjustSize()
+      pack()
+    }
+
+    val resultsTable = new Table() {
+      /**
+       * Sorting is not supported in scala.swing.Table (Scala 2.10).
+       */
+      // See: http://stackoverflow.com/questions/9588765/using-tablerowsorter-with-scala-swing-table
+      override def apply(row: Int, column: Int): Any = model.getValueAt(viewToModelRow(row), viewToModelColumn(column))
+
+      def viewToModelRow(idx: Int) = peer.convertRowIndexToModel(idx)
+
+      def modelToViewRow(idx: Int) = peer.convertRowIndexToView(idx)
+    }
+
+    val model = TargetsModel(Nil)
+    resultsTable.model = model
+    val sorter = new TableRowSorter[TargetsModel](model)
+    resultsTable.peer.setRowSorter(sorter)
+    resultsTable.peer.getRowSorter.toggleSortOrder(0)
+    val frame = QueryResultsFrame(resultsTable)
+  }
+
+  private def reloadSearchData(c: Coordinates) {
+    import QueryResultsWindow.table._
+
+    val query = CatalogQuery.catalogQuery(c, RadiusConstraint.between(Angle.zero, Angle.fromArcmin(17.9)), None)
+    VoTableClient.catalog(query).onSuccess {
+      case x =>
+        Swing.onEDT {
+          val model = TargetsModel(x.result.targets.rows)
+          resultsTable.model = model
+
+          val sorter = new TableRowSorter[TargetsModel](model)
+          resultsTable.peer.setRowSorter(sorter)
+          resultsTable.peer.getRowSorter.toggleSortOrder(0)
+        }
+    }
+  }
+
+  // Public interface
   val instance = this
 
-  sealed trait TableColumn[T] {
-    val title: String
-    def lens: PLens[Target, T]
-    def displayValue(t: T): String = t.toString
-
-    def render(target: Target): String = ~lens.get(target).map(displayValue)
-  }
-  case class IdColumn(title: String) extends TableColumn[String] {
-    override val lens = Target.name
-  }
-  case class RAColumn(title: String) extends TableColumn[RightAscension] {
-    override val lens = Target.coords >=> Coordinates.ra.partial
-    override def displayValue(t: RightAscension) = t.toAngle.formatHMS
-  }
-  case class DECColumn(title: String) extends TableColumn[Declination] {
-    override val lens = Target.coords >=> Coordinates.dec.partial
-    override def displayValue(t: Declination) = t.formatDMS
-  }
-  case class PMRAColumn(title: String) extends TableColumn[RightAscensionAngularVelocity] {
-    override val lens = Target.pm >=> ProperMotion.deltaRA.partial
-    override def displayValue(t: RightAscensionAngularVelocity): String = f"${t.velocity.masPerYear}%.2f"
-  }
-  case class PMDecColumn(title: String) extends TableColumn[DeclinationAngularVelocity] {
-    override val lens = Target.pm >=> ProperMotion.deltaDec.partial
-    override def displayValue(t: DeclinationAngularVelocity): String = f"${t.velocity.masPerYear}%.2f"
-  }
-  case class MagnitudeColumn(band: MagnitudeBand) extends TableColumn[Magnitude] {
-    override val title = band.name
-    // Lens from list of magnitudes to the band's magnitude if present
-    val bLens: List[Magnitude] @?> Magnitude = PLens(ml => ml.find(_.band === band).map(m => Store(b => sys.error("read-only lens"), m)))
-    override val lens = Target.magnitudes >=> bLens
-    override def displayValue(t: Magnitude): String = f"${t.value}%.2f"
-  }
-
-  val baseColumnNames:List[TableColumn[_]] = List(IdColumn("Id"), RAColumn("RA"), DECColumn("DE"))
-  val pmColumns:List[TableColumn[_]] = List(PMRAColumn("pmRA"), PMDecColumn("pmDEC"))
-  val magColumns = MagnitudeBand.all.map(MagnitudeColumn)
-
-  case class TargetsModel(targets: List[SiderealTarget]) extends AbstractTableModel {
-
-    // Available colums from the list of targets
-    val columns = {
-      val bandsInTargets = targets.flatMap(_.magnitudes).map(_.band).distinct
-      val hasPM = targets.exists(_.properMotion.isDefined)
-      val pmCols = if (hasPM) pmColumns else Nil
-      val magCols = magColumns.filter(m => bandsInTargets.contains(m.band))
-
-      baseColumnNames ::: pmCols ::: magCols
-    }
-
-    override def getRowCount = targets.length
-
-    override def getColumnCount = columns.size
-
-    override def getColumnName(column: Int): String =
-      columns(column).title
-
-    override def getValueAt(rowIndex: Int, columnIndex: Int) = (targets.isDefinedAt(rowIndex) option {
-      val target = targets(rowIndex)
-      columns.zipWithIndex.find(_._2 == columnIndex).map { c =>
-        c._1.render(target)
-      }
-    }).flatten.orNull
-  }
-
-  case class QueryResultsFrame(table: Table) extends Frame with PreferredSizeFrame {
-    title = "Query Results"
-    contents = new BorderPanel() {
-      add(new ScrollPane() {
-        contents = table
-      }, BorderPanel.Position.Center)
-    }
-    adjustSize()
-    pack()
-  }
-
-  private val table = new Table() {
-    /**
-     * Sorting is not supported in scala.swing.Table (Scala 2.10).
-     */
-    // See: http://stackoverflow.com/questions/9588765/using-tablerowsorter-with-scala-swing-table
-    override def apply(row: Int, column: Int): Any = model.getValueAt(viewToModelRow(row), viewToModelColumn(column))
-    def viewToModelRow(idx: Int) = peer.convertRowIndexToModel(idx)
-    def modelToViewRow(idx: Int) = peer.convertRowIndexToView(idx)
-  }
-  val model = TargetsModel(Nil)
-  table.model = model
-  val sorter = new TableRowSorter[TargetsModel](model)
-  table.peer.setRowSorter(sorter)
-  table.peer.getRowSorter.toggleSortOrder(0)
-  private val frame = QueryResultsFrame(table)
-
   def showOn(c: Coordinates):Unit = Swing.onEDT {
+    import QueryResultsWindow.table._
     if (frame.visible) {
       frame.peer.toFront()
     } else {
@@ -131,18 +169,4 @@ object QueryResultsWindow {
     reloadSearchData(c)
   }
 
-  def reloadSearchData(c: Coordinates): Unit = {
-    val query = CatalogQuery.catalogQuery(c, RadiusConstraint.between(Angle.zero, Angle.fromArcmin(17.9)), None)
-    VoTableClient.catalog(query).onSuccess {
-      case x =>
-        Swing.onEDT {
-          val model = TargetsModel(x.result.targets.rows)
-          table.model = model
-
-          val sorter = new TableRowSorter[TargetsModel](model)
-          table.peer.setRowSorter(sorter)
-          table.peer.getRowSorter.toggleSortOrder(0)
-        }
-    }
-  }
 }
