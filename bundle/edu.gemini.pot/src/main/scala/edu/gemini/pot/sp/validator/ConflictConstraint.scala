982372e6fdc44c9ba52f5b7bf50b218905021599
swalker2m <swalker2m@mac.com>
2015-07-06 09:54:47 -0300

    REL-2401: specify valid types for a conflict folder

--- bundle/edu.gemini.pot/src/main/scala/edu/gemini/pot/sp/validator/ConflictConstraint.scala
+++ bundle/edu.gemini.pot/src/main/scala/edu/gemini/pot/sp/validator/ConflictConstraint.scala
@@ -1,19 +1,19 @@
 package edu.gemini.pot.sp.validator
 
-import edu.gemini.pot.sp.{SPNodeKey, ISPNode}
+import edu.gemini.pot.sp.{SPComponentType, SPNodeKey, ISPNode}
 
 /** A constraint for the children of a conflict (which is no constraint at all). */
 case object ConflictConstraint extends Constraint {
   val initial = ConflictConstraint
-  def types:Types = sys.error("Unsupported operation.")
+
+  val types: Types =
+    (Types(Set())/:SPComponentType.values()) { (ts,ct) =>
+      if (ct == SPComponentType.CONFLICT_FOLDER) ts
+      else ts.addNarrow(ct)
+    }
+
   def copy(ts: Types) = sys.error("Unsupported operation.")
-  override def apply(n: NodeType[_ <: ISPNode], key:Option[SPNodeKey]) = {
 
-//    println("Validating child %s/%s (unconstrainted)".format(
-//      n.mf.getSimpleName,
-//      n.ct))
-
-    Right(this)
-  }
+  override def apply(n: NodeType[_ <: ISPNode], key:Option[SPNodeKey]) = Right(this)
 }
 
