87215642fb03555009540a27b28084052074db92
Florian Nussberger <florian.nussberger@gmail.com>
2015-10-09 20:21:07 -1000

    OCSADV-336B Replace brightness unit names, new baseline.

--- bundle/edu.gemini.itc.shared/src/main/java/edu/gemini/itc/shared/BrightnessUnit.java
+++ bundle/edu.gemini.itc.shared/src/main/java/edu/gemini/itc/shared/BrightnessUnit.java
@@ -3,21 +3,30 @@ package edu.gemini.itc.shared;
 import edu.gemini.spModel.type.DisplayableSpType;
 
 public enum BrightnessUnit implements DisplayableSpType {
-    // TODO: The "displayable" units are pretty ugly, but we have to keep them for
-    // TODO: now in order to be backwards compatible for regression testing.
-    MAG                 ("mag"),
-    ABMAG               ("ABmag"),
-    JY                  ("Jy"),
-    WATTS               ("watts_fd_wavelength"),
-    ERGS_WAVELENGTH     ("ergs_fd_wavelength"),
-    ERGS_FREQUENCY      ("ergs_fd_frequency"),
-    // -- TODO: same in blue but per area, can we unify those two sets of values?
-    MAG_PSA             ("mag_per_sq_arcsec"),
-    ABMAG_PSA           ("ABmag_per_sq_arcsec"),
-    JY_PSA              ("jy_per_sq_arcsec"),
-    WATTS_PSA           ("watts_fd_wavelength_per_sq_arcsec"),
-    ERGS_WAVELENGTH_PSA ("ergs_fd_wavelength_per_sq_arcsec"),
-    ERGS_FREQUENCY_PSA  ("ergs_fd_frequency_per_sq_arcsec")
+
+    // ==== INTEGRATED BRIGHTNESS
+
+    // Magnitudes
+    MAG                 ("Vega"),
+    ABMAG               ("AB"),
+
+    // Spectral Flux Density / Spectral Irradiance [W/m²/m]
+    JY                  ("Jy"),             // 10e-26 W/m²/Hz
+    WATTS               ("W/m²/µm"),
+    ERGS_WAVELENGTH     ("erg/s/cm²/Å"),
+    ERGS_FREQUENCY      ("erg/s/cm²/Hz"),
+
+    // ==== UNIFORM SURFACE BRIGHTNESS
+
+    // Magnitudes per area
+    MAG_PSA             ("Vega/arcsec²"),
+    ABMAG_PSA           ("AB/arcsec²"),
+
+    // Spectral Flux Density / Spectral Irradiance per area
+    JY_PSA              ("Jy/arcsec²"),
+    WATTS_PSA           ("W/m²/µm/arcsec²"),
+    ERGS_WAVELENGTH_PSA ("erg/s/cm²/Å/arcsec²"),
+    ERGS_FREQUENCY_PSA  ("erg/s/cm²/Hz/arcsec²")
     ;
     private final String displayValue;
     private BrightnessUnit(final String displayName) { this.displayValue = displayName; }
