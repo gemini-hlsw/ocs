1654822d1fd29c606d0a8d2f785051e30b425dab
Florian Nussberger <florian.nussberger@gmail.com>
2015-10-18 21:52:48 -1000

    OCSADV-354: Fixed indentation.

--- bundle/edu.gemini.auxfile.workflow/src/main/java/edu/gemini/auxfile/workflow/CheckTask.java
+++ bundle/edu.gemini.auxfile.workflow/src/main/java/edu/gemini/auxfile/workflow/CheckTask.java
@@ -15,13 +15,13 @@ public class CheckTask extends AuxFileTask {
 	}
 
 	@Override
-	public void execute(Workflow wf) throws Exception {
+    public void execute(Workflow wf) throws Exception {
         final AuxFileType type = AuxFileType.getFileType(getFile());
 
         // Try to send out emails if notification for this file type is desired.
         if (type.sendNotification()) {
-			wf.getMailer().notifyChecked(getProgId(), type, getFile().getName(), _checked);
-		}
-	}
+            wf.getMailer().notifyChecked(getProgId(), type, getFile().getName(), _checked);
+        }
+    }
 
 }
