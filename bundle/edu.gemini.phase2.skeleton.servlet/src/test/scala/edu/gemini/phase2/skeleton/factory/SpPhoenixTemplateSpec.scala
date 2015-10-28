eccc2103d6922d59c3566278de1f79335c66e8ba
Rob Norris <rob_norris@mac.com>
2015-10-28 10:39:26 -0700

    structural tests for template expansion

--- bundle/edu.gemini.phase2.skeleton.servlet/src/test/scala/edu/gemini/phase2/skeleton/factory/SpPhoenixTemplateSpec.scala
+++ bundle/edu.gemini.phase2.skeleton.servlet/src/test/scala/edu/gemini/phase2/skeleton/factory/SpPhoenixTemplateSpec.scala
@@ -2,53 +2,71 @@ package edu.gemini.phase2.skeleton.factory
 
 import edu.gemini.model.p1.immutable.PhoenixBlueprint
 import edu.gemini.model.p1.mutable.{PhoenixFilter, PhoenixFocalPlaneUnit}
-import edu.gemini.pot.sp.ISPGroup
 import edu.gemini.spModel.core.MagnitudeBand
-import edu.gemini.spModel.gemini.phoenix.PhoenixParams.{Filter, Mask}
-import edu.gemini.spModel.gemini.phoenix.blueprint.SpPhoenixBlueprint
+import edu.gemini.spModel.gemini.phoenix.InstPhoenix
+import edu.gemini.spModel.obs.SPObservation
+import edu.gemini.spModel.util.SPTreeUtil
 
-import org.scalacheck.{ Properties, Gen, Arbitrary }
-import org.scalacheck.Arbitrary.arbitrary
-import org.scalacheck.Prop.forAll
-import org.scalacheck.Prop._
-import org.scalacheck.Gen
-
-import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 
 import scala.collection.JavaConverters._
-import scalaz._, Scalaz._
 
-object SpPhoenixTemplateSpec extends TemplateSpec("PHOENIX_BP.xml") with Specification with ScalaCheck {
+object SpPhoenixTemplateSpec extends TemplateSpec("PHOENIX_BP.xml") with Specification {
 
   def test(fpu: PhoenixFocalPlaneUnit, filter: PhoenixFilter) =
     expand(proposal(PhoenixBlueprint(fpu, filter), List(1), MagnitudeBand.R)) { (p, sp) =>
       s"Phoenic Blueprint Expansion $fpu $filter " >> {
+
+        def group = groups(sp).head
+        def obs   = group.getAllObservations.asScala
+        def insts = obs.map(SPTreeUtil.findInstrument(_).getDataObject.asInstanceOf[InstPhoenix])
+        def sci   = obs.find(_.getDataObject.asInstanceOf[SPObservation].getLibraryId == "1").get
+        def scii  = SPTreeUtil.findInstrument(sci).getDataObject.asInstanceOf[InstPhoenix]
+        val (exp, coadds) = filter.name.head match {
+          case 'J' | 'H' | 'K' => (900.0, 1)
+          case 'L'             => (120.0, 3)
+          case 'M'             => ( 30.0, 4)
+        }
 
         "There should be exactly one template group." in {
           groups(sp).size must_== 1
         }
 
         "It should contain all four observations." in {
-          groups(sp).forall(libs(_) == Set(1, 2, 3, 4))
+          libs(group) == Set(1, 2, 3, 4)
         }
 
         "It should contain the how-to note." in {
-          groups(sp).forall(existsNote(_, "How to use the observations in this folder"))
+          existsNote(group, "How to use the observations in this folder")
         }
 
         "It should contain the calibration note." in {
-          groups(sp).forall(existsNote(_, "Darks, Flats, and Arcs"))
+          existsNote(group, "Darks, Flats, and Arcs")
         }
 
-        // TODO: check the configuration of the observations
+        s"All obs should have FPU $fpu" in {
+          insts.forall(_.getMask.name must_== fpu.name)
+        }
+
+        s"All obs should have filter $filter" in {
+          insts.forall(_.getFilter.name must_== filter.name)
+        }
+
+        s"Science obs should have exposure $exp" in {
+          scii.getExposureTime must_== exp
+        }
+
+        s"Science obs should have coadds $coadds" in {
+          scii.getCoadds must_== coadds
+        }
 
       }
     }
 
-
-  PhoenixFilter.values.foreach { filter =>
-    test(PhoenixFocalPlaneUnit.MASK_1, filter)
-  }
+  /*
+   * Test with every filter because exposure and coadds depend on it. We'll just pick an FPU
+   * because it doesn't enter into any of the configuration decisions.
+   */
+  PhoenixFilter.values.foreach(test(PhoenixFocalPlaneUnit.MASK_2, _))
 
 }
