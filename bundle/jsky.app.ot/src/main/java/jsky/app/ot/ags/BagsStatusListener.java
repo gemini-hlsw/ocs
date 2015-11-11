4ca2246732572be781a7ee06724ed514a8c883ae
Sebastian Raaphorst <sraaphor@gemini.edu>
2015-11-11 14:12:41 -0300

    OCSADV-453: Made BagsStatusListener into a functional interface.

--- bundle/jsky.app.ot/src/main/java/jsky/app/ot/ags/BagsStatusListener.java
+++ bundle/jsky.app.ot/src/main/java/jsky/app/ot/ags/BagsStatusListener.java
@@ -3,6 +3,7 @@ package jsky.app.ot.ags;
 import edu.gemini.pot.sp.ISPObservation;
 import edu.gemini.shared.util.immutable.Option;
 
+@FunctionalInterface
 public interface BagsStatusListener {
     void bagsStatusChanged(final ISPObservation obs, final Option<BagsStatus> oldStatus, final Option<BagsStatus> newStatus);
 }
