327c7fa92a5f79dfb57a42fa9d7fe8cfb677e767
Sebastian Raaphorst <sraaphor@gemini.edu>
2015-12-29 09:40:27 -0300

    OCSADV-515: Rebase and code deletion.

--- bundle/edu.gemini.shared.util/src/main/java/edu/gemini/shared/util/immutable/ImOption.java
+++ bundle/edu.gemini.shared.util/src/main/java/edu/gemini/shared/util/immutable/ImOption.java
@@ -12,8 +12,4 @@ public class ImOption {
     public static <T> Option<T> fromScalaOpt(final scala.Option<T> scalaOpt) {
         return scalaOpt.isDefined() ? new Some<>(scalaOpt.get()) : None.instance();
     }
-
-    public static <T> scala.Option<T> toScalaOpt(final Option<T> geminiOpt) {
-        return scala.Option.apply(geminiOpt.getOrNull());
-    }
 }
