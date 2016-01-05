5936fd18458ba0d5becdac343b0cecf82feb7537
Rob Norris <rob_norris@mac.com>
2016-01-05 15:33:33 -0800

    cleanup

--- bundle/edu.gemini.horizons.server/src/main/scala/edu/gemini/horizons/server/backend/HS2.scala
+++ bundle/edu.gemini.horizons.server/src/main/scala/edu/gemini/horizons/server/backend/HS2.scala
@@ -8,191 +8,157 @@ import org.apache.commons.httpclient.methods.GetMethod
 import edu.gemini.horizons.api.HorizonsException
 import java.io.IOException
 import CgiHorizonsConstants._
+
+import scala.util.matching.Regex
 import scalaz._, Scalaz._, scalaz.effect.IO
 
 object HS2 {
 
   case class InternalError(input: List[String], message: String)
+  case class Row[A](a: A, name: String)
 
   sealed abstract class Search[A](val queryString: String) extends Product with Serializable {
-    def parseResponse(lines: List[String]): InternalError \/ List[HS2.Row[A]]
+    def parseResponse(lines: List[String]): String \/ List[HS2.Row[A]]
   }
   object Search {
 
-    final case class Comet(partial: String) extends Search[HorizonsDesignation.Comet](s"COMNAM=$partial*;CAP") {
-      def parseResponse(lines: List[String]): InternalError \/ List[HS2.Row[HorizonsDesignation.Comet]] =
-        lines match {
-          case _ :: header :: tail =>
+    // Representation of column offsets as (start, end) pairs, as deduced from --- -------- --- -----
+    private type Offsets = List[(Int, Int)]
 
-            // Common case is that we have many results, or none.
-            lazy val case0: InternalError \/ List[HS2.Row[HorizonsDesignation.Comet]] =
-              ("""  +Small-body Search Results  """.r.findFirstMatchIn(header) \/>
-                InternalError(lines, "'Small-body Search Results' header not found")).flatMap { _ =>
-                lines.dropWhile(s => !s.trim.startsWith("---")) match {
-                  case Nil => Nil.right // no column headers means no results!
-                  case colHeaders :: rows => // designation and name at column offsets 2 and 3
-                    val offsets = "-+".r.findAllMatchIn(colHeaders).map(m => (m.start, m.end)).toList
-                    try {
-                      (offsets.lift(2) |@| offsets.lift(3)).tupled.map {
-                        case ((ods, ode), (ons, one)) =>
-                          rows.takeWhile(_.trim.nonEmpty).map { row =>
-                            val desig = row.substring(ods, ode).trim
-                            val name  = row.substring(ons, one).trim
-                            HS2.Row(HorizonsDesignation.Comet(desig), name)
-                          }
-                      } \/> InternalError(lines, "Not enough columns.")
-                    } catch {
-                      case sioobe: StringIndexOutOfBoundsException =>
-                        InternalError(lines, "Column value(s) not found.").left
-                    }
+    // Parse many results based on an expected header pattern. This will read through the tail until
+    // headers are found, then parse the remaining lines (until a blank line is encountered) using
+    // the function constructed by `f`, if any (otherwise it is assumed that there were not enough
+    // columns found). Returns a list of values or an error message.
+    private def parseMany[A](header: String, tail: List[String], headerPattern: Regex)(f: Offsets => Option[String => A] ): String \/ List[A] =
+      (headerPattern.findFirstMatchIn(header) \/> ("Header pattern not found: " + headerPattern)) *>
+      (tail.dropWhile(s => !s.trim.startsWith("---")) match {
+        case Nil                => Nil.right // no column headers means no results!
+        case colHeaders :: rows =>           // we have a header row with data rows following
+          val offsets = "-+".r.findAllMatchIn(colHeaders).map(m => (m.start, m.end)).toList
+          try {
+            f(offsets).map(g => rows.takeWhile(_.trim.nonEmpty).map(g)) \/> "Not enough columns."
+          } catch {
+            case    nfe: NumberFormatException           =>  ("Number format exception: " + nfe.getMessage).left
+            case sioobe: StringIndexOutOfBoundsException =>   "Column value(s) not found.".left
+          }
+      })
+
+    // Split into header/tail, parse
+    private def parseHeader[A](lines: List[String])(f: (String, List[String]) => String \/ List[A]): String \/ List[A] =
+      lines match {
+        case _ :: h :: t => f(h, t)
+        case _           => "Fewer than 2 lines!".left
+      }
+
+    // Comet
+    final case class Comet(partial: String) extends Search[HorizonsDesignation.Comet](s"COMNAM=$partial*;CAP") {
+      def parseResponse(lines: List[String]): String \/ List[HS2.Row[HorizonsDesignation.Comet]] =
+        parseHeader[HS2.Row[HorizonsDesignation.Comet]](lines) { case (header, tail) => 
+
+          // Common case is that we have many results, or none.
+          lazy val case0 =
+            parseMany[HS2.Row[HorizonsDesignation.Comet]](header, tail, """  +Small-body Search Results  """.r) { os =>
+              (os.lift(2) |@| os.lift(3)).tupled.map {
+                case ((ods, ode), (ons, one)) => { row =>
+                  val desig = row.substring(ods, ode).trim
+                  val name  = row.substring(ons, one).trim
+                  HS2.Row(HorizonsDesignation.Comet(desig), name)
                 }
               }
+            }
 
-            // Single result with form: JPL/HORIZONS      Hubble (C/1937 P1)     2015-Dec-31 11:40:21
-            lazy val case1 =
-              """  +([^(]+)\s+\((.+?)\)  """.r.findFirstMatchIn(header).map { m =>
-                List(HS2.Row(HorizonsDesignation.Comet(m.group(2)), m.group(1)))
-              } \/> InternalError(lines, "Could not match 'Hubble (C/1937 P1)' header pattern.")
+          // Single result with form: JPL/HORIZONS      Hubble (C/1937 P1)     2015-Dec-31 11:40:21
+          lazy val case1 =
+            """  +([^(]+)\s+\((.+?)\)  """.r.findFirstMatchIn(header).map { m =>
+              List(HS2.Row(HorizonsDesignation.Comet(m.group(2)), m.group(1)))
+            } \/> "Could not match 'Hubble (C/1937 P1)' header pattern."
 
-            // Single result with form: JPL/HORIZONS         1P/Halley           2015-Dec-31 11:40:21
-            lazy val case2 =
-              """  +([^/]+)/(.+?)  """.r.findFirstMatchIn(header).map { m =>
-                List(HS2.Row(HorizonsDesignation.Comet(m.group(1)), m.group(2)))
-              } \/> InternalError(lines, "Could not match '1P/Halley' header pattern.")
+          // Single result with form: JPL/HORIZONS         1P/Halley           2015-Dec-31 11:40:21
+          lazy val case2 =
+            """  +([^/]+)/(.+?)  """.r.findFirstMatchIn(header).map { m =>
+              List(HS2.Row(HorizonsDesignation.Comet(m.group(1)), m.group(2)))
+            } \/> "Could not match '1P/Halley' header pattern."
 
-            // First one that works!
-            case0 orElse
-              case1 orElse
-              case2 orElse InternalError(lines, "Could not parse the header line.").left
-
-          case _ => InternalError(lines, "Fewer than 2 lines!").left
-
+          // First one that works!
+          case0 orElse
+          case1 orElse
+          case2 orElse "Could not parse the header line.".left
         }
     }
 
-
+    // Asteroid
     final case class Asteroid(partial: String) extends Search[HorizonsDesignation.Asteroid](s"ASTNAM=$partial*") {
-      def parseResponse(lines: List[String]): \/[InternalError, List[Row[HorizonsDesignation.Asteroid]]] =
-        lines match {
-          case _ :: header :: tail =>
+      def parseResponse(lines: List[String]): \/[String, List[Row[HorizonsDesignation.Asteroid]]] =
+        parseHeader[Row[HorizonsDesignation.Asteroid]](lines) { case (header, tail) =>
 
-            // Common case is that we have many results, or none.
-            lazy val case0: InternalError \/ List[HS2.Row[HorizonsDesignation.Asteroid]] =
-              ("""  +Small-body Index Search Results  """.r.findFirstMatchIn(header) \/>
-                InternalError(lines, "'Small-body Index Search Results' header not found")).flatMap { _ =>
-                lines.dropWhile(s => !s.trim.startsWith("---")) match {
-                  case Nil => Nil.right // no column headers means no results!
-                  case colHeaders :: rows => // designation and name at column offsets 2 and 3
-                    val offsets = "-+".r.findAllMatchIn(colHeaders).map(m => (m.start, m.end)).toList
-                    try {
-                      (offsets.lift(0) |@| offsets.lift(1) |@| offsets.lift(2)).tupled.map {
-                        case ((ors, ore), (ods, ode), (ons, one)) =>
-                          rows.takeWhile(_.trim.nonEmpty).map { row =>
-                            val rec = row.substring(ors, ore).trim.toInt
-                            val desig = row.substring(ods, ode).trim
-                            val name = row.substring(ons).trim // last column
-                            if (desig == "(undefined)") {
-                              HS2.Row(HorizonsDesignation.AsteroidOldStyle(rec): HorizonsDesignation.Asteroid, name)
-                            } else {
-                              HS2.Row(HorizonsDesignation.AsteroidNewStyle(desig): HorizonsDesignation.Asteroid, name)
-                            }
-                          }
-                      } \/> InternalError(lines, "Not enough columns.")
-                    } catch {
-                      case nfe: NumberFormatException =>
-                        InternalError(lines, "Old-style identifier not an integer.").left
-                      case sioobe: StringIndexOutOfBoundsException =>
-                        InternalError(lines, "Column value(s) not found.").left
-                    }
+          // Common case is that we have many results, or none.
+          lazy val case0 =
+            parseMany[HS2.Row[HorizonsDesignation.Asteroid]](header, tail, """  +Small-body Index Search Results  """.r) { os =>
+              (os.lift(0) |@| os.lift(1) |@| os.lift(2)).tupled.map {
+                case ((ors, ore), (ods, ode), (ons, one)) => { row =>
+                  val rec   = row.substring(ors, ore).trim.toInt
+                  val desig = row.substring(ods, ode).trim
+                  val name  = row.substring(ons     ).trim // last column
+                  desig match {
+                    case "(undefined)" => HS2.Row(HorizonsDesignation.AsteroidOldStyle(rec): HorizonsDesignation.Asteroid, name)
+                    case des           => HS2.Row(HorizonsDesignation.AsteroidNewStyle(des): HorizonsDesignation.Asteroid, name)
+                  }
                 }
               }
+            }
 
-            // Single result with form: JPL/HORIZONS      90377 Sedna (2003 VB12)     2015-Dec-31 11:40:21
-            lazy val case1 =
-              """  +\d+ ([^(]+)\s+\((.+?)\)  """.r.findFirstMatchIn(header).map { m =>
-                List(HS2.Row(HorizonsDesignation.AsteroidNewStyle(m.group(2)) : HorizonsDesignation.Asteroid, m.group(1)))
-              } \/> InternalError(lines, "Could not match '90377 Sedna (2003 VB12)' header pattern.")
+          // Single result with form: JPL/HORIZONS      90377 Sedna (2003 VB12)     2015-Dec-31 11:40:21
+          lazy val case1 =
+            """  +\d+ ([^(]+)\s+\((.+?)\)  """.r.findFirstMatchIn(header).map { m =>
+              List(HS2.Row(HorizonsDesignation.AsteroidNewStyle(m.group(2)) : HorizonsDesignation.Asteroid, m.group(1)))
+            } \/> "Could not match '90377 Sedna (2003 VB12)' header pattern."
 
-            // Single result with form: JPL/HORIZONS      4 Vesta     2015-Dec-31 11:40:21
-            lazy val case2 =
-              """  +(\d+) ([^(]+?)  """.r.findFirstMatchIn(header).map { m =>
-                List(HS2.Row(HorizonsDesignation.AsteroidOldStyle(m.group(1).toInt) : HorizonsDesignation.Asteroid, m.group(2)))
-              } \/> InternalError(lines, "Could not match '4 Vesta' header pattern.")
+          // Single result with form: JPL/HORIZONS      4 Vesta     2015-Dec-31 11:40:21
+          lazy val case2 =
+            """  +(\d+) ([^(]+?)  """.r.findFirstMatchIn(header).map { m =>
+              List(HS2.Row(HorizonsDesignation.AsteroidOldStyle(m.group(1).toInt) : HorizonsDesignation.Asteroid, m.group(2)))
+            } \/> "Could not match '4 Vesta' header pattern."
 
-            // First one that works!
-            case0 orElse
-            case1 orElse
-            case2 orElse InternalError(lines, "Could not parse the header line.").left
-
-          case _ => InternalError(lines, "Fewer than 2 lines!").left
+          // First one that works!
+          case0 orElse
+          case1 orElse
+          case2 orElse "Could not parse the header line.".left
 
         }
-
     }
 
 
     final case class MajorBody(partial: String) extends Search[HorizonsDesignation.MajorBody](s"$partial") {
-      def parseResponse(lines: List[String]): \/[InternalError, List[Row[HorizonsDesignation.MajorBody]]] =
-        lines match {
-          case _ :: header :: tail =>
+      def parseResponse(lines: List[String]): \/[String, List[Row[HorizonsDesignation.MajorBody]]] =
+        parseHeader[Row[HorizonsDesignation.MajorBody]](lines) { case (header, tail) =>
 
-            // Common case is that we have many results, or none.
-            lazy val case0: InternalError \/ List[HS2.Row[HorizonsDesignation.MajorBody]] =
-              ("""Multiple major-bodies match string""".r.findFirstMatchIn(header) \/>
-                InternalError(lines, "'Multiple major-bodies match string' header not found")).flatMap { _ =>
-                lines.dropWhile(s => !s.trim.startsWith("---")) match {
-                  case Nil => Nil.right // no column headers means no results!
-                  case colHeaders :: rows => // designation and name at column offsets 2 and 3
-                    val offsets = "-+".r.findAllMatchIn(colHeaders).map(m => (m.start, m.end)).toList
-                    try {
-                      (offsets.lift(0) |@| offsets.lift(1)).tupled.map {
-                        case ((ors, ore), (ons, one)) =>
-                          rows.takeWhile(_.trim.nonEmpty).map { row =>
-                            val rec = row.substring(ors, ore).trim.toInt
-                            val name = row.substring(ons, one).trim
-                            HS2.Row(HorizonsDesignation.MajorBody(rec.toInt), name)
-                          }.filterNot(_.a.num < 0) // filter out spacecraft
-                      } \/> InternalError(lines, "Not enough columns.")
-                    } catch {
-                      case nfe: NumberFormatException =>
-                        InternalError(lines, "Old-style identifier not an integer.").left
-                      case sioobe: StringIndexOutOfBoundsException =>
-                        InternalError(lines, "Column value(s) not found.").left
-                    }
+          // Common case is that we have many results, or none.
+          lazy val case0 = 
+            parseMany[HS2.Row[HorizonsDesignation.MajorBody]](header, tail, """Multiple major-bodies match string""".r) { os =>
+              (os.lift(0) |@| os.lift(1)).tupled.map {
+                case ((ors, ore), (ons, one)) => { row =>
+                  val rec  = row.substring(ors, ore).trim.toInt
+                  val name = row.substring(ons, one).trim
+                  HS2.Row(HorizonsDesignation.MajorBody(rec.toInt), name)
                 }
               }
+            } .map(_.filterNot(_.a.num < 0)) // filter out spacecraft
 
-            // Single result with form:  Revised: Aug 11, 2015       Charon / (Pluto)     901
-            lazy val case1 =
-              """  +(.*?)  +(\d+) $""".r.findFirstMatchIn(header).map { m =>
-                List(HS2.Row(HorizonsDesignation.MajorBody(m.group(2).toInt), m.group(1)))
-              } \/> InternalError(lines, "Could not match 'Charon / (Pluto)     901' header pattern.")
+          // Single result with form:  Revised: Aug 11, 2015       Charon / (Pluto)     901
+          lazy val case1 =
+            """  +(.*?)  +(\d+) $""".r.findFirstMatchIn(header).map { m =>
+              List(HS2.Row(HorizonsDesignation.MajorBody(m.group(2).toInt), m.group(1)))
+            } \/> "Could not match 'Charon / (Pluto)     901' header pattern."
 
-            // First one that works, otherwise Nil because it falls through to small-body search
-            case0 orElse case1 orElse Nil.right
-
-          case _ => InternalError(lines, "Fewer than 2 lines!").left
-
+          // First one that works, otherwise Nil because it falls through to small-body search
+          case0 orElse 
+          case1 orElse Nil.right
         }
     }
 
   }
 
-  case class Row[A](a: A, name: String)
-
-  def search[A](s: Search[A]): Throwable \/ List[Row[A]] =
-    ???
-
-  def lookup(d: HorizonsDesignation, site: Site, start: Long, duration: Long, steps: Int): Throwable \/ Ephemeris =
-    ???
-
-}
-
-
-object Searcher {
-
-  def search[A](search: HS2.Search[A]): HS2.InternalError \/ List[HS2.Row[A]] = {
-
+  def search[A](search: Search[A]): EitherT[IO, InternalError, List[Row[A]]] = {
     val queryParams: Map[String, String] =
       Map(
         BATCH      -> "1",
@@ -201,17 +167,17 @@ object Searcher {
         EPHEMERIS  -> NO,
         COMMAND    -> s"'${search.queryString}'"
       )
-
-    val lines = fetch(queryParams).unsafePerformIO()
-    // lines.foreach(println)
-    search.parseResponse(lines)
+    EitherT(fetch(queryParams).map(ss => search.parseResponse(ss).leftMap(HS2.InternalError(ss, _))))
   }
 
+  def lookup(d: HorizonsDesignation, site: Site, start: Long, duration: Long, steps: Int): Throwable \/ Ephemeris =
+    ???
 
-  def fetch(params: Map[String, String]): IO[List[String]] =
+
+  private def fetch(params: Map[String, String]): IO[List[String]] =
     IO {
-      // HORIZONS allows only one request at a time for a given host
-      Searcher.synchronized {
+      // HORIZONS allows only one request at a time for a given host :-/
+      HS2.synchronized {
         val method = new GetMethod(CgiHorizonsConstants.HORIZONS_URL)
         try {
           method.setQueryString(params.map { case (k, v) => new NameValuePair(k, v) } .toArray)
@@ -231,6 +197,8 @@ object Searcher {
     }
 
 }
+
+
 
 //
 //
--- bundle/edu.gemini.horizons.server/src/test/scala/edu/gemini/horizons/server/backend/HS2Spec.scala
+++ bundle/edu.gemini.horizons.server/src/test/scala/edu/gemini/horizons/server/backend/HS2Spec.scala
@@ -11,20 +11,20 @@ import org.specs2.mutable.Specification
 
 object HS2Spec extends Specification with ScalaCheck {
 
-  import HS2.{ Search, Row }
-  import Searcher.search
-
+  import HS2.{ InternalError, Row, Search }
   import edu.gemini.spModel.core.{ HorizonsDesignation => HD }
 
+  def runSearch[A](s: Search[A]): InternalError \/ List[Row[A]] =
+    HS2.search(s).run.unsafePerformIO
 
   "comet search" should {
 
     "handle empty results" in {
-      search(Search.Comet("kjhdwekuq")) must_== \/-(Nil)
+      runSearch(Search.Comet("kjhdwekuq")) must_== \/-(Nil)
     }
 
     "handle multiple results" in {
-      search(Search.Comet("hu")).map(_.take(5)) must_== \/-(List(      
+      runSearch(Search.Comet("hu")).map(_.take(5)) must_== \/-(List(      
         Row(HD.Comet("67P"), "Churyumov-Gerasimenko"), 
         Row(HD.Comet("106P"), "Schuster"), 
         Row(HD.Comet("130P"), "McNaught-Hughes"), 
@@ -34,13 +34,13 @@ object HS2Spec extends Specification with ScalaCheck {
     }
 
     "handle single result (Format 1) Hubble (C/1937 P1)" in {
-      search(Search.Comet("hubble")) must_== \/-(List(
+      runSearch(Search.Comet("hubble")) must_== \/-(List(
         Row(HD.Comet("C/1937 P1"), "Hubble")
       ))
     }   
 
     "handle single result (Format 2) 1P/Halley pattern" in {
-      search(Search.Comet("halley")) must_== \/-(List(
+      runSearch(Search.Comet("halley")) must_== \/-(List(
         Row(HD.Comet("1P"), "Halley")
       ))
     }   
@@ -50,11 +50,11 @@ object HS2Spec extends Specification with ScalaCheck {
   "asteroid search" should {
 
     "handle empty results" in {
-      search(Search.Asteroid("kjhdwekuq")) must_== \/-(Nil)
+      runSearch(Search.Asteroid("kjhdwekuq")) must_== \/-(Nil)
     }
 
     "handle multiple results" in {
-      search(Search.Asteroid("her")).map(_.take(5)) must_== \/-(List(
+      runSearch(Search.Asteroid("her")).map(_.take(5)) must_== \/-(List(
         Row(HD.AsteroidOldStyle(103), "Hera"),
         Row(HD.AsteroidOldStyle(121), "Hermione"),
         Row(HD.AsteroidOldStyle(135), "Hertha"),
@@ -64,13 +64,13 @@ object HS2Spec extends Specification with ScalaCheck {
     }
 
     "handle single result (Format 1) 90377 Sedna (2003 VB12)" in {
-      search(Search.Asteroid("sedna")) must_== \/-(List(
+      runSearch(Search.Asteroid("sedna")) must_== \/-(List(
         Row(HD.AsteroidNewStyle("2003 VB12"), "Sedna")
       ))
     }   
 
     "handle single result (Format 2) 29 Amphitrite" in {
-      search(Search.Asteroid("amphitrite")) must_== \/-(List(
+      runSearch(Search.Asteroid("amphitrite")) must_== \/-(List(
         Row(HD.AsteroidOldStyle(29), "Amphitrite")
       ))
     }   
@@ -80,19 +80,19 @@ object HS2Spec extends Specification with ScalaCheck {
   "major body search" should {
 
     "handle empty results" in {
-      search(Search.MajorBody("kjhdwekuq")) must_== \/-(Nil)
+      runSearch(Search.MajorBody("kjhdwekuq")) must_== \/-(Nil)
     }
 
     "handle empty results with small-body fallthrough (many)" in {
-      search(Search.MajorBody("hu")) must_== \/-(Nil)
+      runSearch(Search.MajorBody("hu")) must_== \/-(Nil)
     }
 
     "handle empty results with small-body fallthrough (single)" in {
-      search(Search.MajorBody("hermione")) must_== \/-(Nil)
+      runSearch(Search.MajorBody("hermione")) must_== \/-(Nil)
     }
 
     "handle multiple results" in {
-      search(Search.MajorBody("mar")).map(_.take(5)) must_== \/-(List(
+      runSearch(Search.MajorBody("mar")).map(_.take(5)) must_== \/-(List(
         Row(HD.MajorBody(4), "Mars Barycenter"),
         Row(HD.MajorBody(499), "Mars"),
         Row(HD.MajorBody(723), "Margaret")
@@ -100,7 +100,7 @@ object HS2Spec extends Specification with ScalaCheck {
     }
 
     "handle single result" in {
-      search(Search.MajorBody("charon")) must_== \/-(List(
+      runSearch(Search.MajorBody("charon")) must_== \/-(List(
         Row(HD.MajorBody(901), "Charon / (Pluto)")
       ))
     }   
