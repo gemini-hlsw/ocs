d6bf27e72ddbc4cd836386a70f9f9f7f2dbe1315
swalker2m <swalker2m@mac.com>
2016-01-12 15:05:05 -0300

    OCSADV-514: OptsList testing wip

--- bundle/edu.gemini.pot/src/test/scala/edu/gemini/spModel/target/env/OptsListSpec.scala
+++ bundle/edu.gemini.pot/src/test/scala/edu/gemini/spModel/target/env/OptsListSpec.scala
@@ -4,12 +4,31 @@ import org.scalacheck.Prop.forAll
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 
+import scalaz._, Scalaz._
+
 class OptsListSpec extends Specification with ScalaCheck with Arbitraries {
 
   "OptsList" should {
-    "produce a left disjuntion on clearFocus" in
+    "have no focus after clearFocus" in
       forAll { (opts: OptsList[Int]) =>
-        opts.clearFocus.toDisjunction.isLeft
+        !opts.clearFocus.hasFocus
       }
+
+    "contain an element iff the nel or zipper contains the element" in
+      forAll { (opts: OptsList[Int], i: Int) =>
+        opts.contains(i) == opts.toDisjunction.fold(_.toList.contains(i), _.toStream.contains(i))
+      }
+
+    "have a focus equal to the zipper focus (if any)" in
+      forAll { (opts: OptsList[Int]) =>
+        opts.focus == opts.toDisjunction.toOption.map(_.focus)
+      }
+
+    "have a focus element which is the same as the element at the focus index" in
+      forAll { (opts: OptsList[Int]) =>
+        opts.focus == opts.focusIndex.map { opts.toList }
+      }
+
+    
   }
 }
