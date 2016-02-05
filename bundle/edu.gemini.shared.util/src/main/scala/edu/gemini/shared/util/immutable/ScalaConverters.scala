92136396ab4cc244a73eb9c5d69d4f3bd0117574
Sebastian Raaphorst <sraaphor@gemini.edu>
2016-02-04 21:44:21 -0300

    OCSADV-517: Added converters for Either.

--- bundle/edu.gemini.shared.util/src/main/scala/edu/gemini/shared/util/immutable/ScalaConverters.scala
+++ bundle/edu.gemini.shared.util/src/main/scala/edu/gemini/shared/util/immutable/ScalaConverters.scala
@@ -1,6 +1,7 @@
 package edu.gemini.shared.util.immutable
 
 import scala.collection.JavaConverters._
+import scalaz.{\/-, -\/, \/}
 
 object ScalaConverters {
 
@@ -58,5 +59,22 @@ object ScalaConverters {
   implicit class ImPairOps[A,B](val p: Pair[A,B]) extends AnyVal {
     def asScalaPair: (A,B) =
       (p._1(), p._2())
+  }
+
+  implicit class ScalaEitherOps[L,R](val e: scala.Either[L,R]) extends AnyVal {
+    def asGeminiEither: Either[L,R] =
+      e.fold(Either.left[L,R], Either.right[L,R])
+  }
+
+  implicit class ScalazDisjunctionOps[L,R](val e: \/[L,R]) extends AnyVal {
+    def asGeminiEither: Either[L,R] =
+      e.fold(Either.left[L,R], Either.right[L,R])
+  }
+
+  implicit class EitherOps[L,R](val e: Either[L,R]) extends AnyVal {
+    def asScalaEither: scala.Either[L, R] =
+      e.fold(((l: L) => Left(l)).asGeminiFunction1, ((r: R) => Right(r)).asGeminiFunction1)
+    def asScalazDisjunction: \/[L,R] =
+      e.fold(((l: L) => -\/(l)).asGeminiFunction1, ((r: R) => \/-(r)).asGeminiFunction1)
   }
 }
