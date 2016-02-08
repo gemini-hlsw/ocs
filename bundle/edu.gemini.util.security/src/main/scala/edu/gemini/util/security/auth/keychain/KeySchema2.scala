cffbda6361fd1170959fe6d2d5fa463930f5c5c8
Rob Norris <rob_norris@mac.com>
2016-02-08 13:52:29 -0800

    initial doobie upgrade

---
+++ bundle/edu.gemini.util.security/src/main/scala/edu/gemini/util/security/auth/keychain/KeySchema2.scala
@@ -0,0 +1,157 @@
+package edu.gemini.util.security.auth.keychain
+
+import edu.gemini.spModel.core.{SPProgramID,Affiliate}
+import edu.gemini.util.security.principal._
+
+import doobie.imports._
+
+import java.sql.{SQLException, Timestamp}
+import java.io.File
+import java.security.Principal
+import java.util.logging.Logger
+
+import scalaz._, Scalaz._, effect.IO
+
+object KeySchema2 extends KeyMappers2 {
+  lazy val Log: Logger = java.util.logging.Logger.getLogger(KeySchema.getClass.getName)
+
+  // H2-specific SqlState
+  val TABLE_OR_VIEW_NOT_FOUND = SqlState("42S02")
+
+  // Current schema version
+  val SchemaVersion = 1
+
+  ////// HELPERS
+
+  def info(s: String): ConnectionIO[Unit] =
+    FC.delay(Log.info(s))
+
+  def fail[A](s: String): ConnectionIO[A] =
+    FC.delay(sys.error(s))
+
+  ////// DATABASE OPERATIONS
+
+  val createSchema: ConnectionIO[Unit] = 
+    sql"""
+      create table VERSION (
+        VALUE INTEGER NOT NULL
+      );
+      create table KEYS (
+        CLASS   VARCHAR NOT NULL,
+        NAME    VARCHAR NOT NULL,
+        HASH    VARCHAR NOT NULL,
+        VERSION INTEGER NOT NULL
+      );
+      create unique index KEY_IDX on KEYS (CLASS,NAME)
+    """.update.run.void
+
+  def insertSchemaVersion(version: Int): ConnectionIO[Unit] =
+    sql"insert into VERSION (VALUE) values ($version)".update.run.void
+
+  val getSchemaVersion: ConnectionIO[Int] =
+    sql"select VALUE from VERSION".query[Int].unique
+
+  def checkSchema(path: String): ConnectionIO[Unit] =
+    open(path) exceptSomeSqlState {
+      case TABLE_OR_VIEW_NOT_FOUND => createNewDatabase
+    }
+
+  def createNewDatabase: ConnectionIO[Unit] =
+    for {
+      _ <- info("This is a new database. Creating schema...")
+      _ <- createSchema
+      _ <- insertSchemaVersion(SchemaVersion)
+    } yield ()
+
+  def open(path:String): ConnectionIO[Unit] =
+    for {
+      v <- getSchemaVersion
+      _ <- info(s"Opened database with schema version $SchemaVersion on ${path}")
+      _ <- (v != SchemaVersion).whenM(upgrade(v) >> checkSchema(path))
+    } yield ()
+
+  def upgrade(from: Int): ConnectionIO[Unit] =
+    for {
+      _ <- info(s"Upgrading from version ${from}...")
+      _ <- from match {
+        // case 1 => ...
+        case n => fail("Can't upgrade from ${from}")
+      }
+    } yield ()
+
+  def checkPass(p: GeminiPrincipal, pass: String): ConnectionIO[Option[KeyVersion]] =
+    sql"""
+      select VERSION
+      from   KEYS
+      where  CLASS = ${p.clazz}
+      and    NAME  = ${p.getName}
+      and    HASH  = ${hash(pass)}
+    """.query[KeyVersion].option
+
+  def setPass(p: GeminiPrincipal, pass: String): ConnectionIO[KeyVersion] = 
+    getVersion(p) >>= (_.fold(insertPass(p, pass))(_ => updatePass(p, pass)))
+
+  private def insertPass(p: GeminiPrincipal, pass: String): ConnectionIO[KeyVersion] =
+    sql"""
+      insert into KEYS (CLASS, NAME, HASH, VERSION)
+      values (${p.clazz}, ${p.getName}, ${hash(pass)}, 1)
+    """.update.run *> checkPass(p, pass).map(_.get) // hmm
+
+  def updatePass(p: GeminiPrincipal, pass: String): ConnectionIO[KeyVersion] =
+    sql"""
+      update KEYS 
+      set    HASH    = ${hash(pass)}, 
+             VERSION = VERSION + 1
+      where  CLASS = ${p.clazz} 
+      and    NAME  = ${p.getName}
+    """.update.run *> checkPass(p, pass).map(_.get) // hmm
+
+  def getVersion(p: GeminiPrincipal): ConnectionIO[Option[KeyVersion]] =
+    sql"""
+      select VERSION
+      from   KEYS
+      where  CLASS = ${p.clazz}
+      and    NAME  = ${p.getName}
+    """.query[KeyVersion].option
+
+  def revokeKey(p: GeminiPrincipal): ConnectionIO[Unit] =
+    sql"""
+      delete KEYS
+      where  CLASS = ${p.clazz}
+      and    NAME  = ${p.getName}
+    """.update.run.void
+
+  def backup(f: File): ConnectionIO[Unit] =
+    for {
+      p <- FC.delay(f.getAbsolutePath)
+      _ <- info("Backing up key database to " + p)
+      _ <- sql"BACKUP TO $p".update.run
+    } yield ()
+
+  def hash(s: String) = s // TODO
+
+}
+
+trait KeyMappers2 {
+
+  // Indirection for principal type names
+  object PrincipalType {
+    val Affiliate = "Affiliate"
+    val Program   = "Program"
+    val User      = "User"
+    val Staff     = "Staff"
+    val Visitor   = "Visitor"
+  }
+
+  // Clazz extractor
+  implicit class EnhanceGP(p: GeminiPrincipal) {
+    def clazz: String = p match {
+      case AffiliatePrincipal(n) => PrincipalType.Affiliate
+      case ProgramPrincipal(pid) => PrincipalType.Program
+      case UserPrincipal(n)      => PrincipalType.User
+      case StaffPrincipal(n)     => PrincipalType.Staff
+      case VisitorPrincipal(n)   => PrincipalType.Visitor
+    }
+  }
+
+}
---
+++ bundle/edu.gemini.util.security/src/test/scala/edu/gemini/util/security/auth/keychain/KeySchema2Spec.scala
@@ -0,0 +1,171 @@
+package edu.gemini.util.security.auth.keychain
+
+import doobie.imports._
+import org.specs2.mutable.Specification
+import scalaz._, Scalaz._, effect.IO
+import edu.gemini.util.security.principal._
+import edu.gemini.spModel.core._
+import java.io.File
+
+object KeySchema2Spec extends Specification {
+  import KeySchema2._
+
+  val serialId = new java.util.concurrent.atomic.AtomicLong(System.currentTimeMillis)
+
+  val principals: List[GeminiPrincipal] = 
+    List(
+      AffiliatePrincipal(Affiliate.CHILE),
+      ProgramPrincipal(SPProgramID.toProgramID("GS-2010A-Q-11")),
+      StaffPrincipal.Gemini,
+      UserPrincipal("bob@dobbs.com"),
+      VisitorPrincipal(SPProgramID.toProgramID("GS-2014B-C-2"))
+    )
+
+  def go[A](f: ConnectionIO[A]): A = {
+    val xa = DriverManagerTransactor[IO]("org.h2.Driver", s"jdbc:h2:mem:ks${serialId.getAndIncrement};DB_CLOSE_DELAY=-1")
+    f.transact(xa).ensuring(sql"SHUTDOWN IMMEDIATELY".update.run.transact(xa).attempt).unsafePerformIO
+  }
+
+  "checkSchema" should {
+
+    "initialize with new database" in go {
+      checkSchema("«in memory»") >| true
+    }
+
+    "initialize with existing database" in go {
+      checkSchema("«in memory»").replicateM(2) >| true
+    }
+  
+  }
+
+  "checkPass" should {
+
+    "return None for unkown principals" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        kvs <- principals.traverse(checkPass(_, "blah"))
+      } yield kvs.forall(_ must_== None)
+    }
+
+    "return Some(1) for newly created passwords" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        _   <- principals.traverse(setPass(_, "blah"))
+        kvs <- principals.traverse(checkPass(_, "blah"))
+      } yield kvs.forall(_ must_== Some(1))
+    }
+
+    "return Some(n) for updated passwords" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        _   <- principals.traverse(setPass(_, "blah")).replicateM(3)
+        kvs <- principals.traverse(checkPass(_, "blah"))
+      } yield kvs.forall(_ must_== Some(3))
+    }
+
+    "return None for revoked keys" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        _   <- principals.traverse(setPass(_, "blah"))
+        _   <- principals.traverse(revokeKey)
+        kvs <- principals.traverse(checkPass(_, "blah"))
+      } yield kvs.forall(_ must_== None)
+    }
+
+  }
+
+  "setPass" should {
+
+    "return 1 for newly created passwords" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        kvs <- principals.traverse(setPass(_, "blah"))
+      } yield kvs.forall(_ must_== 1)
+    }
+
+    "return n for updated passwords" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        _   <- principals.traverse(setPass(_, "blah"))
+        kvs <- principals.traverse(setPass(_, "blah"))
+      } yield kvs.forall(_ must_== 2)
+    }
+
+    "correctly set initial password" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        _   <- principals.traverse(p => setPass(p, p.hashCode.toString))
+        kvs <- principals.traverse(p => checkPass(p, p.hashCode.toString))
+      } yield kvs.forall(_ must_== Some(1))
+    }
+
+    "correctly update new password" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        _   <- principals.traverse(p => setPass(p, p.hashCode.toString))
+        _   <- principals.traverse(p => setPass(p, p.hashCode.toString + "-new"))
+        kvs <- principals.traverse(p => checkPass(p, p.hashCode.toString + "-new"))
+      } yield kvs.forall(_ must_== Some(2))
+    }
+
+  }
+
+  "getVersion" should {
+
+     "return None for unkown principals" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        kvs <- principals.traverse(getVersion)
+      } yield kvs.forall(_ must_== None)
+    }
+
+    "return Some(1) for newly created passwords" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        _   <- principals.traverse(setPass(_, "blah"))
+        kvs <- principals.traverse(getVersion)
+      } yield kvs.forall(_ must_== Some(1))
+    }
+
+    "return Some(n) for updated passwords" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        _   <- principals.traverse(setPass(_, "blah")).replicateM(3)
+        kvs <- principals.traverse(getVersion)
+      } yield kvs.forall(_ must_== Some(3))
+    }
+
+    "return None for revoked keys" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        _   <- principals.traverse(setPass(_, "blah"))
+        _   <- principals.traverse(revokeKey)
+        kvs <- principals.traverse(getVersion)
+      } yield kvs.forall(_ must_== None)
+    }
+
+  }
+
+  "revokeKey" should {
+
+    "result in None with checkPass" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        _   <- principals.traverse(setPass(_, "blah"))
+        _   <- principals.traverse(revokeKey)
+        kvs <- principals.traverse(checkPass(_, "blah"))
+      } yield kvs.forall(_ must_== None)
+    }
+
+    "return None for revoked keys" in go {
+      for {
+        _   <- checkSchema("«in memory»")
+        _   <- principals.traverse(setPass(_, "blah"))
+        _   <- principals.traverse(revokeKey)
+        kvs <- principals.traverse(getVersion)
+      } yield kvs.forall(_ must_== None)
+    }
+
+  }
+
+}
