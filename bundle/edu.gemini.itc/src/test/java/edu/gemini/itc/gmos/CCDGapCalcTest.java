e6539c3470fb83280ba0d4641594f76b79062ab0
spakzad <spakzad@gemini.edu>
2016-02-11 10:21:45 -1000

    REL-497: Test added to make sure calcIFU2Shift code in CCDGapCalc.java works and matches the output from 'gfoneshift' in PYRAF.

---
+++ bundle/edu.gemini.itc/src/test/java/edu/gemini/itc/gmos/CCDGapCalcTest.java
@@ -0,0 +1,24 @@
+package edu.gemini.itc.gmos;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Testing out calcIfu2Shift code in CCDGapCalc.
+ * [Add more details!!!]
+ * ...
+ */
+public class CCDGapCalcTest {
+
+    @Test
+    public void thisIsATest() {
+        final double actualResult0  = CCDGapCalc.calcIfu2Shift(750.0, 831.0);
+        final double expectedResult0 = 2736.24773782;           // Taken from gfoneshift in PYRAF
+        final double actualResult1  = CCDGapCalc.calcIfu2Shift(750.0, 150.0);
+        final double expectedResult1 = 3645.34948931;           // Taken from gfoneshift in PYRAF
+
+        Assert.assertTrue("this is true", true);
+        Assert.assertEquals("well this should work", expectedResult0, actualResult0, 0.1);   // 20150814: 0.0172 difference
+        Assert.assertEquals("well this should work", expectedResult1, actualResult1, 0.1);     // 20150814: 0.0288 difference-
+    }
+}
