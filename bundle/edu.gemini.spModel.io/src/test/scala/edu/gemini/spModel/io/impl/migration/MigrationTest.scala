33ee1f56c0d20c3284aa0265629f70844aa54a12
Rob Norris <rob_norris@mac.com>
2016-03-14 23:37:13 -0700

    sanity checker

--- bundle/edu.gemini.spModel.io/src/test/scala/edu/gemini/spModel/io/impl/migration/MigrationTest.scala
+++ bundle/edu.gemini.spModel.io/src/test/scala/edu/gemini/spModel/io/impl/migration/MigrationTest.scala
@@ -1,11 +1,10 @@
 package edu.gemini.spModel.io.impl.migration
 
-import java.io.InputStreamReader
+import java.io.{FileReader, InputStreamReader}
 
 import edu.gemini.pot.sp.ISPProgram
 import edu.gemini.pot.spdb.{DBLocalDatabase, IDBDatabaseService}
 import edu.gemini.spModel.io.impl.PioSpXmlParser
-import org.junit.Assert._
 
 /**
  * Base trait for all migration tests
@@ -31,5 +30,37 @@ trait MigrationTest {
 
   protected def withTestProgram2[A](programName: String)(block: ISPProgram => A): A =
     withTestProgram(programName, (_, p) => block(p))
+
+}
+
+/** Read a bunch of .xml files, reporting any errors. */
+object MigrationTester {
+  import java.io.File
+
+  def main(args: Array[String]): Unit =
+    args match {
+      case Array(dir) => go(new File(dir))
+      case _          => println("usage: MigrationTester <dir>")
+    }
+
+  private def go(dir: File): Unit = {
+    val db = DBLocalDatabase.createTransient
+    try {
+      val parser = new PioSpXmlParser(db.getFactory)
+      val files  = Option(dir.listFiles).getOrElse(Array.empty[File]).filter(_.getName.endsWith(".xml")).sortBy(_.getName)
+      val total  = files.length
+      files.zipWithIndex.foreach { case (f, n) =>
+        print(s"[$n/$total] ${f.getName} ... ")
+        try {
+          parser.parseDocument(new FileReader(f))
+          println(" [ok]")
+        } catch {
+          case e: Exception => println(s" ${Console.RED}[err] ${e.getMessage}${Console.RESET}")
+        }
+      }
+    } finally {
+      db.getDBAdmin.shutdown()
+    }
+  }
 
 }
