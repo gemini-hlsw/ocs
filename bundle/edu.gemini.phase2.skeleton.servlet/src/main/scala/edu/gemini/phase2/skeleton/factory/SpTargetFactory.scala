23c1bddf745e7b636a7c6ec628a4ca477ffd4a75
Rob Norris <rob_norris@mac.com>
2016-03-22 15:44:39 -0700

    bundle_edu_gemini_phase2_skeleton_servlet tests

--- bundle/edu.gemini.phase2.skeleton.servlet/src/main/scala/edu/gemini/phase2/skeleton/factory/SpTargetFactory.scala
+++ bundle/edu.gemini.phase2.skeleton.servlet/src/main/scala/edu/gemini/phase2/skeleton/factory/SpTargetFactory.scala
@@ -1,92 +1,49 @@
 package edu.gemini.phase2.skeleton.factory
 
 import edu.gemini.model.p1.immutable._
-import edu.gemini.shared.{skyobject => SO}
-import edu.gemini.shared.util.immutable.DefaultImList
-import edu.gemini.spModel.core.{Coordinates, MagnitudeSystem, MagnitudeBand, Magnitude}
-import edu.gemini.spModel.{target => SP}
+import edu.gemini.spModel.core
+import edu.gemini.spModel.target.SPTarget
 
-import java.text.DateFormat
-import scala.collection.JavaConverters._
+import scalaz._, Scalaz._
 
+// P1 and P2 models are almost identical now
 object SpTargetFactory {
 
-  def create(t: Target, time: Long): Either[String, SP.SPTarget] =
-    t match {
-      case too: TooTarget         => Right(createTooTarget(too))
-      case nsd: NonSiderealTarget => createNonSiderealTarget(nsd, time)
-      case sid: SiderealTarget    => createSiderealTarget(sid, time)
-    }
+  val dra  = core.ProperMotion.deltaRA  >=> core.RightAscensionAngularVelocity.velocity >=> core.AngularVelocity.masPerYear
+  val ddec = core.ProperMotion.deltaDec >=> core.DeclinationAngularVelocity.velocity    >=> core.AngularVelocity.masPerYear
 
-  private def createTooTarget(too: TooTarget): SP.SPTarget = {
-    val sp   = new SP.SPTarget(0.0, 0.0)
-    sp.setName(too.name)
-    sp
+  def create(t: Target, time: Long): Either[String, SPTarget] = {
+    val ct: core.Target =
+      t match {
+        case too: TooTarget         => core.TooTarget(too.name)
+        case nsd: NonSiderealTarget => initNonSidereal(nsd, time).exec(core.NonSiderealTarget.empty)
+        case sid: SiderealTarget    => initSidereal(sid, time).exec(core.SiderealTarget.empty)
+      }
+    Right(new SPTarget(ct))
   }
 
-  private def siderealCoordinates(t: Target, time: Long): Either[String, Coordinates] =
-    t.coords(time).toRight("Target coordinates could not be determined on %s".format(formatTime(time)))
+  private def toCoreEphemeris(e: List[EphemerisElement]): core.Ephemeris =
+    ==>>.fromList(e.map { case EphemerisElement(coords, _, time) => (time -> coords) })
 
-  private def nonSiderealCoordinates(nsid: NonSiderealTarget, time: Long): Either[String, (Coordinates, Long)] =
-    closestCoordinates(nsid, time).toRight("Non-sidreal target coordinates missing ephemeris data.")
+  private def apparentMag(d: Double): core.Magnitude =
+    new core.Magnitude(d, core.MagnitudeBand.AP, core.MagnitudeSystem.AB)
 
-  private def closestCoordinates(nsid: NonSiderealTarget, time: Long): Option[(Coordinates, Long)] =
-    closestEphemeris(nsid, time) map { e => (e.coords, e.validAt) }
+  def coreProperMotion(pm: ProperMotion): core.ProperMotion =
+    ((dra  := pm.deltaRA) *> (ddec := pm.deltaDec)).exec(core.ProperMotion.zero)
 
-  private def closestEphemeris(nsid: NonSiderealTarget, time: Long): Option[EphemerisElement] =
-    nsid.ephemeris match {
-      case Nil => None
-      case lst => Some(lst minBy { e => (time - e.validAt).abs })
-    }
+  private def initNonSidereal(nsid: NonSiderealTarget, time: Long): State[core.NonSiderealTarget, Unit] =
+    for {
+      _ <- core.NonSiderealTarget.name       := nsid.name
+      _ <- core.NonSiderealTarget.ephemeris  := toCoreEphemeris(nsid.ephemeris)
+      _ <- core.NonSiderealTarget.magnitudes := nsid.magnitude(time).map(apparentMag).toList
+    } yield ()
 
-  private def formatTime(time: Long): String =
-    DateFormat.getDateTimeInstance.format(new java.util.Date(time))
-
-
-  private def createNonSiderealTarget(nsid: NonSiderealTarget, time: Long): Either[String, SP.SPTarget] =
-    ??? // TODO: P1 to P2
-//    for {
-//      coordsAt <- nonSiderealCoordinates(nsid, time).right
-//    } yield {
-//      val itarget = new SP.system.ConicTarget()
-//      val (coords, when) = coordsAt
-//      itarget.setDateForPosition(new java.util.Date(when))
-//
-//      val spTarget = new SP.SPTarget(itarget)
-//      spTarget.setName(nsid.name)
-//      setRaDec(spTarget, coords)
-//
-//      // Add apparent magnitude, if any.
-//      nsid.magnitude(time)
-//        .map(new SO.Magnitude(SO.Magnitude.Band.AP, _, MagnitudeSystem.AB))
-//        .foreach(spTarget.putMagnitude)
-//
-//      spTarget
-//    }
-
-  private def createSiderealTarget(sid: SiderealTarget, time: Long): Either[String, SP.SPTarget] =
-    ??? // TODO: P1 to P2
-//    for {
-//      coords <- siderealCoordinates(sid, time).right
-//      mags   <- siderealMags(sid).right
-//    } yield {
-//      val itarget  = new SP.system.HmsDegTarget()
-//      sid.properMotion.foreach { pm =>
-//        val ra  = pm.deltaRA
-//        val dec = pm.deltaDec
-//        itarget.setPM1(new SPProperMotionRA(ra,   SP.system.CoordinateParam.Units.MILLI_ARCSECS_PER_YEAR))
-//        itarget.setPM2(new SPProperMotionDec(dec, SP.system.CoordinateParam.Units.MILLI_ARCSECS_PER_YEAR))
-//      }
-//
-//      val spTarget = new SP.SPTarget(itarget)
-//      setRaDec(spTarget, coords)
-//      spTarget.setName(sid.name)
-//      spTarget.setMagnitudes(DefaultImList.create(mags.asJava))
-//      spTarget
-//    }
-
-  private def setRaDec(spt: SP.SPTarget, c: Coordinates) {
-    spt.setRaDecDegrees(c.ra.toAngle.toDegrees, c.dec.toDegrees)
-  }
+  private def initSidereal(sid: SiderealTarget, time: Long): State[core.SiderealTarget, Unit] =
+    for {
+      _ <- core.SiderealTarget.name         := sid.name
+      _ <- core.SiderealTarget.coordinates  := sid.coords
+      _ <- core.SiderealTarget.properMotion := sid.properMotion.map(coreProperMotion)
+      _ <- core.SiderealTarget.magnitudes   := sid.magnitudes
+    } yield ()
 
 }
