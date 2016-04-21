3aecc5951eff1d5556d754ee92ee8435ccf55bba
Sebastian Raaphorst <sraaphor@gemini.edu>
2016-04-21 16:41:28 -0300

    OCSADV-453B: WIP.

---
+++ bundle/jsky.app.ot/src/main/scala/jsky/app/ot/ags/BagsStatus.scala
@@ -0,0 +1,10 @@
+package jsky.app.ot.ags
+
+sealed abstract class BagsStatus(val message: String)
+
+object BagsStatus {
+  case object Pending extends BagsStatus("BAGS lookup pending.")
+  case object Running extends BagsStatus("BAGS running...")
+  case class Failed(reason: String)  extends BagsStatus(s"BAGS failed to execute: $reason")
+  case object NoTarget extends BagsStatus("BAGS could not find a target.")
+}
