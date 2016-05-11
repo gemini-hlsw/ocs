0adaa6f5ea23a73202a56f6b5495e718b561ba51
Sebastian Raaphorst <sraaphor@gemini.edu>
2016-05-11 12:05:31 -0300

    REL-2323B: WIP.

--- bundle/edu.gemini.ags/src/main/scala/edu/gemini/ags/impl/MultiProbeStrategy.scala
+++ bundle/edu.gemini.ags/src/main/scala/edu/gemini/ags/impl/MultiProbeStrategy.scala
@@ -6,57 +6,57 @@ import edu.gemini.ags.api.{AgsAnalysis, AgsMagnitude, AgsStrategy}
 import edu.gemini.catalog.api.CatalogQuery
 import edu.gemini.spModel.ags.AgsStrategyKey
 import edu.gemini.spModel.core.{RBandsList, SiderealTarget}
-import edu.gemini.spModel.guide.{ValidatableGuideProbe, GuideProbe}
+import edu.gemini.spModel.guide.{GuideProbe, ValidatableGuideProbe}
 import edu.gemini.spModel.obs.context.ObsContext
 
 import scala.concurrent.Future
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import scalaz._
+import Scalaz._
+
 
 /**
  * Represents an AGS strategy consisting of multiple substrategies.
  *
  * Currently, this is only used to represent combinations of SingleProbeStrategies.
- *
- * Since select returns a position angle, this allows us to use the position angle of the paStrategy and ignore
- * the position angles of the (fully or approximately) rotation-symmetric (around the base pos) strategies returned
- * by select.
  */
-class MultiProbeStrategy(val key: AgsStrategyKey, paStrategy: AgsStrategy, paSymmetricStrategies: List[AgsStrategy]) extends AgsStrategy {
-  val strategies = paStrategy :: paSymmetricStrategies
+case class MultiProbeStrategy(key: AgsStrategyKey, strategies: NonEmptyList[AgsStrategy]) extends AgsStrategy {
 
   override def magnitudes(ctx: ObsContext, mt: MagnitudeTable): List[(GuideProbe, AgsMagnitude.MagnitudeCalc)] =
-    strategies.flatMap(_.magnitudes(ctx, mt))
+    strategies.toList.flatMap(_.magnitudes(ctx, mt))
 
   override def analyze(ctx: ObsContext, mt: MagnitudeTable): List[AgsAnalysis] =
-    strategies.flatMap(_.analyze(ctx, mt))
+    strategies.toList.flatMap(_.analyze(ctx, mt))
 
   override def analyze(ctx: ObsContext, mt: MagnitudeTable, guideProbe: ValidatableGuideProbe, guideStar: SiderealTarget): Option[AgsAnalysis] =
     AgsAnalysis.analysis(ctx, mt, guideProbe, guideStar, probeBands)
 
   override def catalogQueries(ctx: ObsContext, mt: MagnitudeTable): List[CatalogQuery] =
-    strategies.flatMap(_.catalogQueries(ctx, mt))
+    strategies.toList.flatMap(_.catalogQueries(ctx, mt))
 
   override def candidates(ctx: ObsContext, mt: MagnitudeTable): Future[List[(GuideProbe, List[SiderealTarget])]] =
-    Future.traverse(strategies)(_.candidates(ctx, mt)).map(_.flatten)
+    Future.traverse(strategies.toList)(_.candidates(ctx, mt)).map(_.flatten)
 
   override def estimate(ctx: ObsContext, mt: MagnitudeTable): Future[AgsStrategy.Estimate] =
-    Future.fold(strategies.map(_.estimate(ctx, mt)))(1.0)((p,est) => p * est.probability).map(Estimate(_))
+    Future.fold(strategies.toList.map(_.estimate(ctx, mt)))(1.0)((p,est) => p * est.probability).map(Estimate(_))
 
   // As described in the comment for the class, we pick the position angle for the FIRST strategy, since it is the
   // only one that can have a patrol field that is not (at least approximately) symmetric under rotation around the base.
   override def select(ctx: ObsContext, mt: MagnitudeTable): Future[Option[AgsStrategy.Selection]] = {
-    Future.fold(strategies.map(_.select(ctx, mt)))(None: Option[AgsStrategy.Selection])((resOpt,curOpt) =>
+    Future.fold(strategies.toList.map(_.select(ctx, mt)))(None: Option[AgsStrategy.Selection])((resOpt,curOpt) =>
       curOpt.map(cur => AgsStrategy.Selection(cur.posAngle, resOpt.fold(cur.assignments)(cur.assignments ++ _.assignments))).orElse(resOpt)
     )
   }
 
-  // Default is R bands.
-  val probeBands = RBandsList
+  // From the left, take the band that is the superset of the most bands.
+  // This should but may not contain all bands, as BandsList is a sealed trait.
+  override lazy val probeBands = strategies.toList.map(_.probeBands).reduceLeft((sup,cur) => {
+    if (sup.bands.toSet.subsetOf(cur.bands.toSet)) cur else sup
+  })
 
-  override val guideProbes: List[GuideProbe] =
-    strategies.flatMap(_.guideProbes).distinct
+  override lazy val guideProbes: List[GuideProbe] =
+    strategies.toList.flatMap(_.guideProbes).distinct
 }
 
-case object GmosNorthOiwfsAltair extends MultiProbeStrategy(AgsStrategyKey.GmosNorthAltairOiwfsKey, Strategy.GmosNorthOiwfs, List(Strategy.AltairAowfs))
-case object GmosNorthOiwfsPwfs1 extends MultiProbeStrategy(AgsStrategyKey.GmosNorthPwfs1OiwfsKey, Strategy.GmosNorthOiwfs, List(Strategy.Pwfs1North))
+case object GmosNorthOiwfsAltair extends MultiProbeStrategy(AgsStrategyKey.GmosNorthAltairOiwfsKey, NonEmptyList(Strategy.GmosNorthOiwfs, Strategy.AltairAowfs))
+case object GmosNorthOiwfsPwfs1 extends MultiProbeStrategy(AgsStrategyKey.GmosNorthPwfs1OiwfsKey, NonEmptyList(Strategy.GmosNorthOiwfs, Strategy.Pwfs1North))
