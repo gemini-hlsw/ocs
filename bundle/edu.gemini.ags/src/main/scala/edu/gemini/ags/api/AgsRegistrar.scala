1d95eae9952459f7ce1291b5c0e65c3c5549bf73
swalker2m <swalker2m@mac.com>
2016-05-23 12:07:19 -0400

    REL-2788: simplify default strategy calculation

--- bundle/edu.gemini.ags/src/main/scala/edu/gemini/ags/api/AgsRegistrar.scala
+++ bundle/edu.gemini.ags/src/main/scala/edu/gemini/ags/api/AgsRegistrar.scala
@@ -20,32 +20,8 @@ object AgsRegistrar {
   /**
    * Determines the default or best strategy for the given observation.
    */
-  def defaultStrategy(ctx: ObsContext): Option[AgsStrategy] = {
-    // Figure out which guide probes we're using, preferring those we mark as
-    // selected (i.e., with a primary target)
-    val guideEnv   = ctx.getTargets.getGuideEnvironment
-    val ref0       = guideEnv.getPrimaryReferencedGuiders
-    val referenced = (if (ref0.isEmpty) guideEnv.getReferencedGuiders else ref0).asScala
-
-    // Determine how many referenced guiders in the context are assigned by the
-    // given strategy.
-    def incidence(s: AgsStrategy): Int = (referenced & s.guideProbes.toSet).size
-
-    // Get the first strategy with the highest overlap between referenced
-    // guiders and guiders assigned by the strategy.  It's important to preserve
-    // order in this search because the valid strategies are returned in ranked
-    // order of preference.
-    AgsRegistrar.validStrategies(ctx) match {
-      case Nil    =>
-        None
-      case h :: t =>
-        val (highestIncidence, _) = ((h, incidence(h))/:t) { case ((s0, i0), s1) =>
-          val i1 = incidence(s1)
-          if (i0 >= i1) (s0, i0) else (s1, i1)
-        }
-        Some(highestIncidence)
-    }
-  }
+  def defaultStrategy(ctx: ObsContext): Option[AgsStrategy] =
+    validStrategies(ctx).headOption
 
   /**
    * Obtains the user's preference, if any, to override the default strategy
