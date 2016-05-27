729aed90b6b2d5741cd876d2345d54d4bcc2fbe2
Carlos Quiroz <cquiroz@gemini.edu>
2016-05-27 11:22:17 -0400

    REL-2628 Include additional notes for F2 Long-slit templates #time 15m

--- bundle/edu.gemini.phase2.skeleton.servlet/src/main/scala/edu/gemini/phase2/template/factory/impl/flamingos2/Flamingos2Longslit.scala
+++ bundle/edu.gemini.phase2.skeleton.servlet/src/main/scala/edu/gemini/phase2/template/factory/impl/flamingos2/Flamingos2Longslit.scala
@@ -41,7 +41,7 @@ case class Flamingos2Longslit(blueprint:SpFlamingos2BlueprintLongslit, exampleTa
 
   val targetGroup = Seq(11,12) ++ acq ++ Seq(15,16,17,18)
   val baselineFolder = Seq.empty
-  val notes = Seq("F2 Long-Slit Notes")
+  val notes = Seq("F2 Long-Slit Notes", "Repeats contain the ABBA offsets", "Use same PA for science and Telluric")
 
   val scienceAndTellurics = Seq(12,15,16,18)
 
