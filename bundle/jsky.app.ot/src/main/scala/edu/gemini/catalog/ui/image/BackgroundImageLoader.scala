72a6ba6086156462bfc688ada49785637ddfa92e
Carlos Quiroz <cquiroz@gemini.edu>
2016-08-11 11:44:48 -0400

    REL-2859 Set the image on the tpe as soon as downloaded, very important for large programs #time 1h

--- bundle/jsky.app.ot/src/main/scala/edu/gemini/catalog/ui/image/BackgroundImageLoader.scala
+++ bundle/jsky.app.ot/src/main/scala/edu/gemini/catalog/ui/image/BackgroundImageLoader.scala
@@ -2,7 +2,7 @@ package edu.gemini.catalog.ui.image
 
 import java.time.Instant
 import java.util.concurrent.atomic.AtomicInteger
-import java.util.concurrent.{Executors, ThreadFactory}
+import java.util.concurrent.{ExecutorService, Executors, ThreadFactory}
 
 import edu.gemini.catalog.image.{ImageCatalog, ImageCatalogClient, ImageEntry}
 import edu.gemini.spModel.target.obsComp.TargetObsComp
@@ -17,76 +17,12 @@ import jsky.util.Preferences
 import scalaz._
 import Scalaz._
 import scala.swing.Swing
-import scalaz.concurrent.Task
+import scalaz.concurrent.{Strategy, Task}
 
 case class ImageLoadRequest(catalog: ImageCatalog, c: Coordinates)
 
 object BackgroundImageLoader {
   val cacheDir = Preferences.getPreferences.getCacheDir
-
-  /** Called when a program is created to download its images */
-  def watch(prog: ISPProgram): Unit = {
-      val targets = prog.getAllObservations.asScala.toList.flatMap(_.getObsComponents.asScala.map(n => (n, n.getDataObject)).collect {
-        case (node, t: TargetObsComp) =>
-          tpeCoordinates(TpeContext(node))
-      })
-      // remove duplicates
-      val tasks = targets.flatten.distinct.map(requestImageDownload)
-      // Run
-      runAsync(tasks) {
-        case \/-(e) => e.flatten.foreach(setTpeImage) // Try to set the image on the tpe for any target being displayed
-        case -\/(e) => println(e)
-      }
-    }
-
-  /** Called when a program is removed to clear the cache */
-  def unwatch(prog: ISPProgram): Unit = {
-    // not necessary
-  }
-
-  /******************************************
-    * Methods interacting with the java side
-    *****************************************/
-  /**
-    * Display an image if available on disk or request the download if necessary
-    */
-  def loadImageOnTheTpe(tpe: TpeContext): Unit = {
-    // Attempt to find the image locally
-    val local = for {
-      c <- OptionT(Task.now(tpeCoordinates(tpe)))
-      i <- OptionT(ImageCatalogClient.findImage(c, BackgroundImageLoader.cacheDir))
-    } yield i
-
-    // If not found request to download
-    val image = local.orElse(OptionT(BackgroundImageLoader.requestImageDownload(tpe)))
-
-    // Execute and set the image on the tpe
-    runAsync(image.run) {
-      case \/-(Some(x)) => setTpeImage(x)
-      case \/-(_)       => // No image was found, don't do anything
-      case -\/(e)       => // Ignore the errors
-    }
-  }
-
-  /**
-    * Creates a task to load an image
-    */
-  private[image] def requestImageDownload(c: Coordinates): Task[Option[ImageEntry]] =
-    ImageCatalogClient.loadImage(cacheDir)(c).map(Some.apply)//.sequenceU
-
-  private[image] def requestImageDownload(tpe: TpeContext): Task[Option[ImageEntry]] =
-    tpeCoordinates(tpe).map(ImageCatalogClient.loadImage(cacheDir)).sequenceU
-
-  /**
-    * Finds the coordinates for the base target of the tpe
-    */
-  private def tpeCoordinates(tpe: TpeContext): Option[Coordinates] =
-    for {
-      ctx <- tpe.obsContext
-      te  <- tpe.targets.base
-      when = ctx.getSchedulingBlockStart.asScalaOpt | Instant.now.toEpochMilli
-      c   <- te.getTarget.coords(when)
-    } yield c
 
   val ImageDownloadsThreadFactory = new ThreadFactory {
     private val threadNumber: AtomicInteger = new AtomicInteger(1)
@@ -103,16 +39,71 @@ object BackgroundImageLoader {
   /**
     * Execution context lets up to 4 low priority threads
     */
-  implicit val ec = Executors.newFixedThreadPool(4, ImageDownloadsThreadFactory)
+  val ec = Executors.newFixedThreadPool(4, ImageDownloadsThreadFactory)
+
+  /** Called when a program is created to download its images */
+  def watch(prog: ISPProgram): Unit = {
+      val targets = prog.getAllObservations.asScala.toList.flatMap(_.getObsComponents.asScala.map(n => (n, n.getDataObject)).collect {
+        case (node, t: TargetObsComp) =>
+          tpeCoordinates(TpeContext(node))
+      })
+      // remove duplicates
+      val tasks = targets.flatten.distinct.map(requestImageDownload)
+      // Run
+      runAsync(tasks) {
+        case \/-(e) => // done
+        case -\/(e) => println(e)
+      }(ec)
+    }
+
+  /** Called when a program is removed to clear the cache */
+  def unwatch(prog: ISPProgram): Unit = {
+    // not necessary
+  }
+
+  /******************************************
+    * Methods interacting with the java side
+    *****************************************/
+  /**
+    * Display an image if available on disk or request the download if necessary
+    */
+  def loadImageOnTheTpe(tpe: TpeContext): Unit = {
+    val t = tpeCoordinates(tpe).map(requestImageDownload).getOrElse(Task.now(()))
+
+    // This is called on an explicit user interaction so we'd rather
+    // Request the execution in a higher priority thread
+    // Execute and set the image on the tpe
+    runAsync(t) {
+      case \/-(_)       => // No image was found, don't do anything
+      case -\/(e)       => // Ignore the errors
+    }(Strategy.DefaultExecutorService)
+  }
 
   /**
-    * Utility methods to run the tasks on a separate thread
+    * Creates a task to load an image and set it on the tpe
     */
-  private def runAsync[A](tasks: List[Task[A]])(f: Throwable \/ List[A] => Unit) = {
+  private[image] def requestImageDownload(c: Coordinates): Task[Unit] =
+    ImageCatalogClient.loadImage(cacheDir)(c).map(setTpeImage)
+
+  /**
+    * Finds the coordinates for the base target of the tpe
+    */
+  private def tpeCoordinates(tpe: TpeContext): Option[Coordinates] =
+    for {
+      ctx <- tpe.obsContext
+      te  <- tpe.targets.base
+      when = ctx.getSchedulingBlockStart.asScalaOpt | Instant.now.toEpochMilli
+      c   <- te.getTarget.coords(when)
+    } yield c
+
+  /**
+    * Utility methods to run the tasks on separate threads of the pool
+    */
+  private def runAsync[A](tasks: List[Task[A]])(f: Throwable \/ List[A] => Unit)(pool: ExecutorService) = {
     Task.gatherUnordered(tasks.map(t => Task.fork(t))).unsafePerformAsync(f)
   }
 
-  private def runAsync[A](task: Task[A])(f: Throwable \/ A => Unit) =
+  private def runAsync[A](task: Task[A])(f: Throwable \/ A => Unit)(pool: ExecutorService) =
     Task.fork(task).unsafePerformAsync(f)
 
   /**
@@ -133,5 +124,4 @@ object BackgroundImageLoader {
       }
     }
   }
-
 }
