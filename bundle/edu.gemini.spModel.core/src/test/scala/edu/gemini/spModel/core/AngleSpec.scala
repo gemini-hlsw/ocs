bb777dbab6d748696870ebce7afc1261966effc1
Carlos Quiroz <cquiroz@gemini.edu>
2016-08-31 12:36:19 -0300

    REL-2859 Support Angle scalar division

--- bundle/edu.gemini.spModel.core/src/test/scala/edu/gemini/spModel/core/AngleSpec.scala
+++ bundle/edu.gemini.spModel.core/src/test/scala/edu/gemini/spModel/core/AngleSpec.scala
@@ -1,11 +1,8 @@
 package edu.gemini.spModel.core
 
 import scalaz._
-import Scalaz._
-import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
 import org.scalacheck.Prop._
-import org.scalacheck.Gen
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 import AlmostEqual.AlmostEqualOps
@@ -13,9 +10,9 @@ import AlmostEqual.AlmostEqualOps
 object AngleSpec extends Specification with ScalaCheck with Arbitraries with Helpers {
 
   "Angle Conversions" should {
-   
+
     "support Degrees" !
-      forAll { (a: Angle) =>  
+      forAll { (a: Angle) =>
         Angle.fromDegrees(a.toDegrees) ~= a
       }
 
@@ -28,20 +25,20 @@ object AngleSpec extends Specification with ScalaCheck with Arbitraries with Hel
       forAll { (a: Angle) =>
         Angle.fromArcmin(a.toDegrees * 60) ~= a
       }
-    
+
     "support Radians" !
-      forAll { (a: Angle) =>  
+      forAll { (a: Angle) =>
         Angle.fromRadians(a.toRadians) ~= a
       }
 
     "support HourAngle" !
-      forAll { (a: Angle) => 
+      forAll { (a: Angle) =>
         val hms = a.toHourAngle
         Angle.fromHourAngle(hms.hours, hms.minutes, hms.seconds).get ~= a
       }
 
     "support Sexigesimal" !
-      forAll { (a: Angle) => 
+      forAll { (a: Angle) =>
         val dms = a.toSexigesimal
         Angle.fromSexigesimal(dms.degrees, dms.minutes, dms.seconds).get ~= a
       }
@@ -49,21 +46,40 @@ object AngleSpec extends Specification with ScalaCheck with Arbitraries with Hel
   }
 
 
-  "Angle Scalar Multplication" should {
+  "Angle Scalar Multiplication" should {
 
     "have identity" !
-      forAll { (a: Angle) =>  
+      forAll { (a: Angle) =>
         a * 1 ~= a
       }
 
-    "have void" ! 
+    "have void" !
       forAll { (a: Angle) =>
         a * 0 ~= Angle.zero
       }
 
-    "be consistent with addition" ! 
+    "be consistent with addition" !
       forAll { (a: Angle) =>
         a * 2 ~= a + a
+      }
+
+  }
+
+  "Angle Scalar Division" should {
+
+    "have identity" !
+      forAll { (a: Angle) =>
+        a / 1 ~= Some(a)
+      }
+
+    "have void" !
+      forAll { (a: Angle) =>
+        a / 0 ~= None
+      }
+
+    "be consistent with multiplication" !
+      forAll { (a: Angle) =>
+        a * 0.5 ~= (a / 2).getOrElse(Angle.zero)
       }
 
   }
@@ -71,26 +87,26 @@ object AngleSpec extends Specification with ScalaCheck with Arbitraries with Hel
   "Angle Addition" should {
 
     "have left identity" !
-      forAll { (a: Angle) =>  
+      forAll { (a: Angle) =>
         Angle.zero + a ~= a
       }
 
     "have right identity" !
-      forAll { (a: Angle) =>  
+      forAll { (a: Angle) =>
         a + Angle.zero ~= a
       }
 
-    "commute" ! 
+    "commute" !
       forAll { (a: Angle, b: Angle) =>
         a + b ~= b + a
       }
 
-    "associate" ! 
+    "associate" !
       forAll { (a: Angle, b: Angle, c: Angle) =>
         a + (b + c) ~= (a + b) + c
       }
 
-    "be equivalent to subtraction when negated" ! 
+    "be equivalent to subtraction when negated" !
       forAll { (a: Angle, b: Angle) =>
         a + b ~= a - (b * -1)
       }
@@ -100,11 +116,11 @@ object AngleSpec extends Specification with ScalaCheck with Arbitraries with Hel
   "Angle Subtraction" should {
 
     "have right identity" !
-      forAll { (a: Angle) =>  
+      forAll { (a: Angle) =>
         a - Angle.zero ~= a
       }
 
-    "be equivalent to addition when negated" ! 
+    "be equivalent to addition when negated" !
       forAll { (a: Angle, b: Angle) =>
         a - b ~= a + (b * -1)
       }
@@ -119,19 +135,19 @@ object AngleSpec extends Specification with ScalaCheck with Arbitraries with Hel
         Angle.parseDegrees(s).fold(_ => false, _ ~= a)
       }
 
-    "Support Radians" ! 
+    "Support Radians" !
       forAll { (a: Angle) =>
         val s = a.toRadians.toString
         Angle.parseRadians(s).fold(_ => false, _ ~= a)
       }
 
-    "Support Sexigesimal" ! 
+    "Support Sexigesimal" !
       forAll { (a: Angle) =>
         val s = a.formatSexigesimal
         Angle.parseSexigesimal(s).fold(_ => false, _ ~= a)
       }
 
-    "Support HourAngle" ! 
+    "Support HourAngle" !
       forAll { (a: Angle) =>
         val s = a.formatHourAngle
         Angle.parseHourAngle(s).fold(_ => false, _ ~= a)
@@ -141,7 +157,7 @@ object AngleSpec extends Specification with ScalaCheck with Arbitraries with Hel
 
   "Angle Serialization" should {
 
-    "Support Java Binary" ! 
+    "Support Java Binary" !
       forAll { (a: Angle) =>
         canSerialize(a)
       }
