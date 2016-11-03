0857f6a051f349e1f5fd9a381896df9a486612b0
swalker2m <swalker2m@mac.com>
2016-11-03 13:19:03 -0300

    REL-2971: add 2017A ephemeris migration

--- bundle/edu.gemini.spModel.io/src/main/scala/edu/gemini/spModel/io/impl/migration/to2017A/To2017A.scala
+++ bundle/edu.gemini.spModel.io/src/main/scala/edu/gemini/spModel/io/impl/migration/to2017A/To2017A.scala
@@ -1,18 +1,26 @@
 package edu.gemini.spModel.io.impl.migration.to2017A
 
 import edu.gemini.pot.sp.SPComponentType
+import edu.gemini.spModel.core.{Coordinates, Ephemeris}
 import edu.gemini.spModel.gemini.gnirs.InstGNIRS
 import edu.gemini.spModel.io.impl.migration.Migration
 import edu.gemini.spModel.io.impl.migration.PioSyntax._
+import edu.gemini.spModel.pio.codec.ParamSetCodec
+import edu.gemini.spModel.target.TargetParamCodecs._
+import edu.gemini.spModel.target.TargetParamSetCodecs._
 
 import edu.gemini.spModel.pio.xml.PioXmlFactory
 import edu.gemini.spModel.pio.{ParamSet, Container, Pio, Document, Version}
+
+import scalaz._, Scalaz._
+
 
 object To2017A extends Migration {
 
   val version = Version.`match`("2017A-1")
 
-  val conversions: List[Document => Unit] = List(updateExecutedGnirs)
+  val conversions: List[Document => Unit] =
+    List(updateExecutedGnirs, updateNonSiderealTargets)
 
   val fact = new PioXmlFactory
 
@@ -35,5 +43,35 @@ object To2017A extends Migration {
     } Pio.addBooleanParam(fact, d, InstGNIRS.OVERRIDE_ACQ_OBS_WAVELENGTH_PROP.getName, false)
   }
 
+  // REL-2971: read pre-2017A non-sidereal ephemeris data and replace it with
+  // compressed ephemeris data.
+  private def updateNonSiderealTargets(d: Document): Unit = {
+    implicit val ephemerisElementParamSetCodec: ParamSetCodec[(Long, Coordinates)] =
+      ParamSetCodec.initial((0L, Coordinates.zero))
+        .withParam("time", Lens.firstLens[Long, Coordinates])
+        .withParamSet("coordinates", Lens.secondLens[Long, Coordinates])
 
+    val ephemerisElements: Ephemeris @> List[(Long, Coordinates)] =
+      Ephemeris.data.xmapB(_.toList)(==>>.fromList(_))
+
+    val OldEphemerisParamSetCodec: ParamSetCodec[Ephemeris] =
+      ParamSetCodec.initial(Ephemeris.empty)
+        .withParam("site", Ephemeris.site)
+        .withManyParamSet("ephemeris-element", ephemerisElements)
+
+    def convertEphemeris(ps: ParamSet): ParamSet =
+      OldEphemerisParamSetCodec.decode(ps) match {
+        case -\/(er) => throw new RuntimeException("Error reading pre-2017A ephemeris data: " + er)
+        case \/-(ep) => EphemerisParamSetCodec.encode("ephemeris", ep)
+      }
+
+    for {
+      t0 <- allTargets(d)
+      t1 <- t0.paramSet("target").toList
+      es <- t1.paramSet("ephemeris").toList
+    } {
+      t1.removeChild(es)
+      t1.addParamSet(convertEphemeris(es))
+    }
+  }
 }
