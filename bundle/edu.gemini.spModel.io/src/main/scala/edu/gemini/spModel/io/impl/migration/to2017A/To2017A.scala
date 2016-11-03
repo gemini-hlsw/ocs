6894b3f26406df507b14c2c2883a268c8570a7ac
swalker2m <swalker2m@mac.com>
2016-11-03 09:49:23 -0300

    REL-2971: tidy up GNIRS obs wavelength migration

--- bundle/edu.gemini.spModel.io/src/main/scala/edu/gemini/spModel/io/impl/migration/to2017A/To2017A.scala
+++ bundle/edu.gemini.spModel.io/src/main/scala/edu/gemini/spModel/io/impl/migration/to2017A/To2017A.scala
@@ -19,19 +19,21 @@ object To2017A extends Migration {
   // REL-2646: Updates executed GNIRS observations with a flag that tells the
   // sequence generation code to use the old, incorrect, observing wavelength
   // calculation that existed before 2017A.
-  private def updateExecutedGnirs(d: Document): Unit =
+  private def updateExecutedGnirs(d: Document): Unit = {
+    def gnirs(obs: Container): Option[ParamSet] =
+      for {
+        g <- obs.findContainers(SPComponentType.INSTRUMENT_GNIRS).headOption
+        d <- g.dataObject
+      } yield d
+
+    def hasGnirs(obs: Container): Boolean =
+      gnirs(obs).isDefined
+
     for {
       o <- findObservations(d)(c => hasGnirs(c) && isExecuted(c))
       d <- gnirs(o)
     } Pio.addBooleanParam(fact, d, InstGNIRS.OVERRIDE_ACQ_OBS_WAVELENGTH_PROP.getName, false)
+  }
 
-  private def gnirs(obs: Container): Option[ParamSet] =
-    for {
-      g <- obs.findContainers(SPComponentType.INSTRUMENT_GNIRS).headOption
-      d <- g.dataObject
-    } yield d
-
-  private def hasGnirs(obs: Container): Boolean =
-    gnirs(obs).isDefined
 
 }
