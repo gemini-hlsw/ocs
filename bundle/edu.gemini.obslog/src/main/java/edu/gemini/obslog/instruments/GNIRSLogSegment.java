da62e8b4bdaae37c61c9bb11e5a73ec6bd0b83a4
Sebastian Raaphorst <sraaphor@gemini.edu>
2017-04-04 17:10:34 -0300

    REL-1101: Fixed handling of CROSSDISPERSED_KEY to properly handle prism and filters.

--- bundle/edu.gemini.obslog/src/main/java/edu/gemini/obslog/instruments/GNIRSLogSegment.java
+++ bundle/edu.gemini.obslog/src/main/java/edu/gemini/obslog/instruments/GNIRSLogSegment.java
@@ -30,11 +30,10 @@ public class GNIRSLogSegment extends InstrumentLogSegment {
     private static final String READMODE_KEY = "readMode";
     private static final String COADDS_KEY = "coadds";
     private static final String EXPOSURETIME_KEY = "exposureTime";
-    private static final String WOLLASTON_KEY = "wollaston";
     private static final String PIXELSCALE_KEY = "pixelScale";
     private static final String CAMERA_KEY = "camera";
 
-    public GNIRSLogSegment(List<OlLogItem> logItems, OlLogOptions obsLogOptions) {
+    public GNIRSLogSegment(final List<OlLogItem> logItems, final OlLogOptions obsLogOptions) {
         super(SEG_TYPE, logItems, obsLogOptions);
     }
 
@@ -56,12 +55,9 @@ public class GNIRSLogSegment extends InstrumentLogSegment {
             }
         }
 
-        // Not sure if the above is right, if we make it here, we are divining the filter
+        // If we make it here, we are divining the filter.
         final String crossValue = map.sget(CROSSDISPERSED_KEY);
-        if (crossValue.equals("Yes")) {
-            filterValue = "XD";
-        } else {
-            // Cross dispersed = "no'
+        if (crossValue.equals("No")) {
             String lambdaValue = map.sget(WAVELENGTH_KEY);
             if (lambdaValue == null) return;
             try {
@@ -82,21 +78,23 @@ public class GNIRSLogSegment extends InstrumentLogSegment {
             } catch (NumberFormatException ex) {
                 filterValue = "?";
             }
+        } else {
+            filterValue = "XD";
         }
+
         // Finally, set the filter value for the not DARK and not in sequence section
         map.put(FILTER_KEY, filterValue);
     }
 
     // Note this replaces the "disperser" value with disperser/wavelength
-    private void _decorateDisperser(ConfigMap map) {
+    private void _decorateDisperser(final ConfigMap map) {
         if (map == null) return;
 
-        String value = map.sget(DISPERSER_KEY);
+        final String value = map.sget(DISPERSER_KEY);
         if (value == null) return;
-        GNIRSParams.Disperser disperser = GNIRSParams.Disperser.getDisperser(value);
+        final GNIRSParams.Disperser disperser = GNIRSParams.Disperser.getDisperser(value);
 
-        String wavelengthValue = map.sget(WAVELENGTH_KEY);
-
+        final String wavelengthValue = map.sget(WAVELENGTH_KEY);
         if (disperser != null && wavelengthValue != null) {
             map.put(DISPERSER_KEY, disperser.logValue() + '/' + wavelengthValue);
         }
@@ -104,65 +102,63 @@ public class GNIRSLogSegment extends InstrumentLogSegment {
 
     // Note this replaces the slitwidth value with the "log" value
     // Note at this time this doesn't bother with the decker the logs don't seem to either
-    private void _decorateSlit(ConfigMap map) {
+    private void _decorateSlit(final ConfigMap map) {
         if (map == null) return;
 
-        String value = map.sget(SLITWIDTH_KEY);
+        final String value = map.sget(SLITWIDTH_KEY);
         if (value == null) return;
 
-        GNIRSParams.SlitWidth s = GNIRSParams.SlitWidth.getSlitWidth(value);
+        final GNIRSParams.SlitWidth s = GNIRSParams.SlitWidth.getSlitWidth(value);
         if (s != null) map.put(SLITWIDTH_KEY, s.logValue());
     }
 
     // Note this changes the acqmirror value from "in" to "Y"
-    private void _decorateAcquisition(ConfigMap map) {
+    private void _decorateAcquisition(final ConfigMap map) {
         if (map == null) return;
 
-        String value = map.sget(ACQMIRROR_KEY);
+        final String value = map.sget(ACQMIRROR_KEY);
         map.put(ACQMIRROR_KEY, (value != null && value.equals("in")) ? "Y" : "");
     }
 
     // This method looks at the coadds, readmode, and exposureTime to synthesize the new complicated exposure time
-    private void _decorateExposureTime(ConfigMap map) {
+    private void _decorateExposureTime(final ConfigMap map) {
         if (map == null) return;
 
-        String readmodeValue = map.sget(READMODE_KEY);
+        final String readmodeValue = map.sget(READMODE_KEY);
         if (readmodeValue == null) {
             LOG.severe("No readMode found in GNIRS items");
         }
 
-        GNIRSParams.ReadMode rm = GNIRSParams.ReadMode.getReadMode(readmodeValue);
-        int lnrs = rm.getLowNoiseReads();
+        final GNIRSParams.ReadMode rm = GNIRSParams.ReadMode.getReadMode(readmodeValue);
+        final int lnrs = rm.getLowNoiseReads();
 
-        String coaddsValue = map.sget(COADDS_KEY);
-        if (coaddsValue == null) {
+        final String coaddsValue = ImOption.apply(map.sget(COADDS_KEY)).getOrElse(() -> {
             LOG.severe("No coaddsValue found in GNIRS items");
-            coaddsValue = "1";
-        }
+            return "1";
+        });
 
         // Note exposureTime is the entry that is reused for display
-        String exposureTimeValue = map.sget(EXPOSURETIME_KEY);
-        if (exposureTimeValue == null) {
+        final String exposureTimeValue = ImOption.apply(map.sget(EXPOSURETIME_KEY)).getOrElse(() -> {
             LOG.severe("No exposureTime property in GNIRS items");
-            exposureTimeValue = "1";
-        }
+            return "1";
+        });
 
         map.put(EXPOSURETIME_KEY, exposureTimeValue + '/' + lnrs + '/' + coaddsValue);
     }
 
     // Note this replaces the camera item with the camera/prism value
-    private void _decorateCamera(ConfigMap map) {
+    private void _decorateCamera(final ConfigMap map) {
         if (map == null) return;
 
-        String value = map.sget(WAVELENGTH_KEY);
+        final String value = map.sget(WAVELENGTH_KEY);
         if (value == null) return;
 
 
-        String pscaleValue = map.sget(PIXELSCALE_KEY);
-        GNIRSParams.PixelScale ps = GNIRSParams.PixelScale.getPixelScale(pscaleValue);
+        final String pscaleValue = map.sget(PIXELSCALE_KEY);
+        final GNIRSParams.PixelScale ps = GNIRSParams.PixelScale.getPixelScale(pscaleValue);
 
         String camera;
-        try{
+        try {
             double lambda = Double.parseDouble(value);
             if (ps == GNIRSParams.PixelScale.PS_015) {
                     camera = (lambda > 2.5) ? "SR" : "SB";
@@ -174,16 +170,9 @@ public class GNIRSLogSegment extends InstrumentLogSegment {
             camera = "?";
         }
 
-        String prism;
-        String crossValue = map.sget(CROSSDISPERSED_KEY);
-        LOG.info("*** crossValue=" + crossValue);
-        if (crossValue.equalsIgnoreCase("Yes")) {
-            prism = (ps == GNIRSParams.PixelScale.PS_015) ? "SXD" : "LXD";
-        } else {
-            // Not cross dispersed
-            prism = "MIR";
-        }
-
+        // REL-1101: crossValue is either No, SXD, or LXD. Translate No into MIR.
+        final String crossValue = map.sget(CROSSDISPERSED_KEY);
+        final String prism = crossValue.equals("No") ? "MIR" : crossValue;
         map.put(CAMERA_KEY, camera + '/' + prism);
     }
 
@@ -193,7 +182,7 @@ public class GNIRSLogSegment extends InstrumentLogSegment {
      *
      * @param map <code>UniqueConfigMap</code>
      */
-    public void decorateObservationData(ConfigMap map) {
+    public void decorateObservationData(final ConfigMap map) {
         _decorateFilter(map);
         _decorateSlit(map);
         _decorateDisperser(map);
