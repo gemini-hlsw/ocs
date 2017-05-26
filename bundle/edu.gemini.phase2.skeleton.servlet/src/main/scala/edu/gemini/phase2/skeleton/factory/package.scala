a41a7f65d8d95f3373989377796f4e75a909a0ef
sraaphorst <sraaphorst@gemini.edu>
2017-05-26 14:32:20 -0400

    REL-2928: Stupid autoformatting.

--- bundle/edu.gemini.phase2.skeleton.servlet/src/main/scala/edu/gemini/phase2/skeleton/factory/package.scala
+++ bundle/edu.gemini.phase2.skeleton.servlet/src/main/scala/edu/gemini/phase2/skeleton/factory/package.scala
@@ -11,14 +11,14 @@ package object factory {
     def usableObservations: List[Observation] = {
       // If there is an itac acceptance, then use its band assignment.  Otherwise
       // just figure we will use the "normal" band 1/2 observations.
-      val band =
-      (for {
+      val band = (for {
         itac <- proposal.proposalClass.itac
         accept <- itac.decision.right.toOption
       } yield accept.band).getOrElse(1) match {
         case 3 => Band.BAND_3
         case _ => Band.BAND_1_2
       }
+      
       proposal.observations.filter(obs => obs.band == band && obs.enabled)
     }
 
