a8c07380022dfc9f6034981c6e6ca0c8f98be0bf
Rob Norris <rob_norris@mac.com>
2017-06-01 20:32:58 -0700

    machine that goes ping

--- project/OcsBuild.scala
+++ project/OcsBuild.scala
@@ -19,4 +19,19 @@ object OcsBuild extends Build
       (ocsLibraryBundles    in ThisBuild) := ((baseDirectory in LocalRootProject).value / "lib" / "bundle").listFiles.filter(_.getName.endsWith(".jar")).toList
     )
 
+  // When running quiet builds it's important for there to be *some* output, otherwise Travis will
+  // time us out. By defining a system property we can turn on a periodic ping.
+  if (sys.props.isDefinedAt("edu.gemini.ocs.build.ping")) {
+    println("*** Keep-alive ping requested. The build will say PING! every minute or so.")
+    import java.util.{ Timer, TimerTask }
+    val t = new Timer("pinger", true)
+    t.scheduleAtFixedRate(new TimerTask {
+      var count = 0
+      def run() {
+        count += 1
+        println(s"*** PING! Minutes elapsed: rougly $count")
+      }
+    }, 1000 * 60, 1000 * 60)
+  }
+
 }
