af45a4de0ea331487362305ecfebe2eb0f5e8dae
Shane Walker <swalker@gemini.edu>
2017-06-29 16:07:57 -0400

    Modify SeqExecService to use the OcsExportServlet

--- bundle/edu.gemini.seqexec.odb/src/main/scala/edu/gemini/seqexec/odb/package.scala
+++ bundle/edu.gemini.seqexec.odb/src/main/scala/edu/gemini/seqexec/odb/package.scala
@@ -8,11 +8,10 @@ package object odb {
   type TrySeq[A] = SeqFailure \/ A
 
   def catchingAll[A](a: => A): TrySeq[A] =
-    try {
-      a.right
-    } catch {
-      case ex: Exception => SeqException(ex).left
-    }
+    \/.fromTryCatchNonFatal(a).leftMap(SeqException)
+
+  def trySeq[A](a: => TrySeq[A]): TrySeq[A] =
+    catchingAll(a).flatMap(identity)
 
   def closing[A <: { def close():Unit }, B](a:A)(f: A => B): B =
     try {
