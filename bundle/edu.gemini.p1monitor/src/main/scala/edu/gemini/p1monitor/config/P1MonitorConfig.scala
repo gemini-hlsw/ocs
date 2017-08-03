4122aa7e562ecea74ca7c6c93eb6dcf804a94239
Carlos Quiroz <cquiroz@gemini.edu>
2017-08-03 17:07:56 -0400

    REL-3151 Fixed a bug when selecting the correct template on the p1 monitor

--- bundle/edu.gemini.p1monitor/src/main/scala/edu/gemini/p1monitor/config/P1MonitorConfig.scala
+++ bundle/edu.gemini.p1monitor/src/main/scala/edu/gemini/p1monitor/config/P1MonitorConfig.scala
@@ -93,7 +93,7 @@ class P1MonitorConfig(ctx: BundleContext) {
         val to = (elementContent(_type, TO_TAG) ++ global_to).map(new InternetAddress(_))
         val cc = (elementContent(_type, CC_TAG) ++ global_cc).map(new InternetAddress(_))
         val bcc = (elementContent(_type, BCC_TAG) ++ global_bcc).map(new InternetAddress(_))
-        val template = (elementContent(_type, TEMPLATE_TAG) ++ global_bcc).headOption.map(toTemplate)
+        val template = elementContent(_type, TEMPLATE_TAG).headOption.orElse(Some(name)).map(toTemplate)
         (name, MonitoredDirectory(name, rootDir.head, username, group, to, cc, bcc, template.getOrElse(P1PDF.GeminiDefault)))
       }
     }
