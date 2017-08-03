e5f431fda0330cf08e31e023362efb505a9c36ef
Carlos Quiroz <cquiroz@gemini.edu>
2017-08-03 10:30:36 -0400

    REL-3151 Change the defaults for each partner according to spec #time 30m

--- bundle/edu.gemini.p1monitor/src/main/scala/edu/gemini/p1monitor/config/P1MonitorConfig.scala
+++ bundle/edu.gemini.p1monitor/src/main/scala/edu/gemini/p1monitor/config/P1MonitorConfig.scala
@@ -101,10 +101,13 @@ class P1MonitorConfig(ctx: BundleContext) {
   }
 
   private def toTemplate(s: String): P1PDF.Template = s match {
-    case "au" => P1PDF.AU
-    case "cl" => P1PDF.CL
-    case "us" => P1PDF.NOAO
-    case _    => P1PDF.GeminiDefaultListAtTheEnd
+    case "ar" | "br" | "kr" | "uh" => P1PDF.GeminiDefault
+    case "cfh" | "subaru" | "keck" => P1PDF.GeminiDefault
+    case "au"                      => P1PDF.AU
+    case "cl"                      => P1PDF.CL
+    case "us"                      => P1PDF.NOAO
+    case "ca"                      => P1PDF.GeminiDefaultListAtTheEnd
+    case _                         => P1PDF.GeminiDefaultListAtTheEnd
   }
 
   def getDirectories: Traversable[MonitoredDirectory] = map.values
