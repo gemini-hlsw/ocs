c42a3c1bd36e80e420939f0cb1cbba1fbb7d796a
Danny Navarro <j@dannynavarro.net>
2017-08-10 15:41:16 -0400

    REL-2835 Parametrize CandidateObsAttribute by time

--- bundle/edu.gemini.qpt.client/src/main/java/edu/gemini/qpt/ui/view/candidate/CandidateObsAttribute.java
+++ bundle/edu.gemini.qpt.client/src/main/java/edu/gemini/qpt/ui/view/candidate/CandidateObsAttribute.java
@@ -1,57 +1,59 @@
 package edu.gemini.qpt.ui.view.candidate;
 
 import java.util.Comparator;
+import java.util.function.Function;
 
+import edu.gemini.model.p1.immutable.Observation;
 import edu.gemini.qpt.shared.sp.Obs;
 import edu.gemini.qpt.shared.util.TimeUtils;
 
 enum CandidateObsAttribute {
 
-	SB(new Comparator<Obs>() {
+	SB((Long when) -> new Comparator<Obs>() {
 		public int compare(Obs o1, Obs o2) {
 			int ret = o1.getProg().getBand() - o2.getProg().getBand();
-			return ret != 0 ? ret : Observation.getComparator().compare(o1, o2);
+			return ret != 0 ? ret : Observation.getComparator(when).compare(o1, o2);
 		}
 	}),
 	
-	P(new Comparator<Obs>() {
+	P((Long when) -> new Comparator<Obs>() {
 		public int compare(Obs o1, Obs o2) {
 			int ret = o1.getPriority().compareTo(o2.getPriority());
-			return ret != 0 ? ret : Observation.getComparator().compare(o1, o2);
+			return ret != 0 ? ret : Observation.getComparator(when).compare(o1, o2);
 		}
 	}),
 	
-	Score(new Comparator<Obs>() {
+	Score((Long when) -> new Comparator<Obs>() {
 		public int compare(Obs o1, Obs o2) {
 			return 0; // HACK: THIS IS NEVER CALLED! SEE CandidateObsComparator.compare()
 		}
 	}),
 	
-	Observation(new Comparator<Obs>() {
+	Observation((Long when) -> new Comparator<Obs>() {
 		public int compare(Obs o1, Obs o2) {
 			return o1.compareTo(o2);
 		}
 	}), 
 	
-	Target(new Comparator<Obs>() {
+	Target((Long when) -> new Comparator<Obs>() {
 		public int compare(Obs o1, Obs o2) {
 			return o1.getTargetName().compareTo(o2.getTargetName());
 		}
 	}), 
 	
-	RA(new Comparator<Obs>() {
-		public int compare(Long when, Obs o1, Obs o2) {
+	RA((Long when) -> new Comparator<Obs>() {
+		public int compare(Obs o1, Obs o2) {
 			return (int) (100 * o1.getRa(when) - 100 * o2.getRa(when));
 		}
-	}), 
-	
-	Inst(new Comparator<Obs>() {
+	}),
+
+	Inst((Long when) -> new Comparator<Obs>() {
 		public int compare(Obs o1, Obs o2) {
 			return o1.getInstrumentString().compareTo(o2.getInstrumentString());
 		}
 	}), 
 	
-	Dur(new Comparator<Obs>() {
+	Dur((Long when) -> new Comparator<Obs>() {
 		public int compare(Obs o1, Obs o2) {
 			return (int) (o1.getRemainingTime() / TimeUtils.MS_PER_MINUTE - o2.getRemainingTime() / TimeUtils.MS_PER_MINUTE);
 		}
@@ -59,14 +61,14 @@ enum CandidateObsAttribute {
 	
 	;
 	
-	private final Comparator<Obs> comp;
-	
-	private CandidateObsAttribute(Comparator<Obs> comp) {
+	private final Function<Long, Comparator<Obs>> comp;
+
+	private CandidateObsAttribute(Function<Long, Comparator<Obs>> comp) {
 		this.comp = comp;
 	}
 
-	public Comparator<Obs> getComparator() {
-		return comp;
+	public Comparator<Obs> getComparator(Long when) {
+		return comp.apply(when);
 	}
 	
 }
