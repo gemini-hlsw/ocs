d1f8c5ee618d02b5336509addbd7399b699bd5df
sraaphorst <sraaphorst@gemini.edu>
2018-01-17 15:42:34 -0300

    REL-3351: Fixed badly written test case.

--- bundle/edu.gemini.model.p1/src/test/scala/edu/gemini/model/p1/dtree/inst/AlopekeSpec.scala
+++ bundle/edu.gemini.model.p1/src/test/scala/edu/gemini/model/p1/dtree/inst/AlopekeSpec.scala
@@ -3,7 +3,7 @@ package edu.gemini.model.p1.dtree.inst
 import org.specs2.mutable.Specification
 
 class AlopekeSpec extends Specification {
-  "The ʻAlopeke decision tree should" {
+  "The ʻAlopeke decision tree" should {
     "include ʻAlopeke modes" in {
       val alopeke = Alopeke()
       alopeke.title must beEqualTo("Mode")
