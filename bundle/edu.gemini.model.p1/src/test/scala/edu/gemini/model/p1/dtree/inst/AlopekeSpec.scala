d6ee533e5f8ac62a02ff8addafd3d75302436084
sraaphorst <sraaphorst@gemini.edu>
2018-02-06 13:12:52 -0300

    REL-3351: Changed okinas to apostrophes to avoid unicode issues.

--- bundle/edu.gemini.model.p1/src/test/scala/edu/gemini/model/p1/dtree/inst/AlopekeSpec.scala
+++ bundle/edu.gemini.model.p1/src/test/scala/edu/gemini/model/p1/dtree/inst/AlopekeSpec.scala
@@ -3,8 +3,8 @@ package edu.gemini.model.p1.dtree.inst
 import org.specs2.mutable.Specification
 
 class AlopekeSpec extends Specification {
-  "The ʻAlopeke decision tree" should {
-    "include ʻAlopeke modes" in {
+  "The 'Alopeke decision tree" should {
+    "include 'Alopeke modes" in {
       val alopeke = Alopeke()
       alopeke.title must beEqualTo("Mode")
       alopeke.choices must have size 2
