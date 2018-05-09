e1df17f7eec9bc133ef45afdc5c34f0510362b55
Shane Walker <swalker2m@users.noreply.github.com>
2018-05-09 09:01:53 -0300

    REL-902: add a Masks table (#1494)

---
+++ bundle/edu.gemini.qpt.client/src/main/java/edu/gemini/qpt/ui/view/mask/MaskDecorator.java
@@ -0,0 +1,28 @@
+package edu.gemini.qpt.ui.view.mask;
+
+import edu.gemini.ictd.CustomMaskKey;
+import edu.gemini.qpt.core.Schedule;
+import edu.gemini.spModel.ictd.Availability;
+import edu.gemini.ui.gface.GSubElementDecorator;
+import edu.gemini.ui.gface.GViewer;
+
+import java.util.Map;
+import javax.swing.JLabel;
+
+// Decorator just needed to format "SummitCabinet" as "Summit Cabinet".
+public final class MaskDecorator implements GSubElementDecorator<Schedule, Map.Entry<CustomMaskKey, Availability>, MaskAttribute> {
+
+    @Override
+    public void decorate(JLabel label, Map.Entry<CustomMaskKey, Availability> element, MaskAttribute subElement, Object value) {
+        switch (subElement) {
+            case Availability:
+                switch ((Availability) value) {
+                    case SummitCabinet: label.setText("Summit Cabinet"); break;
+                }
+        }
+    }
+
+    @Override
+    public void modelChanged(GViewer<Schedule, Map.Entry<CustomMaskKey, Availability>> viewer, Schedule oldModel, Schedule newModel) {
+    }
+}
