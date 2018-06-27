58a057d580fbf6f046fadbb7efde177441e6558a
Shane Walker <swalker2m@users.noreply.github.com>
2018-06-27 14:35:57 -0400

    REL-1714: publish plan with selected timezone (#1522)

--- bundle/edu.gemini.qpt.client/src/main/java/edu/gemini/qpt/core/Marker.java
+++ bundle/edu.gemini.qpt.client/src/main/java/edu/gemini/qpt/core/Marker.java
@@ -10,7 +10,7 @@ import edu.gemini.shared.util.immutable.Option;
 import edu.gemini.spModel.core.Site;
 
 /**
- * Represents a problem or annotation associated with a model object. 
+ * Represents a problem or annotation associated with a model object.
  * @author rnorris
  */
 public class Marker implements Comparable<Marker> {
@@ -18,14 +18,14 @@ public class Marker implements Comparable<Marker> {
 	public enum Severity {
 		Error, Warning, Notice, Info
 	}
-	
+
 	private final Object owner;
 	private final Object[] path;
 	private final String text;
 	private final Severity severity;
 	private final boolean qcOnly;
 	private final Option<Union<Interval>> union;
-	
+
 	public Marker(boolean qcOnly, Object owner, Severity severity, String text, Option<Union<Interval>> union, Object... path) {
 		this.qcOnly = qcOnly;
 		this.owner = owner;
@@ -42,7 +42,7 @@ public class Marker implements Comparable<Marker> {
 	public boolean isQcOnly() {
 		return qcOnly;
 	}
-	
+
 	public Object getOwner() {
 		return owner;
 	}
@@ -54,20 +54,24 @@ public class Marker implements Comparable<Marker> {
 	public Object getTarget() {
 		return path[path.length - 1];
 	}
-	
+
 	public String getText() {
 		return text;
 	}
 
-	private static final String formatInterval(Site site, Interval interval) {
+	private static final String formatInterval(Site site, Interval interval, TimePreference p) {
 		return String.format(
 				"%s - %s",
-				TimePreference.format(site, interval.getStart(), "HH:mm"),
-				TimePreference.format(site, interval.getEnd(), "HH:mm")
+				p.format(site, interval.getStart(), "HH:mm"),
+				p.format(site, interval.getEnd(), "HH:mm")
 		);
 	}
 
-    public String getUnionText(Site site) {
+	public String getUnionText(Site site) {
+		return getUnionText(site, TimePreference.BOX.get());
+	}
+
+    public String getUnionText(Site site, TimePreference p) {
 
 		final Union<Interval> intervals = this.getUnion().getOrElse(new Union<>());
 
@@ -75,7 +79,7 @@ public class Marker implements Comparable<Marker> {
 
 		String msg;
 		if (is.nonEmpty()) {
-			final String m = is.map(i -> formatInterval(site, i)).mkString(" ", ", ", ".");
+			final String m = is.map(i -> formatInterval(site, i, p)).mkString(" ", ", ", ".");
 			msg = this.getText() + m;
 		} else {
 			msg = this.getText();
@@ -83,8 +87,8 @@ public class Marker implements Comparable<Marker> {
 		return msg;
 	}
 
-	public String getFilteredUnionText(Site site) {
-		return this.getUnionText(site).replace("\u00B0", "&deg;");
+	public String getFilteredUnionText(Site site, TimePreference p) {
+		return this.getUnionText(site, p).replace("\u00B0", "&deg;");
 	}
 
     public Severity getSeverity() {
@@ -100,22 +104,22 @@ public class Marker implements Comparable<Marker> {
 
 	@SuppressWarnings("unchecked")
 	public int compareTo(Marker o) {
-		
+
 		// Sort first on severity
 		int diff = severity.compareTo(o.severity);
 		if (diff != 0) return diff;
-		
+
 		// Next sort by target, if they are the same type and
 		// are comparable.
 		Object t1 = getTarget();
 		Object t2 = o.getTarget();
 		if (t1 instanceof Comparable && t1.getClass().isInstance(t2) && !t1.equals(t2))
 			return ((Comparable) t1).compareTo(t2);
-			
+
 		// Otherwise order predictably but arbitrarily, grouping by target.
 		diff = t1.toString().compareTo(t2.toString());
 		return (diff == 0) ? System.identityHashCode(this) - System.identityHashCode(o) : diff;
-		
+
 	}
-		
+
 }
