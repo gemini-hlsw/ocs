24eb51c8e5abb1153df19763c4b6f9087d334582
Shane Walker <swalker2m@users.noreply.github.com>
2018-08-30 10:17:17 -0300

    REL-1958: show which configuration items are missing (#1570)

--- bundle/edu.gemini.qv.plugin/src/main/scala/edu/gemini/qv/plugin/table/InstallationState.scala
+++ bundle/edu.gemini.qv.plugin/src/main/scala/edu/gemini/qv/plugin/table/InstallationState.scala
@@ -1,5 +1,7 @@
 package edu.gemini.qv.plugin.table
 
+import scalaz.NonEmptyList
+
 /**
  * Instrument component installation state, according to the ICTD.
  */
@@ -8,11 +10,11 @@ sealed trait InstallationState extends Product with Serializable
 object InstallationState {
 
   /** All instrument components and custom mask (if any) known to be installed. */
-  case object AllInstalled     extends InstallationState
+  case object AllInstalled extends InstallationState
 
   /** One or more components or custom mask known to be not installed. */
-  case object SomeNotInstalled extends InstallationState
+  final case class SomeNotInstalled(missing: NonEmptyList[String]) extends InstallationState
 
   /** Unknown installation state (ICTD unavailable). */
-  case object Unknown          extends InstallationState
+  case object Unknown extends InstallationState
 }
