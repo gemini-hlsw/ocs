dd8c4ee35a39b52f8fde53cd89a4924c0099c952
sraaphorst <sraaphorst@gemini.edu>
2018-11-28 15:11:39 -0300

    REL-3552: Adaptive optics not supported for GMOS-N.

--- bundle/edu.gemini.model.p1/src/main/scala/edu/gemini/model/p1/immutable/transform/UpConverter.scala
+++ bundle/edu.gemini.model.p1/src/main/scala/edu/gemini/model/p1/immutable/transform/UpConverter.scala
@@ -145,6 +145,24 @@ case class LastStepConverter(semester: Semester) extends SemesterConverter {
  * This converter will upgrade to 2019A.
  */
 case object SemesterConverter2018BTo2019A extends SemesterConverter {
+    // REL-3552: No GMOS-N + Altair offered for 2019A. Remove Altair from component.
+    // The name of the instrument is auto-generated from its properties, so we don't worry about the <name>...</name> tag.
+    private lazy val gmosnAltairLGSRE  = " Altair Laser Guidestar( w/ (PWFS1|OIWFS))?"
+    private lazy val gmosnAltairNGSRE  = " Altair Natural Guidestar( w/ Field Lens)?"
+    lazy val gmosnAltairRemoverMessage = "GMOS-N does not offer Altair in 2019A. Altair component removed."
+    val gmosnAltairRemover: TransformFunction = {
+      case p @ <gmosN>{ns @ _*}</gmosN> if (p \\ "altair" \ "lgs").nonEmpty || (p \\ "altair" \\ "ngs").nonEmpty =>
+        object GmosNAltairRemover extends BasicTransformer {
+          override def transform(n: xml.Node): xml.NodeSeq = n match {
+            case <altair>{_ @ _*}</altair> => <altair><none/></altair>
+            case <name>{name}</name>       => <name>{name.text.replaceFirst(gmosnAltairLGSRE, "").replaceFirst(gmosnAltairNGSRE, "")}</name>
+            case elem: xml.Elem            => elem.copy(child = elem.child.flatMap(transform))
+            case _                         => n
+          }
+        }
+        StepResult(gmosnAltairRemoverMessage, <gmosN>{GmosNAltairRemover.transform(ns)}</gmosN>).successNel
+    }
+
   // REL-3485: F2 MOS not available for 2019A.
   val f2MOSNotAvailable: TransformFunction = {
     case f @ <flamingos2>{ns @ _*}</flamingos2> if (f \ "mos").nonEmpty =>
@@ -164,31 +182,13 @@ case object SemesterConverter2018BTo2019A extends SemesterConverter {
       StepResult("All CFH exchange time will now be done in queue.", <proposalClass>{ClassicalToQueueTransformer.transform(ns)}</proposalClass>).successNel
   }
 
-  override val transformers = List(f2MOSNotAvailable, cfhClassicalToQueue)
+  override val transformers = List(gmosnAltairRemover, f2MOSNotAvailable, cfhClassicalToQueue)
 }
 
 /**
  * This converter will upgrade to 2018B.
  */
 case object SemesterConverter2018ATo2018B extends SemesterConverter {
-  // REL-3363: No GMOS-N + Altair offered for 2018B. Remove Altair from component.
-  // The name of the instrument is auto-generated from its properties, so we don't worry about the <name>...</name> tag.
-//  private lazy val gmosnAltairLGSRE  = " Altair Laser Guidestar( w/ (PWFS1|OIWFS))?"
-//  private lazy val gmosnAltairNGSRE  = " Altair Natural Guidestar( w/ Field Lens)?"
-//  lazy val gmosnAltairRemoverMessage = "GMOS-N does not offer Altair in 2018B. Altair component removed."
-//  val gmosnAltairRemover: TransformFunction = {
-//    case p @ <gmosN>{ns @ _*}</gmosN> if (p \\ "altair" \ "lgs").nonEmpty || (p \\ "altair" \\ "ngs").nonEmpty =>
-//      object GmosNAltairRemover extends BasicTransformer {
-//        override def transform(n: xml.Node): xml.NodeSeq = n match {
-//          case <altair>{_ @ _*}</altair> => <altair><none/></altair>
-//          case <name>{name}</name>       => <name>{name.text.replaceFirst(gmosnAltairLGSRE, "").replaceFirst(gmosnAltairNGSRE, "")}</name>
-//          case elem: xml.Elem            => elem.copy(child = elem.child.flatMap(transform))
-//          case _                         => n
-//        }
-//      }
-//      StepResult(gmosnAltairRemoverMessage, <gmosN>{GmosNAltairRemover.transform(ns)}</gmosN>).successNel
-//  }
-
   lazy val dssiGNToAlopekeMessage: String = "DSSI Gemini North proposal has been migrated to ʻAlopeke instead."
   val dssiGNToAlopeke: TransformFunction = {
     case p @ <dssi>{ns @ _*}</dssi> if (p \\ "Dssi" \\ "site").map(_.text).exists(_.equals(Site.GN.name)) =>
