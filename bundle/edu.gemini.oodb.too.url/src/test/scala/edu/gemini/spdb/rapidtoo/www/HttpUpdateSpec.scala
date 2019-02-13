7a19cbf942cc39744ccff5261f7b80ba4ffbf6f4
Shane Walker <swalker2m@users.noreply.github.com>
2019-02-13 16:17:40 -0300

    REL-3609: addendum (#1626) (#1628)
    Approved in release branch, so I'll merge this.

--- bundle/edu.gemini.oodb.too.url/src/test/scala/edu/gemini/spdb/rapidtoo/www/HttpUpdateSpec.scala
+++ bundle/edu.gemini.oodb.too.url/src/test/scala/edu/gemini/spdb/rapidtoo/www/HttpUpdateSpec.scala
@@ -4,6 +4,7 @@ package edu.gemini.spdb.rapidtoo.www
 import edu.gemini.shared.util.immutable.ImOption
 import edu.gemini.spdb.rapidtoo.www.HttpTargetSpec._
 
+import org.scalacheck.Gen
 import org.scalacheck.Prop.forAll
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
@@ -28,31 +29,24 @@ object HttpUpdateSpec extends Specification with ScalaCheck with Arbitraries wit
     "dec"      -> "-52:41:44.39"
   )
 
+  val minSec = HttpTooUpdate.MIN_EXPOSURE_TIME.getSeconds.toInt
+  val maxSec = HttpTooUpdate.MAX_EXPOSURE_TIME.getSeconds.toInt
+
   "HttpTooUpdate" should {
 
     def mustFail(req: HttpServletRequest) =
       new HttpTooUpdate(req) must throwA[BadRequestException]
 
-    "exptime: fail if out of range" ! forAll { (e: Int) =>
-      val req2 = req.modifiedWith("exptime" -> e.toString)
-      if (e < 1 || e > 300) mustFail(req2)
-      else (new HttpTooUpdate(req2)).getExposureTime() must_== ImOption.apply(Duration.ofSeconds(e))
+    "exptime: fail if out of range" ! forAll(Gen.oneOf(Gen.choose(Int.MinValue, minSec-1), Gen.choose(maxSec+1, Int.MaxValue))) { (e: Int) =>
+      mustFail(req.modifiedWith("exptime" -> e.toString))
+    }
+
+    "exptime: succeed if in range" ! forAll(Gen.choose(minSec, maxSec)) { (e: Int) =>
+       (new HttpTooUpdate(req.modifiedWith("exptime" -> e.toString))).getExposureTime() must_== ImOption.apply(Duration.ofSeconds(e))
     }
 
     "exptime: fail if not parseable" in {
       mustFail(req.modifiedWith("exptime" -> "1.4"))
-    }
-
-    "exptime: fail if too small" in {
-      mustFail(req.modifiedWith("exptime" -> "0"))
-    }
-
-    "exptime: fail if too big" in {
-      mustFail(req.modifiedWith("exptime" -> "301"))
-    }
-
-    "exptime: succeed if 1 <= exptime <= 300" in {
-       (new HttpTooUpdate(req.modifiedWith("exptime" -> "6"))).getExposureTime() must_== ImOption.apply(Duration.ofSeconds(6L))
     }
 
     "exptime: succeed if not specified" in {
