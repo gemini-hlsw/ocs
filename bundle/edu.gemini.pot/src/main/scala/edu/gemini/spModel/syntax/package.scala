59731736d9d31da6e25f1e10701eb3c6f7537867
Shane Walker <swalker2m@users.noreply.github.com>
2019-03-13 16:26:03 -0300

    REL-3636: add acq cam, phoenix and texes to the time accounting rule exception (#1642)

--- bundle/edu.gemini.pot/src/main/scala/edu/gemini/spModel/syntax/package.scala
+++ bundle/edu.gemini.pot/src/main/scala/edu/gemini/spModel/syntax/package.scala
@@ -2,10 +2,11 @@ package edu.gemini.spModel
 
 import edu.gemini.spModel.syntax.event.ToObsExecEventOps
 import edu.gemini.spModel.syntax.skycalc.{ToObservingNightOps, ToUnionOps}
-import edu.gemini.spModel.syntax.sp.ToNodeOps
+import edu.gemini.spModel.syntax.sp.{ToInstrumentOps, ToNodeOps}
 
 package object syntax {
-  object all extends ToNodeOps
+  object all extends ToInstrumentOps
+                with ToNodeOps
                 with ToObsExecEventOps
                 with ToObservingNightOps
                 with ToUnionOps
