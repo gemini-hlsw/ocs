f84303dc005076a18faeb756491b7ef2e26c4719
sraaphorst <sraaphorst@gemini.edu>
2019-08-02 18:19:56 -0400

    REL-3714: Undo debugging changes to BlueprintEditor.

--- bundle/edu.gemini.pit/src/main/scala/edu/gemini/pit/ui/editor/BlueprintEditor.scala
+++ bundle/edu.gemini.pit/src/main/scala/edu/gemini/pit/ui/editor/BlueprintEditor.scala
@@ -71,7 +71,7 @@ class BlueprintEditor private (initialState:UIState[_, _], editable:Boolean) ext
   // Finish action
   def finish = nextState match {
     case Some(Right((_, bp: BlueprintBase))) => bp
-    case _ => sys.error("Unexpected final state " + nextState) // TODO: this, more safely
+    case _                                   => sys.error("Unexpected final state " + nextState) // TODO: this, more safely
   }
 
   // What would the next state be, based on the current selection?
