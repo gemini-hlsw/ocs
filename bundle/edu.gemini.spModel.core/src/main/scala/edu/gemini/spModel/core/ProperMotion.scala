0597d933dec74e8885f613ca60447536c7f08bd5
sraaphorst <sraaphorst@gemini.edu>
2019-11-19 18:41:25 -0300

    REL-3179: Calculations compiling.

--- bundle/edu.gemini.spModel.core/src/main/scala/edu/gemini/spModel/core/ProperMotion.scala
+++ bundle/edu.gemini.spModel.core/src/main/scala/edu/gemini/spModel/core/ProperMotion.scala
@@ -5,7 +5,7 @@ import java.time.Instant
 import scalaz._
 import Scalaz._
 
-import scala.math.{cos, sin}
+import scala.math.{atan2, cos, hypot, sin}
 
 /**
  * Specification of proper motion.
@@ -49,9 +49,22 @@ object ProperMotion {
   private val secsPerDay:  Double = 86400.0
   private val auPerKm:     Double = 1000.0 / AstronomicalUnit.toDouble
   private val radsPerAsec: Double = Angle.fromArcsecs(1).toArcsecs
+  private val TwoPi:       Double = 6.283185307179586476925286766559
 
   private type Vec2 = (Double, Double)
   private type Vec3 = (Double, Double, Double)
+
+  // Scalar multiplication and addition for Vec3.
+  private implicit class Vec3Ops(a: Vec3) {
+    def *(d: Double): Vec3 =
+      (a._1 * d, a._2 * d, a._3 * d)
+
+    def |+|(d: Double): Vec3 =
+      (a._1 + d, a._2 + d, a._3 + d)
+
+    def |+|(other: Vec3): Vec3 =
+      (a._1 + other._1, a._2 + other._2, a._3 + other._3)
+  }
 
   /**
    * Calculator to determine the new coordinates due to proper motion from epoch after elapsed years.
@@ -91,9 +104,28 @@ object ProperMotion {
     // Change per year due to proper velocity
     val dPos2 = (
       -dRa * pos._2 - dDec * cos(ra) * sin(dec),
-      dRa * pos._1 - dDec * sin(ra) * sin(dec),
-      dDec *           cos(dec)
+       dRa * pos._1 - dDec * sin(ra) * sin(dec),
+                      dDec *           cos(dec)
     )
+
+    // Our new position (still in polar coordinates). `|+|` here is scalar addition provided by
+    // cats … unlike scalaz it does give you Semigroup[Double] even though it's not strictly lawful.
+    val pp = pos |+| ((dPos1 |+| dPos2) * elapsedYears)
+
+    // Back to spherical
+    val (x, y, z) = pp
+    val r    = hypot(x, y)
+    val rap  = if (r === 0.0) 0.0 else atan2(y, x)
+    val decp = if (z === 0.0) 0.0 else atan2(z, r)
+    val rapp = {
+      // Normalize to [0 .. 2π)
+      val rem = rap % TwoPi
+      if (rem < 0.0) rem + TwoPi else rem
+    }
+
+    // TODO: What to do if Declination is None?
+    Coordinates(RA.fromAngle(Angle.fromRadians(rapp)),
+                Declination.fromAngle(Angle.fromRadians(decp)).getOrElse(Declination.zero))
   }
 
   val zero = ProperMotion(RightAscensionAngularVelocity.Zero, DeclinationAngularVelocity.Zero, Epoch.J2000)
