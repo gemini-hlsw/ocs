15fbe0d62bcbca4ec4a8f3176781c39df4c0a14b
Sebastian Raaphorst <sraaphorst@gmail.com>
2020-05-06 15:28:04 -0400

    REL-3727: Fixing baseline tests.

--- bundle/edu.gemini.itc.web/src/test/scala/edu/gemini/itc/web/baseline/BaselineTest.scala
+++ bundle/edu.gemini.itc.web/src/test/scala/edu/gemini/itc/web/baseline/BaselineTest.scala
@@ -25,7 +25,7 @@ import org.junit.{Ignore, Test}
  *
  */
 class BaselineTest {
-  //@Ignore
+  @Ignore
   @Test
   def create(): Unit = {
     val baseSeq = baselines()
@@ -39,7 +39,7 @@ class BaselineTest {
     System.out.println(s"Writing new baseline with ${baseSeq.size} entries")
     Baseline.write(baseSeq)
   }
-  @Ignore
+  //@Ignore
   @Test
   def checkAll(): Unit =
     baselines().foreach { b => assertTrue(Baseline.checkAgainstBaseline(b)) }
