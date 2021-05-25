d1256d15f6f5c5cf487b8758f3424cdb0a4685d5
Sebastian Raaphorst <sraaphorst@gmail.com>
2021-05-24 21:15:50 -0400

    REL-3934: Request for comments.

--- bundle/edu.gemini.programexport.servlet/src/main/scala/edu/gemini/programexport/servlet/ProgramExportServlet.scala
+++ bundle/edu.gemini.programexport.servlet/src/main/scala/edu/gemini/programexport/servlet/ProgramExportServlet.scala
@@ -8,9 +8,14 @@ import edu.gemini.pot.spdb.IDBDatabaseService
 import edu.gemini.spModel.core.{ProgramId, SPProgramID}
 import scalaz._
 import Scalaz._
-import edu.gemini.pot.sp.{ISPProgram, SPComponentType}
-import edu.gemini.spModel.gemini.obscomp.SPProgram
+import edu.gemini.pot.sp.{ISPGroup, ISPNode, ISPObsComponent, ISPObservation, ISPProgram, SPComponentType}
+import edu.gemini.shared.util.immutable.ScalaConverters._
+import edu.gemini.spModel.data.YesNoType
+import edu.gemini.spModel.gemini.obscomp.{SPProgram, SPSiteQuality}
+import edu.gemini.spModel.obs.SPObservation
 import edu.gemini.spModel.obscomp.{ProgramNote, SPGroup, SPNote, SchedNote}
+import edu.gemini.spModel.target.env.TargetEnvironment
+import edu.gemini.spModel.util.SPTreeUtil
 
 import javax.servlet.ServletException
 import scala.collection.JavaConverters._
@@ -26,121 +31,163 @@ final case class ProgramExportServlet(odb: IDBDatabaseService, user: Set[Princip
     doRequest(request, response)
 
   def doRequest(request: HttpServletRequest, response: HttpServletResponse): Unit =
-    // Attempt to retrieve the program name: ProgramID instead?
+  // Attempt to retrieve the program name: ProgramID instead?
     ParamParser(request).id.fold(t => throw new ServletException(t), build(_, response))
 
   // Build the JSON in a bottom-up approach starting at ISPProgram.
   // Traverse a node's children and on the way back up, add the information for the node.
   // Right now we just return an empty String: whether to use Strings or Argonaut to build the
   // JSON is a decision I'm leaving to the end.
-  def build(id: SPProgramID, response: HttpServletResponse): String = {
-    val out: BufferedOutputStream = new BufferedOutputStream(response.getOutputStream)
-    response.setContentType("text/plain")
-    out.write(s"Program name received: $id\n".getBytes)
+  def build(id: SPProgramID, response: HttpServletResponse): Unit =
+    Option(odb.lookupProgramByID(id)) match {
+      case Some(ispProgram) =>
+        val out: BufferedOutputStream = new BufferedOutputStream(response.getOutputStream)
+        response.setContentType("text/plain")
 
-    val ispProgram = odb.lookupProgramByID(id)
-    Option(odb.lookupProgramByID(id)).flatMap(p => Option(p.getDataObject.asInstanceOf[SPProgram])) match {
+        // TODO: Remove
+        out.write(s"Program name received: $id\n".getBytes)
+
+        val json = process(ispProgram)
+        out.write(json.getBytes)
+        out.close()
       case None =>
-        Log.severe(s"Program $id is not in the database!\n")
-        throw new ServletException(s"Program $id does not exist in database.")
-      case Some(spProg)
-    "Hello cruel world."
-    }
-  }
-  def doRequest(id: SPProgramID, response: HttpServletResponse): Unit = {
-    val out: BufferedOutputStream = new BufferedOutputStream(response.getOutputStream)
-    response.setContentType("text/plain")
-    out.write(s"Program name received: $id\n".getBytes)
-
-    val ispProgram = odb.lookupProgramByID(id)
-    Option(odb.lookupProgramByID(id)).flatMap(p => Option(p.getDataObject.asInstanceOf[SPProgram])) match {
-      case None =>
-        Log.severe(s"Program $id is not in the database!\n")
-        throw new ServletException(s"Program $id does not exist in database.")
-      case Some(spProg) =>
-        // NOTE: Some of these can be null. Indicated for each field.
-        val gsa = spProg.getGsaAspect
-
-        // Is this all needed? Any other PI info? Not specified in doc.
-//        val piFirstName = spProg.getPIFirstName
-//        val piLastName  = spProg.getPILastName
-//        val piEmail     = spProg.getPIInfo.getEmail // Needed?
-
-        // Investigator data
-        val gsaPhase1Data = spProg.getGsaPhase1Data // not null
-        gsaPhase1Data.getCois.asScala.foreach { inv =>
-          val first = inv.getFirst
-          val last = inv.getLast
-          val email = inv.getEmail
-        }
-
-        val affiliate = spProg.getPIAffiliate.displayValue // could be null
-        val queueBand = spProg.getQueueBand // String, empty if classical
-        val rolloverFlag = spProg.getRolloverStatus // boolean
-        val isThesis = spProg.isThesis // boolean
-        val programMode = spProg.getProgramMode // isQueue / isClassical // not null
-        val tooType = spProg.getTooType.getDisplayValue // not null
-        // TODO: In what format?
-        val awardedTime = spProg.getAwardedProgramTime // TimeValue, not null
-
-        // All non-null
-        val timeAcctAllocation = spProg.getTimeAcctAllocation
-        val timeAcctCategories = timeAcctAllocation.getCategories
-        timeAcctCategories.asScala.foreach { category =>
-          // Times are Durations. Bryan wants int seconds or nights if nights are specified.
-          // Not sure how possible this is?
-          val timeAcctAward = timeAcctAllocation.getAward(category)
-          val programTime = timeAcctAward.getProgramAward
-          val partnerTime = timeAcctAward.getPartnerAward
-        }
-
-        // Program-specific notes: must go through the components and check type.
-        // The obs components for all of these types are SPNotes or subclasses.
-        val ProgramNotes: Set[SPComponentType] = Set(
-          SPComponentType.INFO_NOTE,
-          SPComponentType.INFO_PROGRAMNOTE,
-          SPComponentType.INFO_SCHEDNOTE
-        )
-        ispProgram
-          .getObsComponents.asScala
-          .filter(note => ProgramNotes.contains(note.getType))
-          .map(_.getDataObject.asInstanceOf[SPNote])
-          .foreach { note =>
-            val noteTitle = note.getTitle
-            val noteText  = note.getNote
-          }
-
-        // Scheduling groups and their observations.
-        ispProgram
-          .getGroups.asScala
-          .map(_.getDataObject.asInstanceOf[SPGroup])
-          .foreach { group =>
-            val groupKey   = group.
-            val groupTitle = group.getTitle
-          }
-
-
-        out.write(s"ToO type: ${spProg.getTooType}".getBytes)
+        val error = s"Program $id is not in the database!"
+        Log.severe(error)
+        throw new ServletException(error)
     }
 
-    out.close()
+  // Recursive method to build up the JSON representation of the program.
+  def process(node: ISPNode): String = {
+    node match {
+      case p: ISPProgram => processProgram(p)
+      case g: ISPGroup => processGroup(g)
+
+      // Other obs component.
+      case n: ISPObsComponent => processObsComponent(n)
+      case _ => ""
+    }
   }
 
-  def processProgram(ispProg: ISPProgram, out: BufferedOutputStream, response: HttpServletResponse): Unit = {
+  // Combine components at the same level into a JSON list.
+  def combine(lst: List[String]): String =
+    ""
 
+  // Top-level program information.
+  def processProgram(ispProgram: ISPProgram): String = {
+    val result = combine(ispProgram.getObsComponents.asScala.map(process).toList)
+
+    // NOTE: Some of these can be null. Indicated for each field.
+    val spProg = ispProgram.getDataObject.asInstanceOf[SPProgram]
+    val gsa = spProg.getGsaAspect
+
+    // Is this all needed? Any other PI info? Not specified in doc.
+    //        val piFirstName = spProg.getPIFirstName
+    //        val piLastName  = spProg.getPILastName
+    //        val piEmail     = spProg.getPIInfo.getEmail // Needed?
+
+    // Investigator data
+    val gsaPhase1Data = spProg.getGsaPhase1Data // not null
+    gsaPhase1Data.getCois.asScala.foreach { inv =>
+      val first = inv.getFirst
+      val last = inv.getLast
+      val email = inv.getEmail
+    }
+
+    val affiliate = spProg.getPIAffiliate.displayValue // could be null
+    val queueBand = spProg.getQueueBand // String, empty if classical
+    val rolloverFlag = spProg.getRolloverStatus // boolean
+    val isThesis = spProg.isThesis // boolean
+    val programMode = spProg.getProgramMode // isQueue / isClassical // not null
+    val tooType = spProg.getTooType.getDisplayValue // not null
+    // TODO: In what format?
+    val awardedTime = spProg.getAwardedProgramTime // TimeValue, not null
+
+    // All non-null
+    val timeAcctAllocation = spProg.getTimeAcctAllocation
+    val timeAcctCategories = timeAcctAllocation.getCategories
+    timeAcctCategories.asScala.foreach { category =>
+      // Times are Durations. Bryan wants int seconds or nights if nights are specified.
+      // Not sure how possible this is?
+      val timeAcctAward = timeAcctAllocation.getAward(category)
+      val programTime = timeAcctAward.getProgramAward
+      val partnerTime = timeAcctAward.getPartnerAward
+    }
+
+    ""
   }
 
-    // Handle an illegal program name.
-//  def illegalArgument(ex: IllegalArgumentException): Unit = {
-//      response.setStatus(ProgramExportServlet.ResponseCode.InvalidProgram.code)
-//      out.write(s"ERROR: ${ex.getMessage}".getBytes)
-//    }
-//
-//    val pathInfo = request.getPathInfo
-//    out.write("Hello world".getBytes)
-//
-//    out.close()
-//  }
+  // Handling of obs components that don't have an ISP.
+  def processObsComponent(n: ISPObsComponent): String = n.getType match {
+      // TODO: Do we need all of these?
+      case SPComponentType.INFO_PROGRAMNOTE => //| SPComponentType.INFO_SCHEDNOTE | SPComponentType.INFO_NOTE =>
+        processProgramNote(n.getDataObject.asInstanceOf[SPNote])
+    }
+
+  // Program-specific notes: must go through the components and check type.
+  // The obs components for all of these types are SPNotes or subclasses.
+  def processProgramNote(note: SPNote): String = {
+    val title = note.getTitle
+    val text  = note.getNote
+    ""
+  }
+
+  def processGroup(ispGroup: ISPGroup): String = {
+    val spGroup = ispGroup.getDataObject.asInstanceOf[SPGroup]
+    val key = ispGroup.getNodeKey
+    val name = spGroup.getTitle
+
+    // Get a list of observation node keys for the scheduling group
+    val observationIds = combine(ispGroup.getObservations.asScala.map {
+      _.getNodeKey.toString
+    }.toList)
+
+    ""
+  }
+
+  def processObservation(ispObservation: ISPObservation): String = {
+    val spObservation = ispObservation.getDataObject.asInstanceOf[SPObservation]
+    val id = ispObservation.getNodeKey
+    val title = spObservation.getTitle
+    val phase2Status = spObservation.getPhase2Status.displayValue
+
+    // TODO: If this is None, which it could be, do we want to display it?
+    val execStatusOverride = spObservation.getExecStatusOverride.asScalaOpt.map(_.displayValue)
+
+    val priority = spObservation.getPriority.displayValue
+    val tooOverrideRapid = spObservation.isOverrideRapidToo.toYesNo.displayValue
+
+    // TODO: Don't know if this is correct way to handle this.
+    val setupTimeType = spObservation.getSetupTimeType.toString
+
+    // TODO: Also need acq overhead. Where is this? In setup time type?
+
+    // Site quality is an obscomponent but not wrapped in an ISP object.
+    // Is this the proper way to get at it? Otherwise we'll have to match on
+    // SPComponentType instead of ISPNode, I think.
+    val conditions = SPTreeUtil.findObsCondNode(ispObservation).getDataObject.asInstanceOf[SPSiteQuality]
+    val cc = conditions.getCloudCover.displayValue
+    val sb = conditions.getSkyBackground.displayValue
+    val iq = conditions.getImageQuality.displayValue
+    val wv = conditions.getWaterVapor.displayValue
+
+    val ecType = conditions.getElevationConstraintType.displayValue
+    val ecMin = conditions.getElevationConstraintMin
+    val ecMax = conditions.getElevationConstraintMax
+
+    val timingWindows = combine(conditions.getTimingWindows.asScala.map { tw =>
+      // TODO: in ms. Is this what we want?
+      val start = tw.getStart
+      val duration = tw.getDuration
+      val period = tw.getPeriod
+
+      val repeat = tw.getRepeat
+      ""
+    }.toList)
+
+    // TODO: Now I need a TargetEnvironment... Do I go through ObsContext?
+
+    ""
+  }
 }
 
 object ProgramExportServlet {
@@ -148,27 +195,15 @@ object ProgramExportServlet {
 
   val IdParam: String = "id"
   case class ParamParser(req: HttpServletRequest) {
-    def id: Throwable \/ SPProgramID = {
+    def id: Throwable \/ SPProgramID =
       \/.fromTryCatchNonFatal(SPProgramID.toProgramID(req.getParameter(IdParam))).leftMap { t =>
         Log.log(Level.SEVERE, "Problem running ProgramExportServlet", t)
         t
-        //    } match {
-        //        case \/-(id) => \/.right(id)
-        //        case -\/(t) =>
-        //          Log.log(Level.SEVERE, "Problem running ProgramExportServlet", t)
-        //          throw new RuntimeException(t)
-        //      }
       }
-    }
   }
 
-  sealed abstract class ResponseCode(val code: Int)
-  object ResponseCode {
-    case object OK extends ResponseCode(1)
-    case object InvalidProgram extends ResponseCode(-1)
+  implicit class ToYesNo(val b: Boolean) extends AnyVal {
+    def toYesNo: YesNoType =
+      b ? YesNoType.YES | YesNoType.NO
   }
-//
-//  private[ProgramExportServlet] implicit class QueryResultToJSON(val queryResult: QueryResult) extends AnyVal {
-//    def toJSON: String = "test"
-//  }
 }
