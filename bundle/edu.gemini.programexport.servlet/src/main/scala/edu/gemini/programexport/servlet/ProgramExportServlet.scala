d5c9d12dbbe6955a6da58ce9c4f935461d9be1d1
Sebastian Raaphorst <sraaphorst@gmail.com>
2021-06-17 17:23:40 -0400

    REL-3934: WIP. At least it compiles now. Guide groups remaining.

--- bundle/edu.gemini.programexport.servlet/src/main/scala/edu/gemini/programexport/servlet/ProgramExportServlet.scala
+++ bundle/edu.gemini.programexport.servlet/src/main/scala/edu/gemini/programexport/servlet/ProgramExportServlet.scala
@@ -29,7 +29,8 @@ import edu.gemini.spModel.config.map.ConfigValMapInstances
 import edu.gemini.spModel.obs.context.ObsContext
 import edu.gemini.spModel.obs.plannedtime.PlannedTimeCalculator
 import edu.gemini.spModel.seqcomp.SeqBase
-import edu.gemini.spModel.target.env.{GuideEnv, GuideGroup, GuideGrp, OptsList}
+import edu.gemini.spModel.target.SPTarget
+import edu.gemini.spModel.target.env.{Asterism, GuideEnv, GuideGroup, GuideGrp, OptsList, TargetEnvironment}
 import edu.gemini.spModel.target.obsComp.TargetObsComp
 
 import java.util.concurrent.TimeUnit
@@ -110,147 +111,140 @@ final case class ProgramExportServlet(odb: IDBDatabaseService, user: Set[Princip
 
     }
 
-  def asterismFields(oc: ISPObsComponent, t: TargetObsComp): Json = {
-    val ispObs = oc.getContextObservation
-    val obsCtx = ObsContext.create(ispObs)
-
-    var te = t.getTargetEnvironment
-    var a = te.getAsterism
-
-    val base = for {
-      base <- a.basePosition(None)
-    } yield {
-      "base" := ("ra" := base.ra.toAngle.formatHMS) ->: ("dec" := base.dec.toAngle.formatDMS) ->: jEmptyObject
+  def magnitudeFields(magnitudes: List[Magnitude]): Json =
+    magnitudes.foldLeft(jEmptyObject) { case (j, m) =>
+      ("band" := m.band.name) ->:
+        ("value" := m.value) ->:
+        ("system" := m.system.name) ->:
+        ("error" :=? m.error) ->?:
+        j
     }
 
-    //val targetInfo = a.allTargets.map { t =>
-    val targetsInfo = a.allSpTargets.foldLeft(jEmptyObject) { (j, t) =>
-      val target = t.getTarget
+  def targetFields(spTarget: SPTarget, te: TargetEnvironment): JsonAssoc = {
+    val target = spTarget.getTarget
 
-      // Spectral distribution
-      val sd = "spectralDistribution" :=? (Target.spectralDistribution.get(target).join match {
-        case Some(BlackBody(t)) =>
-          Some(("type" := "blackBody") ->: ("temperature" := t) ->: jEmptyObject)
-        case Some(PowerLaw(idx)) =>
-          Some(("type" := "powerLaw") ->: ("index" := idx) ->: jEmptyObject)
-        case Some(EmissionLine(wl, w, f, c)) =>
-          Some(("type" := "emissionLine") ->: ("wavelength" := wl.length.toMicrons) ->: ("width" := w.value) ->: ("flux" := f.value) ->: ("continuum" := c.value) ->: jEmptyObject)
-        case Some(UserDefinedSpectrum(_, _)) =>
-          Some(("type" := "userDefined") ->: jEmptyObject)
-        case Some(s: LibraryStar) =>
-          Some(("type" := "libraryStar") ->: ("sedSpectrum" := s.sedSpectrum) ->: jEmptyObject)
-        case Some(n: LibraryNonStar) =>
-          Some(("type" := "libraryNonStar") ->: ("label" := n.label) ->: ("sedSpectrum" := n.sedSpectrum) ->: jEmptyObject)
-        case _ =>
-          None
-      })
+    // Spectral distribution
+    val sd = "spectralDistribution" :=? (Target.spectralDistribution.get(target).join match {
+      case Some(BlackBody(t)) =>
+        Some(("type" := "blackBody") ->: ("temperature" := t) ->: jEmptyObject)
+      case Some(PowerLaw(idx)) =>
+        Some(("type" := "powerLaw") ->: ("index" := idx) ->: jEmptyObject)
+      case Some(EmissionLine(wl, w, f, c)) =>
+        Some(("type" := "emissionLine") ->: ("wavelength" := wl.length.toMicrons) ->: ("width" := w.value) ->: ("flux" := f.value) ->: ("continuum" := c.value) ->: jEmptyObject)
+      case Some(UserDefinedSpectrum(_, _)) =>
+        Some(("type" := "userDefined") ->: jEmptyObject)
+      case Some(s: LibraryStar) =>
+        Some(("type" := "libraryStar") ->: ("sedSpectrum" := s.sedSpectrum) ->: jEmptyObject)
+      case Some(n: LibraryNonStar) =>
+        Some(("type" := "libraryNonStar") ->: ("label" := n.label) ->: ("sedSpectrum" := n.sedSpectrum) ->: jEmptyObject)
+      case _ =>
+        None
+    })
 
-      // Spatial profile
-      val sp = "spatialProfile" :=? (Target.spatialProfile.get(target).join match {
-        case Some(PointSource) =>
-          Some(("type" := "pointSource") ->: jEmptyObject)
-        case Some(UniformSource) =>
-          Some(("type" := "uniformSource") ->: jEmptyObject)
-        case Some(GaussianSource(fwhm)) =>
-          Some(("type" := "gaussianSource") ->: ("fwhm" := fwhm) ->: jEmptyObject)
-        case _ =>
-          None
-      })
+    // Spatial profile
+    val sp = "spatialProfile" :=? (Target.spatialProfile.get(target).join match {
+      case Some(PointSource) =>
+        Some(("type" := "pointSource") ->: jEmptyObject)
+      case Some(UniformSource) =>
+        Some(("type" := "uniformSource") ->: jEmptyObject)
+      case Some(GaussianSource(fwhm)) =>
+        Some(("type" := "gaussianSource") ->: ("fwhm" := fwhm) ->: jEmptyObject)
+      case _ =>
+        None
+    })
 
-      // Tag for sidereal vs nonsidereal
-      val tag = t.isSidereal ? "sidereal" | "nonsidereal"
+    // Tag for sidereal vs nonsidereal.
+    val tag = target.isSidereal ? "sidereal" | "nonsidereal"
+
+    // Determine the target type.
+    val targetType = {
+      if (te.getUserTargets.asScala.map(_.target).contains(spTarget))
+        te.getUserTargets.asScala.find(_.target == spTarget).map(_.`type`.displayName).get
+      else if (te.getGuideEnvironment.containsTarget(spTarget)) {
+        "GuideStar"
+      } else
+        "Base"
+    }
+
+    "target" := (target match {
+      case SiderealTarget(name, coordinates, properMotion, _, _, magnitudes, spectralDistribution, spatialProfile) =>
+        ("name" := name) ->:
+          ("tag" := tag) ->:
+          ("type" := targetType) ->:
+          ("ra" := coordinates.ra.toAngle.formatHMS) ->:
+          ("dec" := coordinates.dec.toAngle.formatDMS) ->:
+          ("deltara" :=? properMotion.map(_.deltaRA.velocity.masPerYear)) ->?:
+          ("deltadec" :=? properMotion.map(_.deltaDec.velocity.masPerYear)) ->?:
+          ("epoch" :=? properMotion.map(_.epoch.year)) ->?:
+          ("magnitudes" := magnitudeFields(magnitudes)) ->:
+          sd ->?: sp ->?:
+          jEmptyObject
+      case NonSiderealTarget(name, _, horizonsDesignation, magnitudes, spectralDistribution, spatialProfile) =>
+        ("name" := name) ->:
+          ("tag" := tag) ->:
+          ("type" := targetType) ->:
+          ("des" :=? horizonsDesignation.map(_.des)) ->?:
+          ("magnitudes" := magnitudeFields(magnitudes)) ->:
+          sd ->?: sp ->?:
+          jEmptyObject
+    })
+  }
+
+  def asterismFields(oc: ISPObsComponent, t: TargetObsComp): Json = {
+    // Needed for guide speed. May not include this as per discussion with Bryan.
+    val ispObs = oc.getContextObservation
+    val obsCtx = ObsContext.create(ispObs)
+    val te = t.getTargetEnvironment
+    val a = te.getAsterism
+
+    // Base information: we are only dealing with asterisms with a single target.
+    val base = a.asInstanceOf[Asterism.Single]
+    val baseInfo = "base" := targetFields(base.t, te) ->: jEmptyObject
+
+    // User target information.
+    val userTargetInfo = te.getUserTargets.asScala.foldLeft(jEmptyObject) { (j, t) =>
+      ("target" := targetFields(t.target, te)) ->: j
+    }
+
+    // Guide group information.
+
+
 
       // Guide groups.
-      val guideEnv = te.getGuideEnvironment.guideEnv
-      guideEnv.groups.foldLeft(jEmptyObject){(j, grp) =>
-        val primary = grp === guideEnv.primaryGroup
-        val tgtMap = GuideGrp.TargetCollectionGuideGroup.targets(grp)
-        tgtMap.values.map { xyz => xyz.map {  }}
-        grp.referencedGuiders.
-      }
+//      val guideEnv = te.getGuideEnvironment.guideEnv
+//      guideEnv.groups.foldLeft(jEmptyObject){(j, grp) =>
+//        val primary = grp === guideEnv.primaryGroup
+//        val tgtMap = GuideGrp.TargetCollectionGuideGroup.targets(grp)
+//        tgtMap.values.map { xyz => xyz.map {  }}
+//        grp.referencedGuiders.
+//      }
 
       // Guide groups.
-      def guideGroups: Option[Json] = target match {
-        case t:SiderealTarget =>
-          var xyz =
-          Some()
-        case _: => None
-        val target: SiderealTarget = ???
-        val primaryGuiders = gg.getPrimaryReferencedGuiders.asScala.toSet
-        val magOpt = gg.getReferencedGuiders.asScala.foldLeft(jEmptyObject) { (j, gp) =>primaryGuiders.con
-          ("guiderKey" := gp.getKey) ->:
-          ("guiderPrimary" := primaryGuiders.contains(gp).toYesNo.displayValue) ->: j
+//      def guideGroups: Option[Json] = target match {
+//        case t:SiderealTarget =>
+//          var xyz =
+//          Some()
+//        case _: => None
+//        val target: SiderealTarget = ???
+//        val primaryGuiders = gg.getPrimaryReferencedGuiders.asScala.toSet
+//        val magOpt = gg.getReferencedGuiders.asScala.foldLeft(jEmptyObject) { (j, gp) =>primaryGuiders.con
+//          ("guiderKey" := gp.getKey) ->:
+//          ("guiderPrimary" := primaryGuiders.contains(gp).toYesNo.displayValue) ->: j
 //          val key = gp.getKey
 //          val isPrimary = primaryGuiders.contains(gp)
 //          gp.getBands.extract(target)
-        }
+//        }
 //        val guideSpeed = AgsMagnitude.fastestGuideSpeed(MagTable, )
 //        jEmptyObject
-      }
+//      }
 
-      val ge = te.getGroups.asScala.foldLeft(jEmptyObject){ case (j, g) =>
-        g
-        ("name" := g.getName.getOrElse("Unnamed")) ->: j
+//      val ge = te.getGroups.asScala.foldLeft(jEmptyObject){ case (j, g) =>
+//        g
+//        ("name" := g.getName.getOrElse("Unnamed")) ->: j
+//
+//      }
 
-      }
-      // Get the target type: science, guide star, user...
-      // This is more complicated than it should be, but that is because of
-      // the user targets being in a separate, independent class.
-      val targetType = {
-        if (te.getUserTargets.asScala.map(_.target).contains(t))
-          te.getUserTargets.asScala.find(_.target == t).map(_.`type`.displayName).get
-        else if (te.getGuideEnvironment.containsTarget(t)) {
-          // AgsMagnitude.fastestGuideSpeed
-          //        ProbeLimitsTable.loadOrThrow()
-          //te.getGuideEnvironment.guideEnv.
-          te.getGuideEnvironment.getGroup(0).getOrNull().grouped(0).map { gpts =>
-            gpts
-          }
-          "Guide Star"
-        } else
-          "Base"
-      }
-
-      def magnitudeFields(magnitudes: List[Magnitude]): Json =
-        magnitudes.foldLeft(jEmptyObject) { case (j, m) =>
-          ("band" := m.band.name) ->:
-            ("value" := m.value) ->:
-            ("system" := m.system.name) ->:
-            j
-        }
-
-      def guideSpeed(target: SiderealTarget): Json = {
-        AgsMagnitude.fastestGuideSpeed(MagTable, target.magnitudes.head, obsCtx.asScalaOpt.get.getConditions)
-      }
-
-      // Sidereal properties
-      val targetInfo: Json = target match {
-        case SiderealTarget(name, coordinates, properMotion, _, _, magnitudes, spectralDistribution, spatialProfile) =>
-          ("name" := name) ->:
-            ("tag" := tag) ->:
-            ("type" := targetType) ->:
-            ("ra" := coordinates.ra.toAngle.formatHMS) ->:
-            ("dec" := coordinates.dec.toAngle.formatDMS) ->:
-            ("deltara" :=? properMotion.map(_.deltaRA.velocity.masPerYear)) ->?:
-            ("deltadec" :=? properMotion.map(_.deltaDec.velocity.masPerYear)) ->?:
-            ("epoch" :=? properMotion.map(_.epoch.year)) ->?:
-            ("magnitudes" := magnitudeFields(magnitudes)) ->:
-            sd ->?: sp ->?:
-            jEmptyObject
-        case NonSiderealTarget(name, _, horizonsDesignation, magnitudes, spectralDistribution, spatialProfile) =>
-          ("name" := name) ->:
-            ("tag" := tag) ->:
-            ("type" := targetType) ->:
-            ("des" :=? horizonsDesignation.map(_.des)) ->?:
-            ("magnitudes" := magnitudeFields(magnitudes)) ->:
-            sd ->?: sp ->?:
-            jEmptyObject
-      }
-
-      ("target" := targetInfo) ->: j
-    }
-
-    base ->?: ("targets" := targetsInfo) ->: jEmptyObject
+    baseInfo ->: ("targets" := userTargetInfo) ->: jEmptyObject
   }
 
   def programFields(p: SPProgram): Json = {
