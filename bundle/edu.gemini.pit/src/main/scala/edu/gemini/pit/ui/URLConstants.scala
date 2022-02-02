cf8c8014bcec835f86ea91dfd3243e1e48279046
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2022-02-02 20:31:30 -0300

    REL-4063 Update Help links
    Signed-off-by: Carlos Quiroz <carlos.quiroz@noirlab.edu>

--- bundle/edu.gemini.pit/src/main/scala/edu/gemini/pit/ui/URLConstants.scala
+++ bundle/edu.gemini.pit/src/main/scala/edu/gemini/pit/ui/URLConstants.scala
@@ -7,5 +7,5 @@ import edu.gemini.model.p1.immutable.Semester
  */
 object URLConstants {
   val GET_TEMPLATES = (s"http://software.gemini.edu/phase1/templates/${Semester.current.display}/", "Open directory for LaTeX/Word text section templates in browser.")
-  val OPEN_ITC = ("http://www.gemini.edu/node/10241", "Open Integration Time Calculator page in browser.")
+  val OPEN_ITC = ("https://www.gemini.edu/observing/resources/itc", "Open Integration Time Calculator page in browser.")
 }
