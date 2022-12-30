9345dfda6e40b98eb66084fb2bb3a6e5da8f21e7
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2022-12-30 14:45:15 -0300

    Refactor proper motion calculation to keep serialization

--- bundle/edu.gemini.spModel.core/src/main/scala/edu/gemini/spModel/core/ProperMotion.scala
+++ bundle/edu.gemini.spModel.core/src/main/scala/edu/gemini/spModel/core/ProperMotion.scala
@@ -24,7 +24,8 @@ case class ProperMotion(
   /** Coordinates `elapsedYears` fractional epoch-years after `epoch`. */
   def plusYears(t: Target, elapsedYears: Double): Coordinates = {
     val baseCoordinates = t.coords(None).getOrElse(Coordinates.zero)
-
+    val properVelocity: Offset = Offset(OffsetP(Angle.fromDegrees(Angle.signedDegrees(deltaRA.velocity.toDegreesPerYear))),
+                                        OffsetQ(Angle.fromDegrees(Angle.signedDegrees(deltaDec.velocity.toDegreesPerYear))))
     val radialVelocity: Double = {
       val redshift: Option[Redshift] = Target.redshift.get(t).flatten
       redshift.getOrElse(Redshift.zero).toRadialVelocity.toKilometersPerSecond
@@ -32,7 +33,7 @@ case class ProperMotion(
     val parallax = Target.parallax.get(t).flatten.orZero.mas / ProperMotion.Million
     ProperMotion.properMotionCalculator(baseCoordinates,
                                         Epoch.JulianLengthOfYear,
-                                        (deltaRA.velocity.toDegreesPerYear, deltaDec.velocity.toDegreesPerYear),
+                                        properVelocity,
                                         radialVelocity,
                                         parallax,
                                         elapsedYears)
@@ -75,17 +76,19 @@ object ProperMotion {
    */
   def properMotionCalculator(baseCoordinates: Coordinates,
                              daysPerYear: Double,
-                             properVelocity: Vec2,
+                             properVelocity: Offset,
                              radialVelocity: Double,
                              parallax: Double,
                              elapsedYears: Double): Coordinates = {
+
+    val (pmra, pmdec): Vec2 = (properVelocity.p.toAngle.toSignedDegrees.toRadians, properVelocity.q.toAngle.toSignedDegrees.toRadians)
     // We want the baseCoordinates in radians.
     val (ra,  dec)  = (baseCoordinates.ra.toAngle.toRadians, baseCoordinates.dec.toAngle.toRadians)
     // at dec 90 this gets undetermined, while we never observe at dec 90 it wouldn't be nice to have
     // a division by zero error. At dec 90 we don't correct by cos(dec)
     val (dRa, dDec) = 
-      if (cos(dec) == 0.0) (properVelocity._1.toRadians, properVelocity._2.toRadians) 
-      else (properVelocity._1.toRadians / cos(dec), properVelocity._2.toRadians)
+      if (cos(dec) == 0.0) (pmra, pmdec) 
+      else (pmra / cos(dec), pmdec)
 
     val pos: Vec3 = {
       val cd = cos(dec)
