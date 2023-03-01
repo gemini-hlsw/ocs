74fa0fa9f17955444ff9f817cd1930006ae01ee9
Francisco Ramos <framos@framos-ml1.local>
2023-03-01 17:26:07 -0300

    Resolve merge with gitignore

---
+++ .Trash-3906/files/GhostBlueprint.scala
@@ -0,0 +1,35 @@
+package edu.gemini.model.p1.immutable
+
+import edu.gemini.model.p1.{mutable => M}
+
+object GhostBlueprint {
+  def apply(m: M.GhostBlueprint): GhostBlueprint = new GhostBlueprint(m)
+}
+
+case class GhostBlueprint(resolutionMode: GhostResolutionMode, targetMode: GhostTargetMode) extends GeminiBlueprintBase {
+  def name: String = s"GHOST ${resolutionMode.value} ${targetMode.value}"
+
+  def this(m: M.GhostBlueprint) = this(
+    m.getResolutionMode,
+    m.getTargetMode
+  )
+
+  override def instrument: Instrument = Instrument.Ghost
+  override def ao: AoPerspective = AoNone
+
+  def mutable(n: Namer) = {
+    val m = Factory.createGhostBlueprint()
+    m.setId(n.nameOf(this))
+    m.setName(name)
+    m.setResolutionMode(resolutionMode)
+    m.setTargetMode(targetMode)
+    m
+  }
+
+  def toChoice(n: Namer) = {
+    val m = Factory.createGhostBlueprintChoice()
+    m.setGhost(mutable(n))
+    m
+  }
+
+}
