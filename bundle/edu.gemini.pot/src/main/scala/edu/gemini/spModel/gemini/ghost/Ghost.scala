eb66671e3fc5da506483601e3bb3eef2c4e4f1d2
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2023-03-17 13:44:17 -0300

    Recover ITC calculation of readout times

--- bundle/edu.gemini.pot/src/main/scala/edu/gemini/spModel/gemini/ghost/Ghost.scala
+++ bundle/edu.gemini.pot/src/main/scala/edu/gemini/spModel/gemini/ghost/Ghost.scala
@@ -2,7 +2,7 @@ package edu.gemini.spModel.gemini.ghost
 
 import java.beans.PropertyDescriptor
 import java.util.logging.Logger
-import java.util.{Collections, List => JList, Map => JMap, Set => JSet}
+import java.util.{ArrayList, Collection, Collections, List => JList, Map => JMap, Set => JSet}
 import edu.gemini.pot.sp._
 import edu.gemini.shared.util.immutable.{Option => JOption}
 import edu.gemini.skycalc.Angle
@@ -17,11 +17,10 @@ import edu.gemini.spModel.gemini.ghost.Ghost.{AsterismTypeConfigInfo, Resolution
 import edu.gemini.spModel.gemini.init.{ComponentNodeInitializer, ObservationNI}
 import edu.gemini.spModel.inst.{ScienceAreaGeometry, VignettableScienceAreaInstrument}
 import edu.gemini.spModel.obs.SPObservation
-import edu.gemini.spModel.obs.plannedtime.{CommonStepCalculator, PlannedTime}
+import edu.gemini.spModel.obs.plannedtime.{CommonStepCalculator, ExposureCalculator, PlannedTime}
 import edu.gemini.spModel.obs.plannedtime.PlannedTime.{CategorizedTime, CategorizedTimeGroup, Category}
 import edu.gemini.spModel.obscomp.{InstConfigInfo, InstConstants, ItcOverheadProvider, SPInstObsComp}
 import edu.gemini.spModel.pio.{ParamSet, Pio, PioFactory}
-import edu.gemini.spModel.rich.shared.immutable._
 import edu.gemini.spModel.seqcomp.SeqConfigNames
 import edu.gemini.spModel.seqcomp.SeqConfigNames.{INSTRUMENT_KEY, OBSERVE_KEY}
 import edu.gemini.spModel.syntax.duration._
@@ -370,6 +369,20 @@ final class Ghost
   override def getSetupTime(obs: ISPObservation): Duration =
     Duration.ofMinutes(20)
 
+  // Formula used on the ITC
+//  override def calc(cur: Config, prev: Option[Config]): PlannedTime.CategorizedTimeGroup = {
+//    val times: util.Collection[PlannedTime.CategorizedTime] = new util.ArrayList[PlannedTime.CategorizedTime]
+//    val exposureTime: Double = ExposureCalculator.instance.exposureTimeSec(cur)
+//    times.add(CategorizedTime.fromSeconds(Category.EXPOSURE, exposureTime))
+//    val readoutTime: Double = GhostReadoutTime.getReadoutOverhead(cur)
+//    System.out.println("*** readoutTime: " + readoutTime)
+//    times.add(CategorizedTime.fromSeconds(Category.READOUT, readoutTime))
+//    System.out.println("DHS suppouse value: " + getDhsWriteTime.time)
+//    //times.add(getDhsWriteTime());
+//    CommonStepCalculator.instance.calc(cur, prev).addAll(times)
+//  }
+
+
   override def calc(cur: Config, prev: JOption[Config]): CategorizedTimeGroup = {
     val times = new java.util.ArrayList[CategorizedTime]()
 
@@ -417,18 +430,18 @@ final class Ghost
   }
 
   override def getSetupTime(conf: Config): Duration =
-    Ghost.SETUP_TIME
+    Ghost.SetupTime
 
   override def getReacquisitionTime(conf: Config): Duration =
-    Ghost.REACQUISITION_TIME
+    Ghost.ReaquisitionTime
 }
 
 object Ghost {
   val LOG: Logger = Logger.getLogger(classOf[Ghost].getName)
-  val SETUP_TIME = Duration.ofSeconds(900) // This value was provided by Venus
+  val SetupTime: Duration = Duration.ofSeconds(900) // This value was provided by Venus
 
   // should be added 900 seconds of the SETUP_TIME
-  val REACQUISITION_TIME: Duration = Duration.ofSeconds(300)
+  val ReaquisitionTime: Duration = Duration.ofSeconds(300)
 
   // Unfortunately we need a Java "Supplier" and "Function" which makes it
   // awkward to create the NodeInitializer via ComponentNodeInitializer.
