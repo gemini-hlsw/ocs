7a462ffb3bbdd51b329ce787e5ce522646818aea
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2023-03-21 10:42:18 -0300

    REL-4265 Setup position angle editor

--- bundle/edu.gemini.pot/src/main/scala/edu/gemini/spModel/gemini/igrins2/Igrins2.scala
+++ bundle/edu.gemini.pot/src/main/scala/edu/gemini/spModel/gemini/igrins2/Igrins2.scala
@@ -1,16 +1,21 @@
 package edu.gemini.spModel.gemini.igrins2
 
-import edu.gemini.pot.sp.{ISPNodeInitializer, ISPObsComponent, SPComponentType}
-import edu.gemini.spModel.core.Site
+import edu.gemini.pot.sp.{ISPNodeInitializer, ISPObsComponent, ISPObservation, SPComponentType}
+import edu.gemini.shared.util.immutable
+import edu.gemini.shared.util.immutable.{DefaultImList, ImList}
+import edu.gemini.spModel.core.{Angle, Site}
 import edu.gemini.spModel.data.ISPDataObject
 import edu.gemini.spModel.data.config.{DefaultParameter, DefaultSysConfig, ISysConfig, StringParameter}
 
 import java.util.{Collections, Map => JMap, Set => JSet}
 import edu.gemini.spModel.data.property.{PropertyProvider, PropertySupport}
 import edu.gemini.spModel.gemini.init.ComponentNodeInitializer
+import edu.gemini.spModel.gemini.parallacticangle.ParallacticAngleSupportInst
+import edu.gemini.spModel.inst.ParallacticAngleSupport
 import edu.gemini.spModel.obscomp.{InstConstants, SPInstObsComp}
 import edu.gemini.spModel.pio.{ParamSet, Pio, PioFactory}
 import edu.gemini.spModel.seqcomp.SeqConfigNames
+import edu.gemini.spModel.telescope.{PosAngleConstraint, PosAngleConstraintAware}
 
 import java.beans.PropertyDescriptor
 import scala.collection.immutable.TreeMap
@@ -20,7 +25,9 @@ import scala.collection.JavaConverters._
  ** The Igrins2 instrument SP model.
  * Note that we do not override clone since private variables are immutable.
  */
-final class Igrins2 extends SPInstObsComp(Igrins2.SP_TYPE) with PropertyProvider with Igrins2Mixin {
+final class Igrins2 extends ParallacticAngleSupportInst(Igrins2.SP_TYPE) with PropertyProvider with PosAngleConstraintAware with Igrins2Mixin {
+  private var _posAngleConstraint = PosAngleConstraint.PARALLACTIC_ANGLE
+
   /**
    * Get the site in which the instrument resides.
    *
@@ -40,20 +47,55 @@ final class Igrins2 extends SPInstObsComp(Igrins2.SP_TYPE) with PropertyProvider
 
 override def getParamSet(factory: PioFactory): ParamSet = {
     val paramSet = super.getParamSet(factory)
-    Pio.addParam(factory, paramSet, InstConstants.POS_ANGLE_PROP, getPosAngleDegreesStr)
-    paramSet
+    Pio.addParam(factory, paramSet, Igrins2.POS_ANGLE_CONSTRAINT_PROP.getName(), getPosAngleConstraint().name());
+  paramSet
   }
 
   override def setParamSet(paramSet: ParamSet): Unit = {
-    Option(Pio.getValue(paramSet, InstConstants.POS_ANGLE_PROP)).map(_.toDouble).foreach(setPosAngleDegrees)
+    Option(Pio.getValue(paramSet, Igrins2.POS_ANGLE_CONSTRAINT_PROP)).map(_.toDouble).foreach(setPosAngleDegrees)
   }
 
   override def getSysConfig: ISysConfig = {
     val sc = new DefaultSysConfig(SeqConfigNames.INSTRUMENT_CONFIG_NAME)
     sc.putParameter(StringParameter.getInstance(ISPDataObject.VERSION_PROP, getVersion))
-    sc.putParameter(DefaultParameter.getInstance(Igrins2.POS_ANGLE_PROP, getPosAngle))
+    sc.putParameter(DefaultParameter.getInstance(InstConstants.POS_ANGLE_PROP, getPosAngleDegrees))
     sc
   }
+
+  override def getPosAngleConstraint: PosAngleConstraint = if (_posAngleConstraint == null) PosAngleConstraint.PARALLACTIC_ANGLE else _posAngleConstraint
+
+  override def setPosAngleConstraint(newValue: PosAngleConstraint): Unit = {
+    val oldValue = getPosAngleConstraint
+    if (!(oldValue == newValue)) {
+      _posAngleConstraint = newValue
+      firePropertyChange(Igrins2.POS_ANGLE_CONSTRAINT_PROP.getName, oldValue, newValue)
+    }
+  }
+
+  /**
+   * The list of valid position angle constraints for this instrument.
+   *
+   * @return a list of the supported position angle constraints.
+   */
+  override def getSupportedPosAngleConstraints: ImList[PosAngleConstraint] = {
+    DefaultImList.create(
+      PosAngleConstraint.FIXED,
+      PosAngleConstraint.PARALLACTIC_ANGLE,
+      PosAngleConstraint.PARALLACTIC_OVERRIDE)
+  }
+
+  /**
+   * Return true if this instrument and its configuration allows calculation of best position angle,
+   * and false otherwise.
+   */
+  override def allowUnboundedPositionAngle(): Boolean = false
+
+  /**
+   * Perform the parallactic angle computation for the observation.
+   */
+  override def calculateParallacticAngle(obs: ISPObservation): immutable.Option[Angle] =
+    super.calculateParallacticAngle(obs)
+
 }
 
 object Igrins2 {
@@ -71,7 +113,7 @@ object Igrins2 {
   // The name of the Ghost instrument configuration.
   val INSTRUMENT_NAME_PROP: String = "IGRINS2"
 
-  val POS_ANGLE_PROP: PropertyDescriptor = initProp(InstConstants.POS_ANGLE_PROP, query = query_no, iter = iter_no)
+  var POS_ANGLE_CONSTRAINT_PROP: PropertyDescriptor = initProp("posAngleConstraint", query=query_no, iter=iter_no)
 
   // Unfortunately we need a Java "Supplier" and "Function" which makes it
   // awkward to create the NodeInitializer via ComponentNodeInitializer.
@@ -88,7 +130,7 @@ object Igrins2 {
     new ComponentNodeInitializer(SPComponentType.INSTRUMENT_IGNRIS2, Igrins2Supplier, Igrins2CbFactory)
 
   private val Properties: List[(String, PropertyDescriptor)] = List[PropertyDescriptor](
-    POS_ANGLE_PROP
+    POS_ANGLE_CONSTRAINT_PROP
   ).map(p => (p.getName, p))
 
   private[igrins2] val PropertyMap: JMap[String, PropertyDescriptor] = {
--- bundle/jsky.app.ot/src/main/scala/jsky/app/ot/gemini/igrins2/Igrins2Editor.scala
+++ bundle/jsky.app.ot/src/main/scala/jsky/app/ot/gemini/igrins2/Igrins2Editor.scala
@@ -1,17 +1,27 @@
 package jsky.app.ot.gemini.igrins2
 
-import edu.gemini.pot.sp.ISPObsComponent
+import edu.gemini.pot.sp.{ISPObsComponent, SPComponentType}
 import edu.gemini.shared.gui.bean.TextFieldPropertyCtrl
+import edu.gemini.spModel.core.Site
 import edu.gemini.spModel.gemini.igrins2.Igrins2
+import jsky.app.ot.OTOptions
 import jsky.app.ot.gemini.editor.ComponentEditor
 import jsky.app.ot.gemini.ghost.GhostEditor.LabelPadding
+import jsky.app.ot.gemini.parallacticangle.PositionAnglePanel
 
+import java.beans.{PropertyChangeEvent, PropertyChangeListener}
 import javax.swing.JPanel
 import scala.swing.GridBagPanel.{Anchor, Fill}
-import scala.swing.{Alignment, Component, GridBagPanel, Insets, Label, Separator, Swing}
+import scala.swing.{Alignment, GridBagPanel, Insets, Label, Separator, Swing}
 
 class Igrins2Editor extends ComponentEditor[ISPObsComponent, Igrins2]{
+
   private object ui extends GridBagPanel {
+    val updateParallacticAnglePCL: PropertyChangeListener = new PropertyChangeListener() {
+      override def propertyChange(evt: PropertyChangeEvent): Unit = {
+        posAnglePanel.updateParallacticControls()
+      }
+    }
 
     private var row = 0
     border = ComponentEditor.PANEL_BORDER
@@ -25,21 +35,22 @@ class Igrins2Editor extends ComponentEditor[ISPObsComponent, Igrins2]{
       anchor = Anchor.NorthEast
       gridx = 0
       gridy = row
-      insets = new Insets(3, 10, 0, LabelPadding)
+      insets = new Insets(3, 10, 0, Igrins2Editor.LabelPadding)
     }
 
-    val posAngleCtrl: TextFieldPropertyCtrl[Igrins2, java.lang.Double] = TextFieldPropertyCtrl.createDoubleInstance(Igrins2.POS_ANGLE_PROP, 1)
-    posAngleCtrl.setColumns(10)
-    layout(Component.wrap(posAngleCtrl.getTextField)) = new Constraints() {
-      anchor = Anchor.NorthWest
-      gridx = 1
-      gridy = row
-      insets = new Insets(0, 0, 0, LabelPadding)
-    }
+//    val posAngleCtrl: TextFieldPropertyCtrl[Igrins2, java.lang.Double] = TextFieldPropertyCtrl.createDoubleInstance(Igrins2.POS_ANGLE_PROP, 1)
+//    posAngleCtrl.setColumns(10)
+//    layout(Component.wrap(posAngleCtrl.getTextField)) = new Constraints() {
+//      anchor = Anchor.NorthWest
+//      gridx = 1
+//      gridy = row
+//      insets = new Insets(0, 0, 0, LabelPadding)
+//    }
 
     val posAngleUnits: Label = new Label("deg E of N")
     posAngleUnits.horizontalAlignment = Alignment.Left
-    layout(posAngleUnits) = new Constraints() {
+    val posAnglePanel: PositionAnglePanel[Igrins2, Igrins2Editor] = PositionAnglePanel.apply[Igrins2, Igrins2Editor](SPComponentType.INSTRUMENT_IGNRIS2)
+    layout(posAnglePanel) = new Constraints() {
       anchor = Anchor.NorthWest
       gridx = 2
       gridy = row
@@ -47,23 +58,23 @@ class Igrins2Editor extends ComponentEditor[ISPObsComponent, Igrins2]{
     }
 
     /** Eat up all remaining horizontal space in the form. **/
-    layout(new Label) = new Constraints() {
-      anchor = Anchor.West
-      gridx = 3
-      gridy = row
-      weightx = 1.0
-    }
-    row += 1
-
-    layout(new Separator()) = new Constraints() {
-      anchor = Anchor.West
-      fill = Fill.Horizontal
-      gridx = 0
-      gridy = row
-      gridwidth = 3
-      insets = new Insets(10, 0, 0, 0)
-    }
-    row += 1
+//    layout(new Label) = new Constraints() {
+//      anchor = Anchor.West
+//      gridx = 3
+//      gridy = row
+//      weightx = 1.0
+//    }
+//    row += 1
+//
+//    layout(new Separator()) = new Constraints() {
+//      anchor = Anchor.West
+//      fill = Fill.Horizontal
+//      gridx = 0
+//      gridy = row
+//      gridwidth = 3
+//      insets = new Insets(10, 0, 0, 0)
+//    }
+//    row += 1
   }
 
   /**
@@ -71,7 +82,23 @@ class Igrins2Editor extends ComponentEditor[ISPObsComponent, Igrins2]{
    */
   override def getWindow: JPanel = ui.peer
 
-  override def handlePostDataObjectUpdate(dataObj: Igrins2): Unit = Swing.onEDT {
-    ui.posAngleCtrl.setBean(dataObj)
+  override def handlePostDataObjectUpdate(inst: Igrins2): Unit = Swing.onEDT {
+//    ui.posAngleCtrl.setBean(inst)
+    ui.posAnglePanel.init(this, Site.GN)
+    val editable = OTOptions.areRootAndCurrentObsIfAnyEditable(getProgram, getContextObservation)
+    ui.posAnglePanel.updateEnabledState(editable)
+
+    inst.addPropertyChangeListener(Igrins2.POS_ANGLE_CONSTRAINT_PROP.getName, ui.updateParallacticAnglePCL)
   }
+
+  override def handlePreDataObjectUpdate (inst: Igrins2): Unit = {
+    Option(inst).foreach {inst =>
+
+      inst.removePropertyChangeListener(Igrins2.POS_ANGLE_CONSTRAINT_PROP.getName, ui.updateParallacticAnglePCL)
+    }
+  }
+}
+
+object Igrins2Editor {
+  val LabelPadding = 15
 }
