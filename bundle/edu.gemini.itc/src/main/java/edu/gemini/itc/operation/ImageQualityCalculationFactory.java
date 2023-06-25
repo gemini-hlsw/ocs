bfef97a22def35d62d16ce6be0e62bad8502c61a
Andrew Stephens <astephens@gemini.edu>
2023-06-25 12:10:10 -1000

    Add logging

--- bundle/edu.gemini.itc/src/main/java/edu/gemini/itc/operation/ImageQualityCalculationFactory.java
+++ bundle/edu.gemini.itc/src/main/java/edu/gemini/itc/operation/ImageQualityCalculationFactory.java
@@ -6,8 +6,11 @@ import edu.gemini.itc.shared.SourceDefinition;
 import edu.gemini.itc.shared.TelescopeDetails;
 import edu.gemini.spModel.guide.GuideProbe;
 import edu.gemini.spModel.core.GaussianSource;
+import java.util.logging.Logger;
 
 public final class ImageQualityCalculationFactory {
+
+    private static final Logger Log = Logger.getLogger(ImageQualityCalculationFactory.class.getName());
 
     private ImageQualityCalculationFactory() {
     }
@@ -17,6 +20,8 @@ public final class ImageQualityCalculationFactory {
             ObservingConditions observingConditions,
             TelescopeDetails telescope,
             Instrument instrument) {
+
+        Log.fine("Source Profile: " + sourceDefinition.profile());
 
         if (sourceDefinition.profile() instanceof GaussianSource) {
             // Case A: The Image quality is defined by the user
@@ -37,6 +42,8 @@ public final class ImageQualityCalculationFactory {
             // Case B: The Image Quality is defined by either of the
             // Probes in conjunction with the Atmospheric Seeing.
             // This case creates an ImageQuality Calculation
+
+            Log.fine("GuideProbe: " + telescope.getWFS());
 
             // For AOWFS the image quality files of OIWFS are used (there are currently no files for AOWFS)
             final GuideProbe.Type wfs =
